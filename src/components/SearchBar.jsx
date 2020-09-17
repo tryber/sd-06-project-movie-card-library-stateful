8e986450d8ae1bfacc107614b0d3b841f4710395
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-17 20:18:14 -0300

    Adding function renderTextInput to create label for input text

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,29 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  renderTextInput() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <label htmlFor="searchText" data-testid="text-input-label">
+      Inclui o texto:
+      <input
+        id="searchText"
+        value={searchText}
+        data-testid="text-input"
+        onChange={onSearchTextChange}
+        type="text"
+      />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <div>{this.renderTextInput()}</div>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
