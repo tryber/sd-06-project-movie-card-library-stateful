59ee2eae66701110ef7435f4cf2b2393a02739bf
Alvaro Neto <alvaroshn@gmail.com>
2020-09-17 19:47:41 -0300

    Code Climate - SearchBar reduzindo linhas [3]

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import SearchTextComponent from './SearchTextComponent'
 
 class SearchBar extends Component {
   render() {
@@ -9,15 +10,14 @@ class SearchBar extends Component {
     } = this.props;
     return (
       <form data-testid="search-bar-form" htmlFor="my-form">
-        <label htmlFor="my-form" data-testid="text-input-label">Inclui o texto:
-          <input type="text" data-testid="text-input"
-            value={searchText} onChange={onSearchTextChange} />
-        </label>
-        <label htmlFor="my-form" data-testid="checkbox-input-label">Mostrar somente favoritos
-          <input type="checkbox" data-testid="checkbox-input"
-            checked={bookmarkedOnly} onChange={onBookmarkedChange} />
-        </label>
-        <label htmlFor="my-form" data-testid="select-input-label">Filtrar por gênero
+        <SearchTextComponent
+          searchText={searchText}
+          onSearchTextChange={onSearchTextChange}
+          bookmarkedOnly={bookmarkedOnly}
+          onBookmarkedChange={onBookmarkedChange}
+        />
+        <label htmlFor="my-form" data-testid="select-input-label">
+          Filtrar por gênero
           <select data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
             <option value="" data-testid="select-option">Todos</option>
             <option value="action" data-testid="select-option">Ação</option>
