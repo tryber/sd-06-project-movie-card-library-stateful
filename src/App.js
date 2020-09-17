3b1d8388020dd5fd6d963a43fbb05e7fd7cdc19f
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-17 17:00:32 -0300

    starting new project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+import MovieCard from './components/MovieCard';
+import MovieList from './components/MovieList';
+import Rating from './components/Rating'
 
 function App() {
   return (
