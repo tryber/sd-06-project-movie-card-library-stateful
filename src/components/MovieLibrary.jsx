43a0bea4092feea5415ebe274ff7dd2d4c89a62b
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-09-17 17:54:41 -0300

    Componente SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,42 @@
-// implement MovieLibrary component here
+
+import React, { Component } from 'react';
+
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  constructor() {
+    super();
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange() {
+  }
+  onBookmarkedChange() {
+  }
+  onSelectedGenreChange() {
+
+  }
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar
+          searchText={'text'}
+          onSearchTextChange={this.onSearchTextChange}
+          bookmarkedOnly={true}
+          onBookmarkedChange={this.onBookmarkedChange}
+          selectedGenre={'Genre'}
+          onSelectedGenreChange={this.onSelectedGenreChange}
+        />,
+        {/* <MovieList movies={this.props.movies} />
+        <AddMovie /> */}
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,59 @@
-// implement SearchBar component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.selectGenre = this.selectGenre.bind(this);
+  }
+
+  selectGenre() {
+    return (
+      <label data-testid="select-input-label" htmlFor="selectedGenre"> Filtrar por gênero
+        <select
+          name="selectedGenre" value={this.props.selectedGenre}
+          onChange={this.props.onSelectedGenreChange} data-testid="select-input"
+        >
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <label data-testid="text-input-label" htmlFor="searchText"> Inclui o texto:
+            <input
+              data-testid="text-input" type="text" name="searchText"
+              value={this.props.searchText} onChange={this.props.onSearchTextChange}
+            />
+          </label>
+          <label data-testid="checkbox-input-label" htmlFor="bookmarkedOnly">
+            Mostrar somente favoritos
+            <input
+              data-testid="checkbox-input" type="checkbox" name="bookmarkedOnly"
+              checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}
+            />
+          </label>
+          {this.selectGenre()}
+        </form>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
