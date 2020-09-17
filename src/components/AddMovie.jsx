5ac0cd62c7de6179432c8a309a8fdf3807c4a900
Stefano Branz <stefano.branz@gmail.com>
2020-09-17 18:25:25 -0300

    First steps and requisite 1

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  render() {
+    return(
+      <div className="add-movie">
+        My Add Movie
+      </div>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,13 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return(
+      <div className="search-bar">
+        My Search Bar
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
