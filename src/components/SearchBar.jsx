c755338ddf0287cb513c4d1cbb0d27492a7dbdd4
ilanribeiro <ilanribeiro@gmail.com>
2020-09-17 19:45:34 -0300

    SearchBar render success. Render 1 Form within SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <p>Search Bar</p>
+        <form action="" data-testid="search-bar-form">
+          <input type="text" name="" id=""/>
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default SearchBar;
