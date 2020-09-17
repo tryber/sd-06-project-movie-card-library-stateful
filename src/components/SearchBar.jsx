f073589eb4a4896dcb3cf8205ee831fe8cac1aa6
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-17 18:26:59 -0300

    Adequando ao CC

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,33 +7,31 @@ class SearchBar extends Component {
     const { selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <label htmlFor="select-input" data-testid="select-input-label">
-          Filtrar por gênero
-          <select id="select-input" value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input">
-            <option data-testid="select-option" value="">Todos</option>
-            <option data-testid="select-option" value="action">Ação</option>
-            <option data-testid="select-option" value="comedy">Comédia</option>
-            <option data-testid="select-option" value="thriller">Suspense</option>
-          </select>
-        </label>
-    )
+        Filtrar por gênero
+        <select id="select-input" value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input">
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
   }
   render() {
-    const { searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange
-    } = this.props;
+    const { searchText,onSearchTextChange,bookmarkedOnly,onBookmarkedChange} = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
           Inclui o texto:
-          <input id="text-input" type="text" value={searchText} 
-            onChange={onSearchTextChange} data-testid="text-input" />
+          <input id="text-input" type="text" value={searchText}
+            onChange={onSearchTextChange} data-testid="text-input"
+          />
         </label>
         <label htmlFor="check-input" data-testid="checkbox-input-label">
           Mostrar somente favoritos
-          <input id="check-input" type="checkbox" checked={bookmarkedOnly} 
-            onChange={onBookmarkedChange} data-testid="checkbox-input" />
+          <input id="check-input" type="checkbox" checked={bookmarkedOnly}
+            onChange={onBookmarkedChange} data-testid="checkbox-input"
+          />
         </label>
         {this.renderSelect()}
       </form>
@@ -42,12 +40,12 @@ class SearchBar extends Component {
 }
 
 SearchBar.propTypes = {
-  searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.string ,
-  bookmarkedOnly: PropTypes.string,
-  onBookmarkedChange: PropTypes.string,
-  selectedGenre: PropTypes.string,
-  onSelectedGenreChange: PropTypes.string 
-}
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.string.isRequired,
+  bookmarkedOnly: PropTypes.string.isRequired,
+  onBookmarkedChange: PropTypes.string.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
