309038054de3ae2cc85b8b4219c95eda33ce9b6e
Renan Cunha <renan2610@gmail.com>
2020-09-17 15:49:02 -0300

    Iniciando o projeto Movie Card Library Statefull

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
