66f3581cea8649828e8ba7a02092c017f6995c29
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-17 18:38:05 -0300

    Adequando ao CC - 06

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,8 +8,11 @@ class SearchBar extends Component {
     return (
       <label htmlFor="select-input" data-testid="select-input-label">
         Filtrar por gênero
-        <select id="select-input" value={selectedGenre}
-        onChange={onSelectedGenreChange} data-testid="select-input"
+        <select
+          id="select-input"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
+          data-testid="select-input"
         >
           <option data-testid="select-option" value="">Todos</option>
           <option data-testid="select-option" value="action">Ação</option>
@@ -30,14 +33,22 @@ class SearchBar extends Component {
       <form data-testid="search-bar-form">
         <label htmlFor="text-input" data-testid="text-input-label">
           Inclui o texto:
-          <input id="text-input" type="text" value={searchText}
-            onChange={onSearchTextChange} data-testid="text-input"
+          <input
+            id="text-input"
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
           />
         </label>
         <label htmlFor="check-input" data-testid="checkbox-input-label">
           Mostrar somente favoritos
-          <input id="check-input" type="checkbox" checked={bookmarkedOnly}
-            onChange={onBookmarkedChange} data-testid="checkbox-input"
+          <input
+            id="check-input"
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
           />
         </label>
         {this.renderSelect()}
