e427a777671d93ed097a64f609cda16d6f908f6a
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-17 16:33:43 -0300

    Renderizado um select com quatro options dentro do componente SearchBar.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,6 +23,20 @@ class SearchBar extends React.Component {
           checked={this.props.bookmarkedOnly}
           />
         </label>
+        <label data-testid="select-input-label" htmlFor="">
+          <select
+          data-testid="select-input"
+          name="select-input"
+          id="select-input"
+          value={this.props.selectedGenre}
+          onChange={this.props.onSelectedGenreChange}
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
       </form>
     )
   }
