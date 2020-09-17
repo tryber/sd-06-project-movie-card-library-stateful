618dce6046e8d5ce97228cba42758d2caa3de257
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-17 16:10:42 -0300

    Add input to checkbox

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,8 +6,12 @@ class SearchBar extends Component {
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label">Inclui o texto:</label>
-          <input data-testid="text-input" value={searchText} onChange={onSearchTextChange} />
+          <label data-testid="text-input-label">Inclui o texto:
+            <input data-testid="text-input" value={searchText} onChange={onSearchTextChange} />
+          </label>
+          <label data-testid="checkbox-input-label">Mostrar somente favoritos
+            <input data-testid="checkbox-input" />
+          </label>
         </form>
       </div>
     );
