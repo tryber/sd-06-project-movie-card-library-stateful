e931e724226883eeb2e25a622267084dbe53ec3c
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-17 15:53:38 -0300

    Add value props in input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    const { searchText } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
           <label data-testid="text-input-label">Inclui o texto:</label>
-          <input data-testid="text-input" />
+          <input data-testid="text-input" value={searchText} />
         </form>
       </div>
     );
