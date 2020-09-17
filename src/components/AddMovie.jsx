1cd47583456f475deb1287a90e679caf95545698
Cyrano webdev <cyrano@cyranowebdev.com>
2020-09-17 19:21:10 -0300

    refat addMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import AddInput from './AddInput';
+import AddSelect from './AddSelect';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -13,10 +14,13 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.handleTextInput = this.handleTextInput.bind(this);
+    this.callbackHandler = this.callbackHandler.bind(this);
   }
 
-  handleTextInput({target}) {
-    this.setState({[target.id]: target.value});
+  handleTextInput({ target }) {
+    this.setState({ [target.id]: target.value });
   }
 
   callbackHandler() {
@@ -35,23 +39,38 @@ class AddMovie extends React.Component {
     return (
       <form data-testid="add-movie-form">
         <fieldset>
-          <AddInput id="title" label="Título" inputType="text" testid="title-input" callback={this.handleTextInput} value={this.state.title} />
-          <AddMovieInput id="subtitle" label="Subtítulo" inputType="text" testid="subtitle-input" callback={this.handleTextInput} value={this.state.subtitle} />
-          <AddInput id="imagePath" label="Imagem" inputType="text" testid="image-input" callback={this.handleTextInput} value={this.state.imagePath} />
+          <AddInput
+            id="title" label="Título" inputType="text" testid="title-input"
+            callback={this.handleTextInput} value={this.state.title}
+          />
+          <AddInput
+            id="subtitle" label="Subtítulo" inputType="text" testid="subtitle-input"
+            callback={this.handleTextInput} value={this.state.subtitle}
+          />
+          <AddInput
+            id="imagePath" label="Imagem" inputType="text" testid="image-input"
+            callback={this.handleTextInput} value={this.state.imagePath}
+          />
         </fieldset>
         <fieldset>
           <label data-testid="storyline-input-label" htmlFor="storyline">Sinopse
-            <textarea id="storyline" value={this.state.storyline} data-testid="storyline-input" onChange={this.handleTextInput} />
+            <textarea
+              id="storyline" value={this.state.storyline} data-testid="storyline-input"
+              onChange={this.handleTextInput}
+            />
           </label>
         </fieldset>
         <fieldset>
-          <AddInput id="rating" label="Avaliação" inputType="number" testid="rating-input" callback={this.handleTextInput} value={this.state.rating} />
-          <label data-testid="genre-input-label" htmlFor="genre">Gênero</label>
-            <select id="genre" value={this.state.genre} onChange={this.handleTextInput} data-testid="genre-input">
-              <option data-testid="genre-option" value="action">Ação</option>
-              <option data-testid="genre-option" value="comedy">Comédia</option>
-              <option data-testid="genre-option" value="thriller">Suspense</option>
-            </select>
+          <AddInput
+            id="rating" label="Avaliação" inputType="number" testid="rating-input"
+            callback={this.handleTextInput} value={this.state.rating}
+          />
+          <AddSelect
+            id="genre" value={this.state.genre} callback={this.handleTextInput}
+            label="Gênero" testid="genre-input" optionId="genre-option" options={[
+              ['action', 'Ação'], ['comedy', 'Comédia'], ['thriller', 'Suspense'],
+            ]}
+          />
         </fieldset>
         <button type="button" data-testid="send-button" onClick={this.callbackHandler}>Adicionar filme</button>
       </form>
