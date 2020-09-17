5d043aca84cefda07fbf5620a89d9a1d07ad3796
Ana Flávia <anaflvia@gmail.com>
2020-09-17 20:54:30 -0300

    j

--- src/components/AddMovie.test.js
+++ src/components/AddMovie.test.js
@@ -61,7 +61,7 @@ describe('Verifica o componente <AddMovie />', () => {
   });
 });
 
-describe.only('Verifica o input de título do componente <AddMovie />', () => {
+describe('Verifica o input de título do componente <AddMovie />', () => {
   it('Será validado se o component renderiza um input de texto para quem usa escrever o titulo do filme', () => {
     expect(titleInput).toBeInTheDocument();
   });
