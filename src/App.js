b64e714125e28f47fdd844649fddb25a7a3497ce
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-17 17:41:56 -0300

    Requisito SearcBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import SearchBar from './components/SearchBar';
 import './App.css';
-
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar
+        searchText={props.searchText}
+        onSearchTextChange={props.onSearchTextChange}
+        bookmarkedOnly={props.bookmarkedOnly}
+        onBookmarkedChange={props.onBookmarkedChange}
+        selectedGenre={props.selectedGenre}
+        onSelectedGenreChange={props.onSelectedGenreChange}
+      />
     </div>
   );
 }
