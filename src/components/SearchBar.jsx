71ec6fef2bc8a7d080827039c91388e3167766e9
Matheuscara <matheuscara2000@gmail.com>
2020-09-17 17:30:29 -0300

    Code-Climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,62 +7,20 @@ class SearchBar extends Component {
   // onBookmarkedChange, uma callback
   // selectedGenre, uma string
   // onSelectedGenreChange, uma callback
-
   render() {
-    const {
-      searchText,
-      onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
-    } = this.props;
-
+    const {searchText,onSearchTextChange,bookmarkedOnly,onBookmarkedChange,selectedGenre,onSelectedGenreChange,} = this.props;
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label">
-          Inclui o texto:
-          <input
-            data-testid="text-input"
-            type="text"
-            name="searchText"
-            value={searchText}
-            onChange={onSearchTextChange}
-            maxLength="40"
-            required="required"
-          ></input>
-        </label>
-
-        <label data-testid="checkbox-input-label">
-          Mostrar somente favoritos
-          <input
-            data-testid="checkbox-input"
-            type="checkbox"
-            name="name"
-            checked={bookmarkedOnly}
-            onChange={onBookmarkedChange}
-          />
-        </label>
-
-        <label data-testid="select-input-label">
-          Filtrar por gênero
-          <select
-            data-testid="select-input"
-            value={selectedGenre}
-            onChange={onSelectedGenreChange}
-          >
-            <option data-testid="select-option" value="">
-              Todos
-            </option>
-            <option data-testid="select-option" value="action">
-              Ação
-            </option>
-            <option data-testid="select-option" value="comedy">
-              Comédia
-            </option>
-            <option data-testid="select-option" value="thriller">
-              Suspense
-            </option>
+        <label data-testid="text-input-label">Inclui o texto:
+          <input data-testid="text-input" type="text" name="searchText" value={searchText} onChange={onSearchTextChange}maxLength="40"required="required"></input></label>
+        <label data-testid="checkbox-input-label">Mostrar somente favoritos
+          <input data-testid="checkbox-input" type="checkbox" name="name" checked={bookmarkedOnly} onChange={onBookmarkedChange}/></label>
+        <label data-testid="select-input-label">Filtrar por gênero
+          <select data-testid="select-input"value={selectedGenre}onChange={onSelectedGenreChange}>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
           </select>
         </label>
       </form>
