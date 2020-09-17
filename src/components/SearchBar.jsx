34863612fa1f83b2ccd1e28b57db25db39dfabaa
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-17 19:24:10 -0300

    sfgsdfh

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
-      selectedGenre, onSelectedGenreChange } = this.props;
+    const { 
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange
+    } = this.props;
     return (
       <form data-testid="search-bar-form" >
         <label data-testid="text-input-label" htmlFor="text-input">Inclui o texto:</label>
