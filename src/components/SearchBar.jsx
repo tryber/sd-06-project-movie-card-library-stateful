776b1b5478364a3d966754a403cadc9f91b3eb5c
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-17 18:51:28 -0300

    Select Input_fixing CC issues 05

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,14 +16,15 @@ class SearchBar extends React.Component {
     return (
       <form data-testid="search-bar-form">
 
-        <label data-testid="text-input-label">Inclui o texto:</label>
+        <label htmlFor="text-input" data-testid="text-input-label">Inclui o texto:</label>
         <input type="text" value={this
         .props
         .searchText} onChange={this
         .props
-        .onSearchTextChange} data-testid="text-input" />
+        .onSearchTextChange} data-testid="text-input" 
+        />
 
-        <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
         <input type="checkbox" data-testid="checkbox-input" checked={this
           .props
           .bookmarkedOnly} onChange={this
@@ -31,8 +32,12 @@ class SearchBar extends React.Component {
           .onBookmarkedChange}
         />
 
-        <label data-testid="select-input-label">Filtrar por gênero</label>
-        <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange} data-testid="select-input">
+        <label htmlFor="select-option" data-testid="select-input-label">Filtrar por gênero</label>
+        <select value={this
+          .props
+          .selectedGenre} onChange={this
+          .props
+          .onSelectedGenreChange} data-testid="select-input">
           <option value="" data-testid="select-option">Todos</option>
           <option value="action" data-testid="select-option">Ação</option>
           <option value="comedy" data-testid="select-option">Comédia</option>
