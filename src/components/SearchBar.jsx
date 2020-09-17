0fb6e36c9da92a94664be0d269d98751ba236bf5
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-17 18:35:58 -0300

    Criando SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,17 @@
 // implement SearchBar component here
-// import React, { Component } from 'react';
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import React, { Component } from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
-// class SearchBar extends Component {
+class SearchBar extends Component {
  
-//   render() {
-//     return (
-//       <div></div>
-//     );
-//   }
-// }
+  render() {
+    
+    return (
+      <div></div>
+    );
+  }
+}
 
-// export default SearchBar;
+export default SearchBar;
