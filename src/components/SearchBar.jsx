499f771c93d338a1c6a4cdae76791f75da54a5ca
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-17 17:21:28 -0300

    1rst requirement_fixing CC issues

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,24 +3,19 @@ import React from 'react';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ""}
+    this.state = { value: '' };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }  
-
+  handleChange(event) { this.setState({value: event.target.value}); }
   render() {
     return (
       <form>
         <input type="text" value={this.state.value} onChange={this.handleChange} />
         <input type="checkbox" />
       </form>
-    )
+    );
   }
 }
-
-
 
 export default SearchBar;
