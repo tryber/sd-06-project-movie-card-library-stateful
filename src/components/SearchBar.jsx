af993c4365375425f602a22be63570953bcab2f9
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-17 17:21:10 -0300

    Add id option

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,10 +17,10 @@ class SearchBar extends Component {
           <label data-testid="select-input-label" htmlFor="select-input">
             Filtrar por gênero
             <select data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
-              <option value="">Todos</option>
-              <option value="action">Ação</option>
-              <option value="comedy">Comédia</option>
-              <option value="thriller">Suspense</option>
+              <option data-testid="select-option" value="">Todos</option>
+              <option data-testid="select-option" value="action">Ação</option>
+              <option data-testid="select-option" value="comedy">Comédia</option>
+              <option data-testid="select-option" value="thriller">Suspense</option>
             </select>
           </label>
         </form>
