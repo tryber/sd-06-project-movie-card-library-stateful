0ffd9115c6e168d76f79308966e016b2f993e832
Thales Rangel <thalesgr@gmail.com>
2020-09-17 15:40:00 -0300

    searchBar feito

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,39 @@
 // implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super()
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: '',
+    };
+  };
+
+  handleChange(e) {
+    const { name , value } = e.target;
+    this.setState({
+        [name]: value,
+    })
+  };
+
+  render() {
+    const { onClick } = this.props;
+    return (
+      <form>
+        <label data-testid="title-input-label">
+          Titulo
+          <input value={this.state.title}
+          data-testid="title-input"
+          name="title"
+          onChange={this.handleChange}/>
+        </label>
+      </form>
+    )
+  }
+};
