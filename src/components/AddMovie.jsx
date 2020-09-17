35fb500674d3e3ed7c2c7555c7516082b2f8389a
Thales Rangel <thalesgr@gmail.com>
2020-09-17 16:29:43 -0300

    testando evaluator

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -23,7 +23,7 @@ class AddMovie extends React.Component {
   };
 
   render() {
-    const { onClick } = this.props;
+    const onClick  = this.props;
     return (
       <form>
         <label data-testid="title-input-label">
@@ -31,9 +31,63 @@ class AddMovie extends React.Component {
           <input value={this.state.title}
           data-testid="title-input"
           name="title"
-          onChange={this.handleChange}/>
+          onChange={this.handleChange}
+          type="text"
+          />
         </label>
+        <label data-testid="subtitle-input-label">
+          Subtítulo
+          <input value={this.state.subtitle}
+          data-testid="subtitle-input"
+          name="subtitle"
+          onChange={this.handleChange}
+          type="text"
+          />
+        </label>
+        <label data-testid="image-input-label">
+          Imagem
+          <input value={this.state.imagePath}
+          data-testid="image-input"
+          name="imagePath"
+          onChange={this.handleChange}
+          type="text"
+          />
+        </label>
+        <label data-testid="storyline-input-label">
+          Sinopse
+          <textarea value={this.state.storyline}
+          data-testid="storyline-input"
+          name="storyline"
+          onChange={this.handleChange}
+          />
+        </label>
+        <label data-testid="rating-input-label">
+          Avaliação
+          <input value={this.state.rating}
+          data-testid="rating-input"
+          name="rating"
+          onChange={this.handleChange}
+          type="number"
+          />
+        </label>
+        <label data-testid="genre-input-label">
+          Gênero
+          <select value={this.state.genre} 
+          data-testid="genre-input" 
+          name="genre" 
+          onChange={this.handleChange}
+          >
+            <option value="action" data-testid="genre-option">Ação</option>    
+            <option value="comedy" data-testid="genre-option">Comédia</option>    
+            <option value="thriller" data-testid="genre-option">Suspense</option>    
+          </select>
+        </label>
+        <button data-testid="send-button"
+        onClick={onClick}
+        >Adicionar filme</button>
       </form>
     )
   }
 };
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,26 @@
 // implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    }
+  };
+  render() {
+    const movies = this.props;
+    return (
+      <SearchBar
+      searchText={this.state.searchText}
+      bookmarkedOnly={this.state.bookmarkedOnly}
+      />
+    ) 
+  }
+}
