d44cb83bde2fed49c75f813fbcb052f259fb18ef
Ana Higo <anahigo.arq@gmail.com>
2020-09-17 16:43:40 -0300

    SearchBar, teste ok! codeclimate, again?

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,26 +1,29 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const { 
-      searchText, 
-      onSearchTextChange, 
-      bookmarkedOnly, 
-      onBookmarkedChange, 
-      selectedGenre, 
-      onSelectedGenreChange 
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
     } = this.props;
 
     return (
       <div>
         <form data-testid="search-bar-form">
           <label data-testid="text-input-label"> Inclui o texto:
-            <input data-testid="text-input" type="text"
+            <input data-testid="text-input" 
+              type="text"
               value={searchText} onChange={onSearchTextChange}
             />
           </label>
           <label data-testid="checkbox-input-label">Mostrar somente favoritos
-            <input data-testid="checkbox-input" type="checkbox"
+            <input data-testid="checkbox-input" 
+              type="checkbox"
               checked={bookmarkedOnly} onChange={onBookmarkedChange}
             />
           </label>
@@ -42,11 +45,11 @@ class SearchBar extends Component {
 
 SearchBar.propTypes = {
   searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.callback, 
-  bookmarkedOnly: PropTypes. boolean, 
-  onBookmarkedChange: PropTypes.callback,
-  selectedGenre: PropTypes.string, 
-  onSelectedGenreChange: PropTypes.callback,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
 };
 
 export default SearchBar;
