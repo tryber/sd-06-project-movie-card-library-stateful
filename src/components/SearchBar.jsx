0e16444d0cd71be4a274ebf1d670f03382ba153c
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-09-17 19:01:52 -0300

    Fixing CFC

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,39 +10,51 @@ class SearchBar extends Component {
     return (
       <fieldset data-testid="search-bar-form">
         <div className="text-container">
-          <label data-testid="text-input-label">Inclui o texto</label>
-          <input data-testid="text-input" onChange={onSearchTextChange} value={searchText} />
-        </div>
-
-        <div className="checkbox-container">
-          <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+          <label htmlFor="filter" data-testid="text-input-label">Inclui o texto</label>
           <input
-            data-testid="checkbox-input" type="checkbox"
-            onChange={onBookmarkedChange} checked={bookmarkedOnly}
+            name="filter" id="filter" data-testid="text-input"
+            onChange={onSearchTextChange} value={searchText}
           />
         </div>
 
         <div className="checkbox-container">
-          <label data-testid="select-input-label">Filtrar por gênero</label>
-          <select data-testid="select-input" onChange={onSelectedGenreChange} value={selectedGenre}>
-            <option data-testid="select-option" value="">Todos</option>
-            <option data-testid="select-option" value="action">Ação</option>
-            <option data-testid="select-option" value="comedy">Comédia</option>
-            <option data-testid="select-option" value="thriller">Suspense</option>
-          </select>
+          <label htmlFor="check" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+          <input
+            name="check" id="check" data-testid="checkbox-input" type="checkbox"
+            onChange={onBookmarkedChange} checked={bookmarkedOnly}
+          />
         </div>
+        < Select onSelectedGenreChange={onSelectedGenreChange} selectedGenre={selectedGenre} />
       </fieldset>
     );
   }
 }
 
+class Select extends Component {
+  render() {
+    const { onSelectedGenreChange, selectedGenre } = this.props;
+
+    return (
+      <div className="checkbox-container">
+        <label data-testid="select-input-label">Filtrar por gênero</label>
+        <select data-testid="select-input" onChange={onSelectedGenreChange} value={selectedGenre}>
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </div>
+    )
+  }
+}
+
 SearchBar.propTypes = {
   onSearchTextChange: PropTypes.func.isRequired,
-  searchText: PropTypes.string.isRequired,
   onBookmarkedChange: PropTypes.func.isRequired,
-  bookmarkedOnly: PropTypes.bool.isRequired,
   onSelectedGenreChange: PropTypes.func.isRequired,
+  searchText: PropTypes.string.isRequired,
   selectedGenre: PropTypes.string.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
 };
 
 export default SearchBar;
