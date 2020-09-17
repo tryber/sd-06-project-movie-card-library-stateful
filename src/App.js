dfb129d2a96d7bb6725477d7510e7dbb1ec6d8d8
Zeca Costa <jccostaso@gmail.com>
2020-09-17 16:38:38 -0300

    commit inicial App.js + Header.jsx + SearchBar.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-
+import movies from './data.js';
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
     </div>
   );
 }
