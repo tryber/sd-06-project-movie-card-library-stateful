89f9480ff04782cc5467698f1878c81e3399582d
FlavioSugano <flavio.sugano@gmail.com>
2020-09-17 17:52:33 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import movies from './data';
+import MovieLibrary from './components/MovieLibrary';
+import MovieList from './components/MovieList';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieLibrary />
+        <MovieList movies={movies}/>
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,11 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>Essa div é do MovieLibrary</div>
+    )
+  }
+}
+
+export default MovieLibrary;
