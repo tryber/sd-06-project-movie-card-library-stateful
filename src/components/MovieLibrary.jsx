062581e62d0393359196dd0430fd4b03567f755d
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-09-17 16:53:48 -0300

    Retirado comentário no arquivo index.html e comentado algumas linhas de comando que não serão usadas agora, além disso foi implementado o código do componente SearchBar para renderizar uma caixa de texto

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-import MovieList from './MovieList';
+// import MovieList from './MovieList';
 import SearchBar from './SearchBar';
-import AddMovie from './AddMovie';
+// import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
   constructor(props) {
@@ -14,8 +14,8 @@ class MovieLibrary extends Component {
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie />
+        {/* <MovieList movies={this.props.movies} />
+        <AddMovie /> */}
       </div>
     );
   }
