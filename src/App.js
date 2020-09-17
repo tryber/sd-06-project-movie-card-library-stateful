25a5f8d9abd1fc6b7e85386dbc80dbb0694314ef
Anderson <andersoni.trybe@gmail.com>
2020-09-17 20:03:48 -0300

    CodeClimate test

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import './App.css';
-
+import PropTypes from 'prop-types'
 import Header from './components/Header';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  // const { search } = this.props;
   return (
     <div className="App">
       <Header />
+      <SearchBar 
+        /* searchText={search.searchText} 
+        onSearchTextChange={search.onSearchTextChange} 
+        bookmarkedOnly={search.bookmarkedOnly}
+        onBookMarkedOnly={search.onBookMarkedOnly}
+        selectedGenre={search.selectedGenre}
+        onSelectedGenre={search.onSelectedGenre} */
+        />
     </div>
   );
 }
 
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookMarkedOnly: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenre: PropTypes.func,
+}
+
 export default App;
-//upload
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,21 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div className="search-bar">
+                <section className="form" data-testid="search-bar-form">
+                    <label data-testid="text-input-label">Inclui o texto:</label>
+                    <input  data-testid="text-input"
+                    type="text" value="searchText" onchange="onSearchChange" />
+                    <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+                    <input type="checkbox" data-testid="checkbox-input" 
+                    checked="bookMarkedOnly" onchange="onBookMarkedChange" />
+                </section>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
