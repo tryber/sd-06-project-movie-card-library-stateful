a05573e016e78c8827698688be07fb5498e4e2bf
HCaetano <hrcgeo@gmail.com>
2020-09-17 15:52:06 -0300

    Add 'render a select input inside of the form' requirement

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange, } = this.props;
 
-    return <form data-testid="search-bar-form">
-      <label htmlFor="containsTextFilter" data-testid="text-input-label">Inclui o texto:</label>
-      <input type="text" name="containsTextFilter" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
-      <label htmlFor="showFavorites" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
-      <input type="checkbox" name="showFavorites" checked={bookmarkedOnly} onChange={onBookmarkedChange} data-testid="checkbox-input" />
-    </form>;
+    return (
+      <form data-testid="search-bar-form">
+        <label htmlFor="containsTextFilter" data-testid="text-input-label">Inclui o texto:</label>
+        <input type="text" name="containsTextFilter"
+          value={searchText}
+          onChange={onSearchTextChange}
+          data-testid="text-input"/>
+        <label htmlFor="showFavorites" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+        <input type="checkbox" name="showFavorites"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+          data-testid="checkbox-input"/>
+        <label htmlFor="genre" data-testid="select-input-label">Filtrar por gênero</label>
+        <select name="genre" value={selectedGenre}
+          onChange={onSelectedGenreChange}
+          data-testid="select-input">
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </form>
+    );
   }
 }
 
