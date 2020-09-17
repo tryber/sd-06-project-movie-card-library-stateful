6d9c1eeac9b084358f137f0f3ea458a69a16fba9
Ana Flávia <anaflvia@gmail.com>
2020-09-17 19:15:33 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import movies from './data';
-import AddMovie from './components/AddMovie'
+import AddMovie from './components/AddMovie';
 import SearchBar from './components/SearchBar';
 
 function App() {
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -10,8 +10,8 @@ class AddMovie extends React.Component {
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action'
-    }
+      genre: 'action',
+    };
     this.refreshTitle = this.refreshTitle.bind(this);
     this.refreshSubtitle = this.refreshSubtitle.bind(this);
     this.refreshImage = this.refreshImage.bind(this);
@@ -22,27 +22,27 @@ class AddMovie extends React.Component {
   }
 
   refreshTitle(event) {
-    this.setState({title: event.target.value})
+    this.setState({ title: event.target.value });
   }
 
   refreshSubtitle(event) {
-    this.setState({subtitle: event.target.value})
+    this.setState({ subtitle: event.target.value });
   }
 
   refreshImage(event) {
-    this.setState({imagePath: event.target.value})
+    this.setState({ imagePath: event.target.value });
   }
 
   refreshStoryline(event) {
-    this.setState({storyline: event.target.value})
+    this.setState({ storyline: event.target.value });
   }
 
   refreshRating(event) {
-    this.setState({rating: event.target.value})
+    this.setState({ rating: event.target.value });
   }
 
   refreshGenre(event) {
-    this.setState({genre: event.target.value})
+    this.setState({ genre: event.target.value });
   }
 
   button() {
@@ -54,8 +54,8 @@ class AddMovie extends React.Component {
       imagePath: '',
       storyline: '',
       rating: 0,
-      genre: 'action'
-    })
+      genre: 'action',
+    });
   }
 
   render() {
@@ -63,7 +63,10 @@ class AddMovie extends React.Component {
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title" >
           Título
-          <input type="text" name="title" value={this.state.title} data-testid="title-input" onChange={this.refreshTitle} id="title" />
+          <input
+            type="text"
+            name="title" value={this.state.title} data-testid="title-input" onChange={this.refreshTitle} id="title"
+          />
         </label><br /><br />
         <label data-testid="subtitle-input-label" htmlFor="subtitle">
           Subtítulo
@@ -75,11 +78,17 @@ class AddMovie extends React.Component {
         </label><br /><br />
         <label data-testid="storyline-input-label" htmlFor="storyline">
           Sinopse
-          <textarea id="storyline" name="storyline" data-testid="storyline-input" onChange={this.refreshStoryline} value={this.state.storyline} />
+          <textarea
+            id="storyline" name="storyline"
+            data-testid="storyline-input" onChange={this.refreshStoryline} value={this.state.storyline}
+          />
         </label><br /><br />
         <label data-testid="rating-input-label" htmlFor="rating">
         Avaliação
-          <input type="number" id="rating" name="rating" value={this.state.rating} data-testid="rating-input" onChange={this.refreshRating} />
+          <input
+            type="number" id="rating" name="rating"
+            value={this.state.rating} data-testid="rating-input" onChange={this.refreshRating}
+          />
         </label><br /><br />
         <label data-testid="genre-input-label" htmlFor="genre">
           Gênero
@@ -95,8 +104,6 @@ class AddMovie extends React.Component {
   }
 }
 
-AddMovie.propType = {
-  onClick: PropTypes.func.isRequired
-}
+AddMovie.propType = { onClick: PropTypes.func.isRequired };
 
 export default AddMovie;
