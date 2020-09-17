1bd83de12b9e15ad2a5e6dedd7309cff7d441842
Micaeli Teixeira <luime6@gmail.com>
2020-09-17 19:22:29 -0300

    Implementando o arquivo SearchBar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import SearchBar from './components/SearchBar'
+import MovieList from './components/MovieList';
+import movies from './data';
 import './App.css';
 
 import Header from './components/Header';
@@ -7,6 +10,8 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar /> 
+      <MovieList movies={movies}/>
     </div>
   );
 }
