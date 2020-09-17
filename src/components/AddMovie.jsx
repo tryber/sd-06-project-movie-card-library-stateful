5635e9f98b32eb5a9132ec31376097ce1fcaa897
Alvaro Neto <alvaroshn@gmail.com>
2020-09-17 18:45:08 -0300

    Code Climate [4]

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -30,38 +30,55 @@ class AddMovie extends Component {
       <form htmlFor="this-form" data-testid="add-movie-form">
         <label htmlFor="this-form" data-testid="title-input-label">
           Título
-          <input data-testid="title-input" type="text" name="title"
-            onChange={this.handleChange} value={this.state.title}
+          <input data-testid="title-input"
+            type="text"
+            name="title"
+            onChange={this.handleChange}
+            value={this.state.title}
           />
         </label>
         <label htmlFor="this-form" data-testid="subtitle-input-label">
         Subtítulo
-          <input data-testid="subtitle-input" onChange={this.handleChange}
-            value={this.state.subtitle} type="text" name="subtitle"
+          <input data-testid="subtitle-input"
+            onChange={this.handleChange}
+            value={this.state.subtitle}
+            type="text"
+            name="subtitle"
           />
         </label>
         <label htmlFor="this-form" data-testid="image-input-label">
         Imagem
-          <input data-testid="image-input" type="text" name="imagePath"
-            onChange={this.handleChange} value={this.state.imagePath}
+          <input data-testid="image-input"
+            type="text"
+            name="imagePath"
+            onChange={this.handleChange}
+            value={this.state.imagePath}
           />
         </label>
         <label htmlFor="this-form" data-testid="storyline-input-label">
           Sinopse
-          <textarea data-testid="storyline-input" type="text" name="storyline"
-            value={this.state.storyline} onChange={this.handleChange}
+          <textarea data-testid="storyline-input"
+            type="text"
+            name="storyline"
+            value={this.state.storyline}
+            onChange={this.handleChange}
           />
         </label>
         <label htmlFor="this-form" data-testid="rating-input-label">
           Avaliação
-          <input data-testid="rating-input" onChange={this.handleChange}
-            value={this.state.rating} type="number" name="rating"
+          <input data-testid="rating-input"
+            onChange={this.handleChange}
+            value={this.state.rating}
+            type="number"
+            name="rating"
           />
         </label>
         <label htmlFor="this-form" data-testid="genre-input-label">
           Gênero
-          <select value={this.state.genre} data-testid="genre-input"
-            onChange={this.handleChange} name="genre">
+          <select value={this.state.genre}
+            data-testid="genre-input"
+            onChange={this.handleChange}
+            name="genre">
             <option data-testid="genre-option" value="action">Ação</option>
             <option data-testid="genre-option" value="comedy">Comédia</option>
             <option data-testid="genre-option" value="thriller">Suspense</option>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,14 +10,18 @@ class SearchBar extends Component {
       <form data-testid="search-bar-form" htmlFor="my-form">
         <label htmlFor="my-form" data-testid="text-input-label">
           Inclui o texto:
-          <input type="text" data-testid="text-input"
-            value={searchText} onChange={onSearchTextChange}
+          <input type="text"
+            data-testid="text-input"
+            value={searchText}
+            onChange={onSearchTextChange}
           />
         </label>
         <label htmlFor="my-form" data-testid="checkbox-input-label">
           Mostrar somente favoritos
-          <input type="checkbox" data-testid="checkbox-input"
-            checked={bookmarkedOnly} onChange={onBookmarkedChange}
+          <input type="checkbox"
+            data-testid="checkbox-input"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
           />
         </label>
         <label htmlFor="my-form" data-testid="select-input-label">
