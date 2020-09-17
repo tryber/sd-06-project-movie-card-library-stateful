5bea5f98af700a99c30e7691ae02dbd885a3b2ba
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-17 15:05:37 -0300

    React(ing) to my despair :sparkles:

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,15 @@
 // implement SearchBar component here
+import React from 'react';
+
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+          Hello, World!
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
