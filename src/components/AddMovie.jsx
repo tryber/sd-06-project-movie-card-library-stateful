73396a7ddd74f1428088d3def9d7e9dd7c4d54b2
wfelipefarina <wfelipefarina@gmail.com>
2020-09-17 15:58:00 -0300

    Iniciando o projeto Movie Cards Stateful

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,9 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+    render() {
+        return(<div>oi</div>)
+    }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,9 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+    render() {
+        return (<div>oi</div>)
+    }
+}
+
+export default MovieLibrary;
