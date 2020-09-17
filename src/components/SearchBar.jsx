beb23a983d2e3c732ffdde1bb3193bcbc7dcb4b3
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-17 19:46:29 -0300

    requisito AddMovie completo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,29 +3,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
+  renderSelect() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label htmlFor="select" data-testid="select-input-label">
+      Filtrar por gênero
+        <select value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input">
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
-    const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
           <label htmlFor="searchtext" data-testid="text-input-label">
             Inclui o texto:
-            <input id="searchtext" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
+            <input value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
           </label>
           <label htmlFor="checkbox" data-testid="checkbox-input-label">
             Mostrar somente favoritos
-            <input id="checkbox" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} data-testid="checkbox-input" />
+            <input
+              type="checkbox"
+              checked={bookmarkedOnly}
+              onChange={onBookmarkedChange}
+              data-testid="checkbox-input"
+            />
           </label>
-          <label htmlFor="select" data-testid="select-input-label">
-            Filtrar por gênero
-            <select id="select" value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input">
-              <option data-testid="select-option" value="">Todos</option>
-              <option data-testid="select-option" value="action">Ação</option>
-              <option data-testid="select-option" value="comedy">Comédia</option>
-              <option data-testid="select-option" value="thriller">Suspense</option>
-            </select>
-          </label>
+          {this.renderSelect()}
         </form>
       </div>
     );
