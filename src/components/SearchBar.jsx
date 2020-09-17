528ba1a454d17a2da6bad701fdeaa91776e8a7be
LeandroParisi <lepcbelisario@gmail.com>
2020-09-17 15:40:10 -0300

    SearchBar Describe 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,8 @@ class SearchBar extends React.Component {
   render () {
     return (
       <form data-testid="search-bar-form">
-        
+        <label data-testid="text-input-label">Inclui o texto:</label>
+        <input value={this.props.searchText} onChange={this.props.onSearchTextChange} data-testid="text-input" />
       </form>
     )
   }
