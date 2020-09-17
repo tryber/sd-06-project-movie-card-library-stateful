179e9d488d4be80973dfd1655826c7610fb1d46e
LeandroParisi <lepcbelisario@gmail.com>
2020-09-17 15:56:00 -0300

    SearchBar Describe 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,6 +9,14 @@ class SearchBar extends React.Component {
 
         <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
         <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange} data-testid="checkbox-input"></input>
+
+        <label data-testid="select-input-label">Filtrar por gênero</label>
+        <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange} data-testid="select-input">
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option> 
+        </select>
       </form>
     )
   }
