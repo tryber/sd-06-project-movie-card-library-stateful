3a8fc942411cbb47d1a4a33621d63d494693c047
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-17 17:42:43 -0300

    issues req SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -28,7 +28,7 @@ class SearchBar extends Component {
             <option data-testid="select-option" value="" >Todos</option>
             <option data-testid="select-option" value="action" >Ação</option>
             <option data-testid="select-option" value="comedy" >Comédia</option>
-            <option data-testid="select-option" value="triller" >Suspense</option>
+            <option data-testid="select-option" value="thriller" >Suspense</option>
           </select>
         </label>
       </form>
