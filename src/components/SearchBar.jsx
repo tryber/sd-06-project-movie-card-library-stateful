f88e2fc7dfa6bbb99a3a6285fdb1331ef8b289fa
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-17 16:52:13 -0300

    Rendering SearchBar (still not implemented)

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,17 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
+
+    return <span>Hello World</span>
+  }
+}
+
+export default SearchBar;
