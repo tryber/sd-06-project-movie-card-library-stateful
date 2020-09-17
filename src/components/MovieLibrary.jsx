27c294a18d4ddb76576f953a46de784fd6330234
alcantara2509 <alcantara2509@gmail.com>
2020-09-17 19:30:26 -0300

    pausa

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,15 +7,41 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+
+    this.handleChange = this.handleChange.bind(this)
+    this.bookmarkedChange = this.bookmarkedChange.bind(this)
+    this.selectedGenreChange = this.selectedGenreChange.bind(this)
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies
+    }
+  }
+
+  handleChange({ target }) {
+    this.setState({searchText: target.value})
+  }
+
+  bookmarkedChange() {
+    this.setState({ bookmarkedOnly: true })
+  }
+
+  selectedGenreChange({ target }) {
+    this.setState({ selectedGenre: target.value })
   }
 
   render() {
+    const noClick = () => console.log('oi')
+    console.log(this.props.movies.filter(e => e.title === this.state.searchText))
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
-        <AddMovie />
+        <SearchBar searchText={this.state.searchText} onSearchTextChange={this.handleChange} bookmarkedOnly={this.state.bookmarkedOnly} 
+        onBookmarkedChange={this.bookmarkedChange} selectedGenre={this.state.selectedGenre} onSelectedGenreChange={this.selectedGenreChange} />
+        <MovieList movies={this.props.movies.filter(e => e.title === this.state.searchText)} />
+        <AddMovie onClick={noClick}/>
       </div>
     );
   }
