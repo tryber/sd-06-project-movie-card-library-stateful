64d7adcea78adba851aea4c9df99796dc4e81b77
Elias Carvalho <eliascarvalho01@hotmail.com>
2020-09-17 18:45:52 -0300

    SearchBar Done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar';
+import movies from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+
+    this.state = {
+      filterMovieByName: movies,
+      bookmarkedOnly: false,
+      searchText: '',
+      selectedGenre: '',
+    };
+  }
+
+  onSearchTextChange({ target }) {
+    const movieName = target.value.toLowerCase();
+    const nameFiltered = movies.find(item => (
+      item.title.toLowerCase().split(' ').includes(movieName)
+    ));
+
+    console.log(nameFiltered);
+
+    if (nameFiltered === undefined) {
+      this.setState({ filterMovieByName: movies });
+    } else {
+      this.setState({ filterMovieByName: [nameFiltered] });
+    }
+  }
+
+  onBookmarkedChange({ target }) {
+    if (target.checked === true) {
+      this.setState({
+        bookmarkedOnly: true,
+        filterMovieByName: [movies[0]]
+      });
+    } else {
+      this.setState({
+        bookmarkedOnly: false,
+        filterMovieByName: movies
+      });
+    }
+  }
+
+  onSelectedGenreChange({ target }) {
+    const genreOption = target.value;
+    const genreFiltered = movies.find(item => item.genre === genreOption);
+
+    if (genreFiltered === undefined) {
+      this.setState({ filterMovieByName: movies });
+    } else {
+      this.setState({ filterMovieByName: [genreFiltered] });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar
+          onSearchTextChange={this.onSearchTextChange} searchText={this.state.searchText}
+          onBookmarkedChange={this.onBookmarkedChange} bookmarkedOnly={this.state.bookmarkedOnly}
+          onSelectedGenreChange={this.onSelectedGenreChange} selectedGenre={this.state.selectedGenre} />
+        <MovieList movies={this.state.filterMovieByName} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,48 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class SearchBar extends Component {
+  render() {
+    const { onSearchTextChange, searchText } = this.props;
+    const { onBookmarkedChange, bookmarkedOnly, } = this.props;
+    const { onSelectedGenreChange, selectedGenre } = this.props;
+
+    return (
+      <fieldset data-testid="search-bar-form">
+        <div className="text-container">
+          <label htmlFor="filter" data-testid="text-input-label">Inclui o texto</label>
+          <input name="filter" id="filter" data-testid="text-input" type="text"
+            onChange={onSearchTextChange} value={searchText} />
+        </div>
+
+        <div className="checkbox-container">
+          <label htmlFor="check" data-testid="checkbox-input-label" >Mostrar somente favoritos</label>
+          <input name="check" id="check" data-testid="checkbox-input" type="checkbox"
+            onChange={onBookmarkedChange} checked={bookmarkedOnly} />
+        </div>
+
+        <div className="checkbox-container">
+          <label htmlFor="gender" data-testid="select-input-label" >Filtrar por gênero</label>
+          <select name="gender" id="gender" data-testid="select-input"
+            onChange={onSelectedGenreChange} value={selectedGenre}>
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </div>
+      </fieldset>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  onSearchTextChange: PropTypes.func,
+  searchText: PropTypes.string,
+  onBookmarkedChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onSelectedGenreChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+};
+
+export default SearchBar;
