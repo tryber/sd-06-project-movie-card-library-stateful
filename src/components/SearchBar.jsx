8d02b7a823d3cfd85bb1d5eb349644d713bedd31
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-17 18:35:08 -0300

    Adequando ao CC - 05

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,7 +9,7 @@ class SearchBar extends Component {
       <label htmlFor="select-input" data-testid="select-input-label">
         Filtrar por gênero
         <select id="select-input" value={selectedGenre}
-          onChange={onSelectedGenreChange} data-testid="select-input"
+        onChange={onSelectedGenreChange} data-testid="select-input"
         >
           <option data-testid="select-option" value="">Todos</option>
           <option data-testid="select-option" value="action">Ação</option>
