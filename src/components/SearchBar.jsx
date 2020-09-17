f8e65480bf7d3aad4bd9f0f31e32c3bbb7345422
kissyla <kissylamottapereira@gmail.com>
2020-09-17 15:43:42 -0300

    primeiro commit projeto movie card library stateful

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,28 @@
-// implement SearchBar component here
+import react, { Component } from 'react';
+import MovieList from './MovieList.jsx';
+
+class SearchBar extends Component {
+  render() {
+    return(
+      <forms data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+        Inclui o texto:
+          <input value={this.searchText} onChange={this.onSearchTextChange} data-testid="text-input" />
+          </label>
+        
+
+      </forms>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  searchText: propTypes.string,
+  onSearchTextChange: propTypes.function,
+  bookmarkedOnly: propTypes.bool,
+  onBookmarkedChange: propTypes.function,
+  selectedGenre: propTypes.function,
+  onSelectedGenreChange: propTypes.function
+}
+
+export default SearchBar;
