a66858ecdc42e0f1a9459cad5d51cf5d3d3335cc
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-17 18:18:21 -0300

    issues vai pf

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,15 +3,26 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
-    const { searchText = '', onSearchTextChange = '', bookmarkedOnly = false, onBookmarkedChange = '', selectedGenre = '', onSelectedGenreChange = '' } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
 
     return (
       <form data-testid="search-bar-form" >
-        <label data-testid="text-input-label" for="text-input">Inclui o texto:</label>
-        <input data-testid="text-input" value={searchText} onChange={onSearchTextChange}/>
-        <label data-testid="checkbox-input-label" for="checkbox-input">Mostrar somente favoritos</label>
-        <input data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
-        <label data-testid="select-input-label" for="select-input" >Filtrar por gênero</label>
+        <label data-testid="text-input-label" htmlFor="text-input">Inclui o texto:</label>
+        <input data-testid="text-input"
+          value={searchText}
+          onChange={onSearchTextChange}
+        />
+        <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+            Mostrar somente favoritos
+        </label>
+        <input data-testid="checkbox-input"
+          type="checkbox"
+          checked={bookmarkedOnly}
+          onChange={onBookmarkedChange}
+        />
+        <label data-testid="select-input-label" htmlFor="select-input" >
+          Filtrar por gênero
+        </label>
         <select data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange} >
           <option data-testid="select-option" value="" >Todos</option>
           <option data-testid="select-option" value="action" >Ação</option>
@@ -22,14 +33,5 @@ class SearchBar extends Component {
     );
   }
 }
-
-SearchBar.propTypes = {
-  searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.func,
-  bookmarkedOnly: PropTypes.bool,
-  onBookmarkedChange: PropTypes.func,
-  selectedGenre: PropTypes.string,
-  onSelectedGenreChange: PropTypes.func,
-};
 
 export default SearchBar;
