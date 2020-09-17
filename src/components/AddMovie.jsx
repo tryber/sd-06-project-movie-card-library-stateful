83cd84e60077888523bd3aae9d8032931310eda3
Anderson R <misterregisslv@gmail.com>
2020-09-17 16:27:05 -0300

    adicionando SerachBar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here!!!
+import React, { Component } from 'react';
+
+class AddMovie extends  Component {
+
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+  render() {
+    const { onClick } = this.props
+    return (
+      <form action="">
+        <label data-testid="title-input-label">
+          Título
+          <input type="text" value={this.state.title} data-testid="title-input" onChange={this.onClick} />
+        </label>
+      </form>
+    )
+  }
+}
+
+export default AddMovie;
