dc7cf82bb96ac3767a03e233a6b7959a0b3c0389
Daniel Moura <darthur.moura@gmail.com>
2020-09-17 18:14:53 -0300

    Rendering SearchBar with form and text input

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -6,15 +6,35 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+
+    this.state = {
+      searchText: "",
+      bookmarkedOnly: false,
+      selectedGenre: "",
+      movies: [],
+    }
+  }
+
+  onSearchTextChange({ target }) {
+    this.setState({
+      searchText: target.value
+    })
   }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          value={this.state.searchText}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          selectedGenre={this.state.selectedGenre}
+          onChange={this.onSearchTextChange}
+        />
         <MovieList movies={this.props.movies} />
-        <AddMovie />
+        {/* <AddMovie /> */}
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,22 @@
-// implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
+
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">
+          <input
+            data-testid="text-input"
+            onChange={onSearchTextChange}
+            type="text"
+            value={searchText}
+          />
+        </label>
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
