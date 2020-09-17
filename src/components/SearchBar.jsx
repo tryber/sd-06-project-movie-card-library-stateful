fecb3950f16f288aceeff85e15c099d6bd8c98c8
Pedro Risso <pmerchiorattor@gmail.com>
2020-09-17 17:40:10 -0300

    searchBar being rendered containing one form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,36 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      serachText: '',
+      onSearchTextChange: this.handleChange(),
+      bookmarkedOnly: true,
+      onBookmarkedChange: this.handleChange(),
+      selectedGenre: '',
+      onSelectedGenreChange: this.handleChange(),
+    }
+  }
+
+  handleChange(event) {
+    this.setState(() => event.target.type === 'checkbox'
+    ? { name: event.target.checked }
+    : { name: event.target.value });
+  }
+
+  render() {
+    return (
+      <form data-testid="search-bar-form">
+        <label>
+          Search:
+          <input type="text" value={this.state.serachText} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+
+export default SearchBar;
