439ee995825b8162ed86372402c990a045dbe2b6
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-17 18:33:36 -0300

    Adequando ao CC - 04

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,9 @@ class SearchBar extends Component {
     return (
       <label htmlFor="select-input" data-testid="select-input-label">
         Filtrar por gênero
-        <select id="select-input" value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input">
+        <select id="select-input" value={selectedGenre}
+          onChange={onSelectedGenreChange} data-testid="select-input"
+        >
           <option data-testid="select-option" value="">Todos</option>
           <option data-testid="select-option" value="action">Ação</option>
           <option data-testid="select-option" value="comedy">Comédia</option>
@@ -18,11 +20,11 @@ class SearchBar extends Component {
     );
   }
   render() {
-    const { 
+    const {
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
-      onBookmarkedChange
+      onBookmarkedChange,
     } = this.props;
     return (
       <form data-testid="search-bar-form">
