81a4fe2bd6349a81d7cbc2b37e64de3aa16e699e
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2020-09-17 20:15:19 -0300

    Updating Files Again

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom';
 
 import Header from './Header';
 
-describe.skip('Verifica o componente <Header />', () => {
+describe('Verifica o componente <Header />', () => {
   it('renders without crashing', () => {
     render(<Header />);
   });
