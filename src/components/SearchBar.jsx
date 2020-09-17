5739f93a5765577a41f7b28916051b316ab21262
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-17 15:59:31 -0300

    Add onChange function

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const { searchText } = this.props;
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
           <label data-testid="text-input-label">Inclui o texto:</label>
-          <input data-testid="text-input" value={searchText} />
+          <input data-testid="text-input" value={searchText} onChange={onSearchTextChange} />
         </form>
       </div>
     );
