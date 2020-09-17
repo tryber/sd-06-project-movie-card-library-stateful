eaaccc4f5375a5e86aad3de0add1aec0a5f283a6
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-17 20:49:25 -0300

    Select Input_fixing CC issues 11

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -30,36 +30,39 @@ class SearchBar extends React.Component {
 
   renderCheckBox() {
     return(
-      <label
-        htmlFor="checkbox-input"
-        data-testid="checkbox-input-label">Mostrar somente favoritos
-      </label>
-
-      <input
-        type="checkbox"
-        data-testid="checkbox-input"
-        checked={this.props.bookmarkedOnly}
-        onChange={this.props.onBookmarkedChange}
-      />
+      <div>
+        <label
+          htmlFor="checkbox-input"
+          data-testid="checkbox-input-label">Mostrar somente favoritos
+        </label>
+        <input
+          type="checkbox"
+          data-testid="checkbox-input"
+          checked={this.props.bookmarkedOnly}
+          onChange={this.props.onBookmarkedChange}
+        />
+      </div>
     )
   }
         
   renderSelect() {
     return(
-      <label
-        htmlFor="select-option"
-        data-testid="select-input-label">Filtrar por gênero
-          <select
-          value={this.props.selectedGenre}
-          onChange={this.props.onSelectedGenreChange}
-          data-testid="select-input"
-        >
-          <option value="" data-testid="select-option">Todos</option>
-          <option value="action" data-testid="select-option">Ação</option>
-          <option value="comedy" data-testid="select-option">Comédia</option>
-          <option value="thriller" data-testid="select-option">Suspense</option>
-        </select>
-      </label>
+      <div>
+        <label
+          htmlFor="select-option"
+          data-testid="select-input-label">Filtrar por gênero
+            <select
+            value={this.props.selectedGenre}
+            onChange={this.props.onSelectedGenreChange}
+            data-testid="select-input"
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
+        </label>
+      </div>
     )
   }
       
