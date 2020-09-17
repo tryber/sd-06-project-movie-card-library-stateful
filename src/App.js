3be84d36aec2f6627831e8966c995b53d46261cf
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-17 17:09:52 -0300

    finalizando cabeçalho

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
-import MovieCard from './components/MovieCard';
-import MovieList from './components/MovieList';
-import Rating from './components/Rating'
+// import MovieCard from './components/MovieCard';
+// import MovieList from './components/MovieList';
+// import Rating from './components/Rating'
 
 function App() {
   return (
