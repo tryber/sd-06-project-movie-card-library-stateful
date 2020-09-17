0e4c27e14b2bd45d947bae8f38d4ba0e0980ee43
Pablo Santos <pablo.trybe@gmail.com>
2020-09-17 20:05:49 -0300

    Starting the project :rocket:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import MovieLibrary from './MovieLibrary';
 
 class Header extends React.Component {
   render() {
     return (
       <header className="movie-card-header">
         <h1 className="page-title">Movie Cards Library</h1>
+        <MovieLibrary />
       </header>
     );
   }
