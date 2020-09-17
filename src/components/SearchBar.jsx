c0b6c559b885e771aaeccb451ede7c983e137a14
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-17 17:17:34 -0300

    1rst requirement

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,26 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ""}
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }  
+
+  render() {
+    return (
+      <form>
+        <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <input type="checkbox" />
+      </form>
+    )
+  }
+}
+
+
+
+export default SearchBar;
