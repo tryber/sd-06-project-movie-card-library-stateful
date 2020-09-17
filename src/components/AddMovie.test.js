6d01c9752ca15bef86a488b2e098ecde84d7fc7f
Daniele Perse <danieleperse3@gmail.com>
2020-09-17 18:34:07 -0300

    add alteraçoes

--- src/components/AddMovie.test.js
+++ src/components/AddMovie.test.js
@@ -52,11 +52,11 @@ beforeEach(() => {
 
 
 describe('Verifica o componente <AddMovie />', () => {
-  it.skip('Será validado se o componente renderiza', () => {
+  it('Será validado se o componente renderiza', () => {
     render(<AddMovie onClick={() => jest.fn()} />);
   });
 
-  it.skip('Será validado se o componente renderiza 1, e apenas 1, form', () => {
+  it('Será validado se o componente renderiza 1, e apenas 1, form', () => {
     expect(form).toHaveLength(1);
   });
 });
