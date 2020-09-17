d5ad3b6908bb64f9d77e3cfc8509f3d9b10ab3fb
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-17 16:58:53 -0300

    Add checked with prop value

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label">Inclui o texto:
+          <label data-testid="text-input-label" htmlFor="text-input">
+            Inclui o texto:
             <input data-testid="text-input" value={searchText} onChange={onSearchTextChange} />
           </label>
-          <label data-testid="checkbox-input-label">Mostrar somente favoritos
-            <input data-testid="checkbox-input" />
+          <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
+            Mostrar somente favoritos
+            <input data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} />
           </label>
         </form>
       </div>
