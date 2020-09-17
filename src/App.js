efdf47690004df6edca665dc18bade0cf91ee807
Stefano Branz <stefano.branz@gmail.com>
2020-09-17 18:04:11 -0300

    Films appearing at my page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
+// import MovieList from './components/MovieList';
+import MovieLibrary from './components/MovieLibrary';
+
+import movies from './data';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <MovieList movies={movies} /> */}
+      <MovieLibrary movies={movies} />
     </div>
   );
 }
