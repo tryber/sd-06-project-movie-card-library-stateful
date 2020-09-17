b11fef4f9f79d1a0c55ff34b105e33d1371a6440
VitorSSMoreira <vitorssmoreira@gmail.com>
2020-09-17 20:22:27 -0300

    Second commit, starting project-movie-card-library-stateful

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,16 @@
-// implement SearchBar component here
+import React from 'react';
+//import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
