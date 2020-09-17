08dd486dfb99e6cb406472c6abdf038bc391e051
Leandro <leandro.feitoza8018@gmail.com>
2020-09-17 20:04:00 -0300

    Resolvendo conflitos do Git

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import AddMovie from './components/AddMovie';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <SearchBar />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,29 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+ 
+  render() {
+    const { onClick } = this.props
+    return (
+      <div>
+        <form data-testid="add-movie-form" >
+
+        </form>
+      </div>
+      
+    )
+  }
+}
+
+export default AddMovie;
