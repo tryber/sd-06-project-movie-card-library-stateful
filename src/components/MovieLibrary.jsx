603a228d0927cfa40d7b1b988946834f8afdc61e
RenatoCostaDev <renatotrybe@gmail.com>
2020-09-17 18:00:48 -0300

    04- segundo requisito/codeclimate

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import MovieList from './MovieList';
+// import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
