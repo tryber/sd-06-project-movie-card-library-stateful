6e67dba072e55a67fa4aec76271ee7199a3b6c7a
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-17 18:58:01 -0300

    Select Input_fixing CC issues 06

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,15 +17,19 @@ class SearchBar extends React.Component {
       <form data-testid="search-bar-form">
 
         <label htmlFor="text-input" data-testid="text-input-label">Inclui o texto:</label>
-        <input type="text" value={this
+        <input type="text"
+        value={this
         .props
         .searchText} onChange={this
         .props
-        .onSearchTextChange} data-testid="text-input" 
+        .onSearchTextChange} data-testid="text-input"
         />
 
-        <label htmlFor="checkbox-input" data-testid="checkbox-input-label">Mostrar somente favoritos</label>
-        <input type="checkbox" data-testid="checkbox-input" checked={this
+        <label htmlFor="checkbox-input"
+        data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+        <input type="checkbox"
+        data-testid="checkbox-input"
+        checked={this
           .props
           .bookmarkedOnly} onChange={this
           .props
@@ -37,7 +41,8 @@ class SearchBar extends React.Component {
           .props
           .selectedGenre} onChange={this
           .props
-          .onSelectedGenreChange} data-testid="select-input">
+          .onSelectedGenreChange} data-testid="select-input"
+        >
           <option value="" data-testid="select-option">Todos</option>
           <option value="action" data-testid="select-option">Ação</option>
           <option value="comedy" data-testid="select-option">Comédia</option>
