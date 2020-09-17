22485ff4ccaf38c9bd0c8dbae6205fcacab22cd2
HCaetano <hrcgeo@gmail.com>
2020-09-17 18:44:21 -0300

    Refactor input names to ones that the tests expected

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
-  constructor(props) {
+  constructor() {
     super();
 
     this.state = {
@@ -33,19 +33,19 @@ class AddMovie extends Component {
       <input type="text" name="title" value={this.state.title} data-testid="title-input" onChange={this.handleChange} />
       <label htmlFor="subtitle" data-testid="subtitle-input-label">Subtítulo</label>
       <input type="text" name="subtitle" value={this.state.subtitle} data-testid="subtitle-input" onChange={this.handleChange} />
-      <label htmlFor="movie-cover" data-testid="image-input-label">Imagem</label>
-      <input type="text" name="movie-cover" value={this.state.imagePath} data-testid="image-input" onChange={this.handleChange} />
-      <label htmlFor="plot-summary" data-testid="storyline-input-label">Sinopse</label>
-      <input type="text" name="plot-summary" value={this.state.storyline} data-testid="storyline-input" onChange={this.handleChange}/>
+      <label htmlFor="imagePath" data-testid="image-input-label">Imagem</label>
+      <input type="text" name="imagePath" value={this.state.imagePath} data-testid="image-input" onChange={this.handleChange} />
+      <label htmlFor="storyline" data-testid="storyline-input-label">Sinopse</label>
+      <input type="text" name="storyline" value={this.state.storyline} data-testid="storyline-input" onChange={this.handleChange}/>
       <label htmlFor="rating" data-testid="rating-input-label">Avaliação</label>
-      <input type="text" name="rating" value={this.state.rating} data-testid="rating-input" onChange={this.handleChange} />
+      <input type="number" name="rating" value={this.state.rating} data-testid="rating-input" onChange={this.handleChange} />
       <label htmlFor="genre" data-testid="genre-input-label">Gênero</label>
       <select type="text" name="genre" value={this.state.genre} data-testid="genre-input" onChange={this.handleChange}>
         <option value="action" data-testid="genre-option">Ação</option>
         <option value="comedy" data-testid="genre-option">Comédia</option>
         <option value="thriller" data-testid="genre-option">Suspense</option>
       </select>
-      <button data-testid="send-button" onClick={onClick}>Adicionar filme</button>
+      <button onClick={onClick} data-testid="send-button">Adicionar filme</button>
     </form>;
   }
 }
