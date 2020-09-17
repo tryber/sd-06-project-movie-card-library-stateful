e2c0e26494c2f54d2a9f68b540a698d58cbdee65
Matheuscara <matheuscara2000@gmail.com>
2020-09-17 17:33:15 -0300

    Code-Climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,7 +8,7 @@ class SearchBar extends Component {
   // selectedGenre, uma string
   // onSelectedGenreChange, uma callback
   render() {
-    const {searchText,onSearchTextChange,bookmarkedOnly,onBookmarkedChange,selectedGenre,onSelectedGenreChange,} = this.props;
+    const {searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange} = this.props;
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label">Inclui o texto:
@@ -16,7 +16,7 @@ class SearchBar extends Component {
         <label data-testid="checkbox-input-label">Mostrar somente favoritos
           <input data-testid="checkbox-input" type="checkbox" name="name" checked={bookmarkedOnly} onChange={onBookmarkedChange}/></label>
         <label data-testid="select-input-label">Filtrar por gênero
-          <select data-testid="select-input"value={selectedGenre}onChange={onSelectedGenreChange}>
+          <select data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
             <option data-testid="select-option" value="">Todos</option>
             <option data-testid="select-option" value="action">Ação</option>
             <option data-testid="select-option" value="comedy">Comédia</option>
