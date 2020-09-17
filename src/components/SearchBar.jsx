3d86bb8b72384e3702b67fd6e83787f2837fca2e
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-17 20:22:05 -0300

    Adding function renderSelect to create label for select field

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -34,11 +34,32 @@ class SearchBar extends React.Component {
     );
   }
 
+  renderSelect() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label htmlFor="label-option" data-testid="select-input-label">
+        Filtrar por gênero
+        <select
+          id="label-option"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
+          data-testid="select-input"
+        >
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     return (
       <form data-testid="search-bar-form">
         <div>{this.renderTextInput()}</div>
         <div>{this.renderCheckBoxInput()}</div>
+        <div>{this.renderSelect()}</div>
       </form>
     );
   }
