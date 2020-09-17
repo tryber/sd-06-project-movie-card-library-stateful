c409ea82270b4ecc03257d5c07b04df6c3c391c7
Alvaro Neto <alvaroshn@gmail.com>
2020-09-17 20:58:43 -0300

    Quebrando AddMovies em 3 funcoes

---
+++ src/components/AddMovieComponentPartThree.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class AddMovieComponentPartThree extends React.Component {
+  render() {
+    const { value, handleChange } = this.props;
+    return (
+      <div>
+        <label htmlFor="this-form" data-testid="genre-input-label">
+          Gênero
+          <select
+            value={value}
+            data-testid="genre-input"
+            onChange={handleChange}
+            name="genre"
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+          </label>
+      </div>
+    );
+  }
+}
+
+AddMovieComponentPartThree.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+
+export default AddMovieComponentPartThree;
---
+++ src/components/AddMovieComponentPartTwo.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class AddMovieComponentPartTwo extends React.Component {
+  render() {
+    const { value, handleChange } = this.props;
+    return (
+      <div>
+        <label htmlFor="this-form" data-testid="image-input-label">
+        Imagem
+          <input
+            data-testid="image-input"
+            type="text"
+            name="imagePath"
+            onChange={handleChange}
+            value={value}
+          />
+        </label>
+        <label htmlFor="this-form" data-testid="storyline-input-label">Sinopse
+          <textarea
+            data-testid="storyline-input"
+            type="text"
+            name="storyline"
+            value={value}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="this-form" data-testid="rating-input-label">
+          Avaliação
+          <input
+            data-testid="rating-input"
+            onChange={handleChange}
+            value={value}
+            type="number"
+            name="rating"
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+AddMovieComponentPartTwo.propTypes = {
+  value: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default AddMovieComponentPartTwo;
