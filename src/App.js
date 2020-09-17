852a07027295682a28b6dfa04b01c89aa500aca5
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-17 19:54:46 -0300

    Implementação parcial do componemte MovieLibrary.

--- src/App.js
+++ src/App.js
@@ -2,55 +2,16 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie'
+import MovieLibrary from './components/MovieLibrary';
+import movies from './data';
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.onSearchTextChange = this.onSearchTextChange.bind(this);
-    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
-    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
-    this.onClickAddMovie = this.onClickAddMovie.bind(this);
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: '',
-      selectedGenre: '',
-    }
-  }
-
-  onSearchTextChange({ target: { value } }) {
-    this.setState({ searchText: value });
-  }
-
-  onBookmarkedChange({ target: { checked } }) {
-    this.setState({ bookmarkedOnly: checked });
-  }
-
-  onSelectedGenreChange({ target: { value } }) {
-    this.setState({ selectedGenre: value });
-  }
-
-  onClickAddMovie() {
-
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <SearchBar
-          searchText={this.state.searchText}
-          onSearchTextChange={this.onSearchTextChange}
-          bookmarkedOnly={this.state.bookmarkedOnly}
-          onBookmarkedChange={this.onBookmarkedChange}
-          selectedGenre={this.state.selectedGenre}
-          onSelectedGenreChange={this.onSelectedGenreChange}
-        />
-        <AddMovie onClick={this.onClickAddMovie}/>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MovieLibrary movies={movies} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,68 @@
-// implement MovieLibrary component here
+import React from 'react';
+
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super()
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.listFilter = this.listFilter.bind(this);
+    this.onClickAddMovie = this.onClickAddMovie.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    }
+  }
+
+  listFilter(criteria, filter) {
+    const { state } = this;
+    console.log(filter);
+    const filteredMovies = state.movies.filter(movie => movie[criteria].includes(filter)).map(movie => movie);
+    console.log(filteredMovies);
+  }
+
+  onSearchTextChange({ target: { value } }) {
+    this.setState(() => ({ searchText: value }));
+    this.listFilter('title', this.state.searchText);
+  }
+
+  onBookmarkedChange({ target: { checked } }) {
+    this.setState({ bookmarkedOnly: checked });
+    this.listFilter('bookmarked', this.state.bookmarkedOnly);
+  }
+
+  onSelectedGenreChange({ target: { value } }) {
+    this.setState({ selectedGenre: value });
+    this.listFilter('genre', this.state.selectedGenre);
+  }
+
+  onClickAddMovie() {
+
+  }
+
+  render() {
+    const { onSearchTextChange, onBookmarkedChange, onSelectedGenreChange, onClickAddMovie,
+      state: { searchText, bookmarkedOnly, selectedGenre, movies, } } = this;
+    return (
+    <div>
+      <MovieList movies={movies} />
+      <SearchBar
+        searchText={searchText}
+        onSearchTextChange={onSearchTextChange}
+        bookmarkedOnly={bookmarkedOnly}
+        onBookmarkedChange={onBookmarkedChange}
+        selectedGenre={selectedGenre}
+        onSelectedGenreChange={onSelectedGenreChange}
+      />
+      <AddMovie onClick={onClickAddMovie}/>
+    </div>
+  )}
+}
+
+export default MovieLibrary;
