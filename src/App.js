4914c44cb556e8df5c3c89644dcfe0366b16c9be
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-17 17:50:31 -0300

    cc 1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header';
+import movies from './data';
+import SearchBar from './components/SearchBar'
+import MovieList from './components/MovieList';
+// import MovieCard from './components/MovieCard';
 
+// import Rating from './components/Rating';
 function App() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
+      <MovieList movies={movies}/>
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,32 @@
-// implement SearchBar component here
+import React from 'react';
+// import MovieList from './components/MovieList'
+
+export default class SearchBar extends React.Component {
+	render() {
+		const {keyword, setKeyWord} = this.props
+		return(
+			<div>
+			<input key="random1" value={keyword} placeholder={"search movie"} onChange={(e) => setKeyWord(e.target.value)} />
+			</div>
+					)
+		}
+	}
+	// searchText={props.searchText}
+	// onSearchTextChange={props.onSearchTextChange}
+	// bookmarkedOnly={props.bookmarkedOnly}
+	// onBookmarkedChange={props.onBookmarkedChange}
+	// selectedGenre={props.selectedGenre}
+	// onSelectedGenreChange={props.onSelectedGenreChange}
+
+
+// 	SearchBar.propTypes = {
+//   pokemon: PropTypes.shape({
+//     name: PropTypes.string,
+//     type: PropTypes.string,
+//     averageWeight: PropTypes.shape({
+//       measurementUnit: PropTypes.string,
+//       value: PropTypes.number
+//     }),
+//     image: PropTypes.string,
+//   }).isRequired,
+// };
