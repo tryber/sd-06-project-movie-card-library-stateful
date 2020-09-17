ad1211677e18d97a42a82d92f9680948a9d1c5e4
Cyrano webdev <cyrano@cyranowebdev.com>
2020-09-17 18:53:48 -0300

    c

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -15,9 +15,9 @@ class AddMovie extends React.Component {
     };
   }
 
-  handleTextInput = ({target}) => {
+  handleTextInput({target}) {
     this.setState({[target.id]: target.value});
-  };
+  }
 
   callbackHandler = () => {
     this.props.onClick(this.state);
