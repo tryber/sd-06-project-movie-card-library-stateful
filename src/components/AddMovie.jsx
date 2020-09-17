4d4bb7a934d1b1b9bc5c63fab3d9c2c3e09f3c1a
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-09-17 20:05:04 -0300

    Correções CodeClimate.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -39,32 +39,29 @@ class AddMovie extends React.Component {
 
   render () {
     const { state: { title, subtitle, imagePath, storyline, rating, genre },
-    handleClick, onChange } = this;
-    return (
-      <form data-testid="add-movie-form" action="">
-        <label data-testid="title-input-label" htmlFor="title-input">Título
-          <input type="text" name="title" id="title-input" value={title} 
-          data-testid="title-input" onChange={onChange} /></label>
-        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">Subtítulo
-          <input type="text" name="subtitle" id="subtitle-input" value={subtitle}
-          data-testid="subtitle-input" onChange={onChange} /></label>
-        <label data-testid="image-input-label" htmlFor="image-input">Imagem
-          <input type="text" name="imagePath" id="image-input" value={imagePath}
-          data-testid="image-input" onChange={onChange} /></label>
-        <label data-testid="storyline-input-label" htmlFor="storyline-input">Sinopse
-          <textarea name="storyline" id="storyline-input" cols="30" value={storyline}
-          rows="10" data-testid="storyline-input" onChange={onChange} /></label>
-        <label data-testid="rating-input-label" htmlFor="rating-input">Avaliação
-          <input type="number" name="rating" id="rating-input" value={rating}
-          data-testid="rating-input" onChange={onChange} /></label>
-        <label data-testid="genre-input-label" htmlFor="genre-input">Gênero
-          <select name="genre" id="genre-input" data-testid="genre-input" value={genre}
-          onChange={onChange}><option data-testid="genre-option" value="action">Ação</option>
-            <option data-testid="genre-option" value="comedy">Comédia</option>
-            <option data-testid="genre-option" value="thriller">Suspense</option></select></label>
-        <button data-testid="send-button" onClick={handleClick}>Adicionar filme</button>
-      </form>
-    )
+      handleClick, onChange } = this;
+    return (<form data-testid="add-movie-form" action="">
+      <label data-testid="title-input-label" htmlFor="title-input">Título
+        <input type="text" name="title" id="title-input" value={title} 
+        data-testid="title-input" onChange={onChange} /></label>
+      <label data-testid="subtitle-input-label" htmlFor="subtitle-input">Subtítulo
+        <input type="text" name="subtitle" id="subtitle-input" value={subtitle}
+        data-testid="subtitle-input" onChange={onChange} /></label>
+      <label data-testid="image-input-label" htmlFor="image-input">Imagem
+        <input type="text" name="imagePath" id="image-input" value={imagePath}
+        data-testid="image-input" onChange={onChange} /></label>
+      <label data-testid="storyline-input-label" htmlFor="storyline-input">Sinopse
+        <textarea name="storyline" id="storyline-input" cols="30" value={storyline}
+        rows="10" data-testid="storyline-input" onChange={onChange} /></label>
+      <label data-testid="rating-input-label" htmlFor="rating-input">Avaliação
+        <input type="number" name="rating" id="rating-input" value={rating}
+        data-testid="rating-input" onChange={onChange} /></label>
+      <label data-testid="genre-input-label" htmlFor="genre-input">Gênero
+        <select name="genre" id="genre-input" data-testid="genre-input" value={genre}
+        onChange={onChange}><option data-testid="genre-option" value="action">Ação</option>
+          <option data-testid="genre-option" value="comedy">Comédia</option>
+          <option data-testid="genre-option" value="thriller">Suspense</option></select></label>
+      <button data-testid="send-button" onClick={handleClick}>Adicionar filme</button></form>)
   }
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly,
+    onBookmarkedChange, onSelectedGenreChange, selectedGenre } = this.props;
     return (
       <form data-testid="search-bar-form" action="GET">
         <label data-testid="text-input-label" htmlFor="text-input">Inclui o texto:
           <input data-testid="text-input" type="text" name="text-input" id="text-input"
-            onChange={this.props.onSearchTextChange} value={this.props.searchText} />
+            onChange={onSearchTextChange} value={searchText} />
         </label>
         <label data-testid="checkbox-input-label"
         htmlFor="checkbox-input">Mostrar somente favoritos
           <input
           data-testid="checkbox-input" type="checkbox" name="checkbox-input" id="checkbox-input"
-          onChange={this.props.onBookmarkedChange} checked={this.props.bookmarkedOnly} />
+          onChange={onBookmarkedChange} checked={bookmarkedOnly} />
         </label>
         <label data-testid="select-input-label" htmlFor="">Filtrar por gênero
           <select data-testid="select-input" name="select-input" id="select-input"
-          value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
+          value={selectedGenre} onChange={onSelectedGenreChange}>
             <option value="" data-testid="select-option">Todos</option>
             <option value="action" data-testid="select-option">Ação</option>
             <option value="comedy" data-testid="select-option">Comédia</option>
