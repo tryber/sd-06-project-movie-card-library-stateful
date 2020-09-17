214de6b0fde505f0919dba8ce174f73949727373
Jean Machado <jeanvictor.arq@gmail.com>
2020-09-17 18:03:27 -0300

    codeclimate 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,22 +4,29 @@ class SearchBar extends React.Component {
   render() {
     const {
       searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
-      selectedGenre, onSelectedGenreChange } = this.props;
+      selectedGenre, onSelectedGenreChange
+    } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label htmlFor="incluiOTexto" data-testid="text-input-label">Inclui o texto:
-            <input id="incluiOTexto" type="text" value={searchText} onChange={onSearchTextChange}
+          <label htmlFor="incluiOTexto" data-testid="text-input-label">
+            Inclui o texto:
+            <input
+              id="incluiOTexto" type="text" value={searchText} onChange={onSearchTextChange}
               data-testid="text-input"
             />
           </label>
-          <label htmlFor="mostraFavoritos" data-testid="checkbox-input-label">Mostrar somente favoritos
-            <input id="mostraFavoritos" type="checkbox" checked={ bookmarkedOnly }
-              onChange={ onBookmarkedChange } data-testid="checkbox-input"
+          <label htmlFor="mostraFavoritos" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
+            <input
+              id="mostraFavoritos" type="checkbox" checked={bookmarkedOnly}
+              onChange={onBookmarkedChange} data-testid="checkbox-input"
             />
           </label>
-          <label htmlFor="filtrarGenero" data-testid="select-input-label">Filtrar por gênero
-            <select id="filtrarGenero"value={selectedGenre} onChange={onSelectedGenreChange}
+          <label htmlFor="filtrarGenero" data-testid="select-input-label">
+            Filtrar por gênero
+            <select
+              id="filtrarGenero"value={selectedGenre} onChange={onSelectedGenreChange}
               data-testid="select-input"
             >
               <option value={''} data-testid="select-option">Todos</option>
