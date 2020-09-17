85447666f679d0cdd4c1e80c45ebfed56937c68e
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-17 18:28:29 -0300

    import MovieList App

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,16 @@
 // implement SearchBar component here
+// import React, { Component } from 'react';
+// import MovieList from './MovieList';
+// import SearchBar from './SearchBar';
+// import AddMovie from './AddMovie';
+
+// class SearchBar extends Component {
+ 
+//   render() {
+//     return (
+//       <div></div>
+//     );
+//   }
+// }
+
+// export default SearchBar;
