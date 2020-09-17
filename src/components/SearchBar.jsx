6f5c7067119e2b46163f6a742fcb914efad2f9d3
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-17 17:25:48 -0300

    rendering form and first input, added prop validation

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText,
+    const {
+      searchText,
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange
+      onSelectedGenreChange,
     } = this.props;
 
-    return <span>Hello World</span>
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" >
+          Inlcui o texto
+          <input type="text" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
+        </label>
+      </form>
+    );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string,
+  onSelectedGenreChange: PropTypes.func,
+};
+
+SearchBar.defaultProps = {
+  searchText: '',
+  onSearchTextChange: undefined,
+  bookmarkedOnly: false,
+  onBookmarkedChange: undefined,
+  selectedGenre: '',
+  onSelectedGenreChange: undefined,
+};
 
 export default SearchBar;
