b2ef75e8674dbcd2e0b50ff1a436c84e274cd41e
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2020-09-17 17:45:41 -0300

    SearchBar complete, CC to check

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,9 +14,34 @@ class SearchBar extends React.Component {
 
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label" >
-          Inlcui o texto
-          <input type="text" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
+        <label data-testid="text-input-label" htmlFor="search-text">
+          Inclui o texto
+          <input
+            id="search-text"
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            data-testid="text-input"
+          />
+        </label>
+        <label data-testid="checkbox-input-label" htmlFor="favorites">
+          Mostrar somente favoritos
+          <input
+            id="favorites"
+            type="checkbox"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+            data-testid="checkbox-input"
+          />
+        </label>
+        <label data-testid="select-input-label" htmlFor="genre-selection">
+          Filtrar por gênero
+          <select id="genre-selection" data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
