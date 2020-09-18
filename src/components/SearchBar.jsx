7ac6bd89c974675e31f4ec4e902ea136ff7f2bc0
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-18 10:03:43 -0300

    cc 1.2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,20 +2,18 @@ import React from 'react';
 // import MovieList from './components/MovieList'
 
 export default class SearchBar extends React.Component {
-	render() {
-		const {keyword, setKeyWord} = this.props
-		return(
-			<div>
-			<input key="random1" value={keyword} placeholder={"search movie"} onChange={(e) => setKeyWord(e.target.value)} />
-			</div>
-					)
-		}
-	}
-	// searchText={props.searchText}
-	// onSearchTextChange={props.onSearchTextChange}
-	// bookmarkedOnly={props.bookmarkedOnly}
-	// onBookmarkedChange={props.onBookmarkedChange}
-	// selectedGenre={props.selectedGenre}
-	// onSelectedGenreChange={props.onSelectedGenreChange}
-
-
+  render() {
+    const {keyword, setKeyWord} = this.props
+    return (
+      <div>
+        <input key="random1" value={keyword} placeholder={"search movie"} onChange={(e) => setKeyWord(e.target.value)} />
+      </div>
+    );
+  }
+}
+// searchText={props.searchText}
+// onSearchTextChange={props.onSearchTextChange}
+// bookmarkedOnly={props.bookmarkedOnly}
+// onBookmarkedChange={props.onBookmarkedChange}
+// selectedGenre={props.selectedGenre}
+// onSelectedGenreChange={props.onSelectedGenreChange}
