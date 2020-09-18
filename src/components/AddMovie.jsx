3f62fee596d7a7e17c5752dc17a9bda75c1ea76a
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-09-18 16:29:42 -0300

    add features AddMovie.js

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -10,7 +10,7 @@ class AddMovie extends React.Component {
       imagePath: '',
       storyline: '',
       rating: 0,
-      gender: 'action',
+      genre: 'action',
     };
   }
   render() {
@@ -18,7 +18,53 @@ class AddMovie extends React.Component {
       <form
         data-testid="add-movie-form"
       >
-        <input type="text" />
+        <label data-testid="title-input-label" htmlFor="title-id">Título</label>
+        <input 
+          type="text"
+          data-testid="title-input"
+          value={this.state.title}
+          id="title-id"
+        />
+
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-id">Subtítulo</label>
+        <input 
+          type="text"
+          data-testid="subtitle-input"
+          value={this.state.subtitle}
+          id="subtitle-id"
+        />
+
+        <label data-testid="image-input-label" htmlFor="image-id">Imagem</label>
+        <input 
+          type="text"
+          data-testid="image-input"
+          value={this.state.imagePath}
+          id="image-id"
+        />
+
+        <label data-testid="storyline-input-label" htmlFor="storyline-id">Sinopse</label>
+        <textarea
+          data-testid="storyline-input"
+          value={this.state.storyline}
+          id="storyline-id"
+        />
+
+        <label data-testid="rating-input-label" htmlFor="rating-id">Avaliação</label>
+        <input 
+          type="number"
+          data-testid="rating-input"
+          value={this.state.rating}
+          id="rating-id"
+        />
+
+        <label data-testid="genre-input-label" htmlFor="genre-id">Gênero</label>
+        <select data-testid="genre-input" value={this.state.genre}>
+          <option value="action" data-testid="genre-option">Ação</option>
+          <option value="comedy" data-testid="genre-option">Comédia</option>
+          <option value="thriller" data-testid="genre-option">Suspense</option>
+        </select>
+
+        <button data-testid="send-button">Adicionar filme</button>
       </form>
     );
   }
