39764d32008fc3886666b44311fca4b8822c2160
Stefano Branz <stefano.branz@gmail.com>
2020-09-18 15:36:13 -0300

    Search Bar requisites done push 01

--- src/components/Rating.test.js
+++ src/components/Rating.test.js
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import '@testing-library/jest-dom';
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-// import Rating from './Rating';
+import Rating from './Rating';
 
-// describe('Verifica o componente <Rating />', () => {
-//   it('Será validado se o componente <Rating /> é renderizado com sucesso', () => {
-//     render(<Rating />);
-//   });
+describe('Verifica o componente <Rating />', () => {
+  it('Será validado se o componente <Rating /> é renderizado com sucesso', () => {
+    render(<Rating />);
+  });
 
-//   it('Será validado se o componente renderiza o <Rating /> com o valor passado para ele via props `rating`', () => {
-//     const { getByTestId } = render(<Rating rating={3} />);
-//     const rating = getByTestId('rating');
+  it('Será validado se o componente renderiza o <Rating /> com o valor passado para ele via props `rating`', () => {
+    const { getByTestId } = render(<Rating rating={3} />);
+    const rating = getByTestId('rating');
 
-//     expect(rating).toHaveTextContent(3);
-//   });
-// });
+    expect(rating).toHaveTextContent(3);
+  });
+});
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+
     return (
-      <div className="search-bar">
-        My Search Bar
-      </div>
+      <form data-testid="search-bar-form" className="search-bar-form">
+        <label htmlFor="textFilter" data-testid="text-input-label">Inclui o texto: </label>
+        <input type="text" name="textFilter" id="textFilter" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
+      
+        <label htmlFor="checkBoxFilter" data-testid="checkbox-input-label">Mostrar somente favoritos: </label>
+        <input type="checkbox" name="checkBoxFilter" id="checkBoxFilter" checked={bookmarkedOnly} onChange={onBookmarkedChange} data-testid="checkbox-input" />
+
+        <label htmlFor="selectFilter" data-testid="select-input-label">Filtrar por gênero: </label>
+        <select name="selectFilter" id="selectFilter" value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input">
+          <option value="" data-testid="select-option">Todos</option>
+          <option value="action" data-testid="select-option">Ação</option>
+          <option value="comedy" data-testid="select-option">Comédia</option>
+          <option value="thriller" data-testid="select-option">Suspense</option>
+        </select>
+      </form>
     );
   }
 }
