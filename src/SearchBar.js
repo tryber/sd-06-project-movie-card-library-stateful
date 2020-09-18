0e4336271d77395c33c59696b420946198468462
paulo-lins <paulolins100@gmail.com>
2020-09-18 12:02:04 -0300

    SearchBar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -24,13 +24,17 @@ class SearchBar extends React.Component {
   }
 
   render() {
+    const inputSearch = {
+      onChange="onSearchTextChange",
+      name="searchtext",
+      value="searchText",
+    }
     return (
       <div className="movie-card-header">
         <form id="search-bar-form" data-testid="text-input-label">
           <label htmlFor="text-input-label">
             Inclui o texto:
-            <input type="text" onChange="onSearchTextChange"
-              name="searchtext" value="searchText"
+            <input type="text" {...inputSearch}
               data-testid="text-input" />
           </label>
         </form>
