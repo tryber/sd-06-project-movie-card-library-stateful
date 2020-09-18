7c7eaac50a22f6dec9b902138190c97a34fac125
Thales Rangel <thalesgr@gmail.com>
2020-09-18 16:04:18 -0300

    mais um requisito completo em movie library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -9,6 +9,9 @@ class MovieLibrary extends React.Component {
   constructor() {
     super();
     const movies = this.props;
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
@@ -16,12 +19,28 @@ class MovieLibrary extends React.Component {
       movies,
     };
   }
+
+  onSearchTextChange(e) {
+    this.setState({ searchText: e.target.value });
+  }
+
+  onBookmarkedChange(e) {
+    this.setState({bookmarkedOnly: e.target.checked });
+    // if (this.state.bookmarkedOnly === true) movies.filter((movie) => movie.bookmarked === true);
+  }
+
+  onSelectedGenreChange(e) {
+    this.setState({ selectedGenre: e.target.value });
+  }
+
   render() {
     return (
       <div>
         <SearchBar
           searchText={this.state.searchText} bookmarkedOnly={this.state.bookmarkedOnly}
-          selectedGenre={this.state.selectedGenre}
+          selectedGenre={this.state.selectedGenre} onSearchTextChange={this.onSearchTextChange}
+          onBookmarkedChange={this.onBookmarkedChange}
+          onSelectedGenreChange={this.onSelectedGenreChange}
         />
       </div>
     );
