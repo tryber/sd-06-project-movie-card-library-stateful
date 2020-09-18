421619bc3c16dc80b017047be3c9a6bf96dd8351
Ana Flávia <anaflvia@gmail.com>
2020-09-18 16:35:47 -0300

    MovieLibrary parte 1

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import movies from '../data';
+// import movies from '../data';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies,
+      movies: props.movies,
     };
     this.refreshSearchText = this.refreshSearchText.bind(this);
     this.refreshBookmarkedOnly = this.refreshBookmarkedOnly.bind(this);
@@ -21,24 +21,34 @@ class MovieLibrary extends React.Component {
   refreshBookmarkedOnly(event) {
     this.setState({ bookmarkedOnly: event.target.checked }, () => {
       if (this.state.bookmarkedOnly === true) {
-        const favoriteMovies = movies.filter((movie) => movie.bookmarked === true);
+        const favoriteMovies = this.props.movies.filter((movie) => movie.bookmarked === true);
         this.setState({ movies: favoriteMovies });
       } else {
-        this.setState({ movies });
+        this.setState({ movies: this.props.movies });
       }
     });
   }
 
   refreshSelectedGenre(event) {
-    this.setState({ selectedGenre: event.target.value });
+    this.setState({ selectedGenre: event.target.value }, () => {
+      if (this.state.selectedGenre === '') {
+        this.setState({ movies: this.props.movies });
+      } else {
+        const moviesGenre = this.props.movies.filter((movie) => movie.genre === this.state.selectedGenre)
+        this.setState({ movies: moviesGenre })
+      }
+    });
   }
 
-  // onClick() {
+  onClick() {
 
-  // }
+  }
 
   refreshSearchText(event) {
-    this.setState({ searchText: event.target.value });
+    this.setState({ searchText: event.target.value }, () => {
+      const moviesFiltered = this.props.movies.filter((movie) => movie.title.toLowerCase().includes(this.state.searchText.toLowerCase()) || movie.subtitle.toLowerCase().includes(this.state.searchText.toLowerCase()) || movie.storyline.toLowerCase().includes(this.state.searchText.toLowerCase()));
+      this.setState({ movies: moviesFiltered });
+    });
   }
 
   render() {
