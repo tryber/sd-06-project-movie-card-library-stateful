9c6dcea9f8241e0d996ecb3d30211df5720059da
FPhilipi <fabiphilipi@gmail.com>
2020-09-18 08:59:19 -0300

    CC - 2

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,9 +8,10 @@ class SearchBar extends Component {
     return (
       <label data-testid="text-input-label" htmlFor="text-input-label">
         Inclui o texto
-        <input type="text" data-testid="text-input"
-          onChange={onSearchTextChange} value={searchText}
-        />
+        <input
+        type="text" data-testid="text-input"
+        onChange={onSearchTextChange} value={searchText}
+           />
       </label>
     );
   }
