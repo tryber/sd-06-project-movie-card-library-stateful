46fa3636cd59b17dcee1969b64e130acbfb6f38c
FPhilipi <fabiphilipi@gmail.com>
2020-09-18 08:43:04 -0300

    CC

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,6 +3,16 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
 
+  functionNeededTwo() {
+    const { searchText, onSearchTextChange } = this.props;
+    return (
+      <label data-testid="text-input-label" htmlFor="text-input-label">
+        Inclui o texto
+        <textarea data-testid="text-input" onChange={onSearchTextChange} value={searchText} />
+      </label>
+    )
+  }
+
   functionNeeded() {
     const { selectedGenre, onSelectedGenreChange } = this.props;
     return (
@@ -16,23 +26,24 @@ class SearchBar extends Component {
   }
 
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange } = this.props;
+    const { bookmarkedOnly, onBookmarkedChange } = this.props;
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label data-testid="text-input-label" htmlFor="text-input-label">
+          {/* <label data-testid="text-input-label" htmlFor="text-input-label">
             Inclui o texto
             <textarea data-testid="text-input" onChange={onSearchTextChange} value={searchText} />
-          </label>
+          </label> */}
+          {this.functionNeededTwo()}
           <br />
           <label data-testid="checkbox-input-label" htmlFor="checkbox-input-label">
             Mostrar somente favoritos
           <input
-            data-testid="checkbox-input"
-            type="checkbox"
-            onChange={onBookmarkedChange}
-            checked={bookmarkedOnly}
-          />
+              data-testid="checkbox-input"
+              type="checkbox"
+              onChange={onBookmarkedChange}
+              checked={bookmarkedOnly}
+            />
           </label><br />
           <label data-testid="select-input-label" htmlFor="select-input-label">
             Filtrar por gênero
