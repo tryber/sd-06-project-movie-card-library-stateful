3254614e8e5967bd16383c0fb077da38bba27546
ilanribeiro <ilanribeiro@gmail.com>
2020-09-18 15:41:56 -0300

    included props "searchText" and "onSearchTextChange"

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    const { searchText, onSearchTextChange } = this.props;
+
     return (
       <div>
         <p>Search Bar</p>
         <form action="" data-testid="search-bar-form">
           <label htmlFor="" data-testid="text-input-label">Inclui o texto</label>
-          <input type="text" name="" id="" data-testid="text-input"/>
+          <input
+            type="text"
+            name=""
+            id=""
+            data-testid="text-input"
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
         </form>
       </div>
     )
   }
-
 }
 
 export default SearchBar;
