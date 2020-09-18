26cf035d14fde7b52deb9d42c54de6fd92af1e34
paulo-lins <paulolins100@gmail.com>
2020-09-18 11:30:50 -0300

    SearchBar corrigindi code climate

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,38 +1,33 @@
-import { render } from 'enzyme';
 import React from 'react';
 import './App.css';
 
-import SearchBar from './components/SearchBar';
-
 class SearchBar extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.handleChange = this.handleChange.bind(this);
 
     this.state = {
       searchText: '',
-      onSearchTextChange: callback,
+      onSearchTextChange: 'callback',
       bookmarkedOnly: false,
-      onBookmarkedChange: callback,
+      onBookmarkedChange: 'callback',
       selectedGenre: '',
-      onSelectedGenreChange: callback,
-    }
+      onSelectedGenreChange: 'callback',
+    };
   }
 
   handleChange({ target }) {
-    const { name, value } = target
+    const { name, value } = target;
 
-    this.setState({
-      [name]: value
-    })
+    this.setState({ [name]: value });
   }
 
   render() {
     return (
       <div className="movie-card-header">
-        <form id='search-bar-form'>
-          <label id='text-input-label'>
+        <form id="search-bar-form">
+          <label id="text-input-label">
             Inclui o texto:
             <input type="text" name="searchtext" value="searchText" />
           </label>
