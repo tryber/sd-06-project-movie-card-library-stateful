b3cf58f45320121c72713185877a81425539ea49
rafMewtwo <ponciano.raf@gmail.com>
2020-09-18 01:00:48 -0300

    falta 9 requisitos

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,10 +6,11 @@ class AddMovie extends React.Component {
         super()
 
         this.handleChange = this.handleChange.bind(this)
+        this.result = this.result.bind(this)
 
         this.state = {
-            subtitle: '',
             title: '',
+            subtitle: '',
             imagePath: '',
             storyline: '',
             rating: 0,
@@ -22,16 +23,49 @@ class AddMovie extends React.Component {
             this.setState({ [name]: value})
           }
 
+        result() {
+            const onCLick  = this.props.onClick
+            onCLick(this.state)
+            this.setState({
+               title: '',
+                subtitle: '',
+                imagePath: '',
+                storyline: '',
+                rating: 0,
+                genre: 'action',
+            
+            })
+        }
+
     render() {
-        const { onClick } = this.props
+        const {title, subtitle, imagePath, storyline, rating, genre} = this.state
         return (
-            <form>
+            <form data-testid="add-movie-form">
 
                 <label for="title-input" data-testid="title-input-label">Título</label>
-                <intup type="text" value={this.state.title} data-testid="title-input" onChange={this.handleChange} />
+                <input type="text" value={title} data-testid="title-input" onChange={this.handleChange} name="title" />
 
-                <label for="title-input" data-testid="subtitle-input-label">Subtítulo</label>
-                <intup type="text" value={this.state.subtitle} data-testid="subtitle-input" onChange={this.handleChange} />
+                <label for="subtitle-input" data-testid="subtitle-input-label">Subtítulo</label>
+                <input type="text" value={subtitle} data-testid="subtitle-input" onChange={this.handleChange} name="subtitle" />
+
+                <label for="image-input" data-testid="image-input-label">Imagem</label>
+                <input type="text" value={imagePath} data-testid="image-input" onChange={this.handleChange} name="imagePath" />
+
+                <label for="storyline-input" data-testid="storyline-input-label">Sinopse</label>
+                <textarea value={storyline} data-testid="storyline-input" onChange={this.handleChange} name="storyline">
+                </textarea>
+
+                <label for="rating-input" data-testid="rating-input-label">Avaliação</label>
+                <input type="number" value={rating} data-testid="rating-input" onChange={this.handleChange} name="rating" />
+
+                <label data-testid="genre-input-label">Gênero</label>
+                <select value={genre} data-testid="genre-input" onChange={this.handleChange} name="genre">
+                    <option value="action" data-testid="genre-option">Ação</option>
+                    <option value="comedy" data-testid="genre-option">Comédia</option>
+                    <option value="thriller" data-testid="genre-option">Suspense</option>
+                </select>
+
+                <button type="submit" data-testid="send-button" onClick={this.result}>Adicionar filme</button>
 
             </form>
         )
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -4,19 +4,43 @@ import React, { Component } from 'react';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
+import movies from '../data';
 
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    }
+  }
+
+  onSearchTextChange(event) {
+    this.setState({searchText: event.target.value})
+  }
+
+  onBookmarkedChange(event) {
+    this.setState({bookmarkedOnly: event.target.value})
+  }
+
+  onSelectedGenreChange(event) {
+    this.setState({selectedGenre: event.target.value})
+  }
+
+  onClick() {
+
   }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar searchText={this.state.searchText} onSearchTextChange={this.onSearchTextChange} bookmarkedOnly={this.state.bookmarkedOnly} onBookmarkedChange={this.onBookmarkedChange} selectedGenre={this.state.selectedGenre} onSelectedGenreChange={this.onSelectedGenreChange}/>
         <MovieList movies={this.props.movies} />
-        <AddMovie />
+        <AddMovie onClick={this.onClick}/>
       </div>
     );
   }
