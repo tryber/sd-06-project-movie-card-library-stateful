50edb974b4e51a9dd3b6340426a9a8dfc98e95f6
Thales Rangel <thalesgr@gmail.com>
2020-09-18 15:41:59 -0300

    correção cc2

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import propTypes from 'prop-types';
+// import propTypes from 'prop-types';
 // import MovieList from './MovieList';
 // import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
