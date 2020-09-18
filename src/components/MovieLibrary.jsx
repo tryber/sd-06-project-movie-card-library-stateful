546e1c8e2a8550ddeba34781a23eaacb99a0ebd5
Alessandra <itsalematos@hotmail.com>
2020-09-18 14:12:02 -0400

    testando code climate

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import SearchBar from './SearchBar';
-import MovieList from './MovieList'
+// import React, { Component } from 'react';
+// import SearchBar from './SearchBar';
+// import MovieList from './MovieList';
 
-class MovieLibrary extends Component {
-  constructor() {
-    super();
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: ''
-    }
-  }
+// class MovieLibrary extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       searchText: '',
+//       bookmarkedOnly: false,
+//       selectedGenre: '',
+//       movies: '',
+//     };
+//   }
 
-  render() {
-    const { movies } = this.props;
-    return(
-      <div>
-        <SearchBar searchText={this.searchText} bookmarkedOnly={this.bookmarkedOnly} selectedGenre={this.selectedGenre} />
-        {/* <MovieList movies={movies}/> */}
-        
-      </div>
-    )
-  }
-}
+//   render() {
+//     const { movies } = this.props;
+//     return (
+//       <div>
+//         <SearchBar searchText={this.searchText} 
+//         bookmarkedOnly={this.bookmarkedOnly} selectedGenre={this.selectedGenre} 
+//         />
+//         {/* <MovieList movies={movies}/> */}
+//       </div>
+//     );
+//   }
+// }
 
-export default MovieLibrary;
+// export default MovieLibrary;
