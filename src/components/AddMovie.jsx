5449be2592dd595e1f66782c88ab128359065dea
Rods <rods.leite27@gmail.com>
2020-09-18 03:36:22 -0300

    Separei todos os inputs do AddMovie em componentes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import TitleInput from './TitleInput';
+import SubtitleInput from './SubtitleInput';
+import GenreInput from './GenreInput';
+import ImageInput from './ImageInput';
+import Storyline from './Storyline';
+import RatingInput from './RatingInput';
 
 class AddMovie extends React.Component {
   constructor() {
@@ -22,48 +28,17 @@ class AddMovie extends React.Component {
 
   render() {
     const { onClick } = this.props;
+    const { subtitle, title, imagePath, storyline, rating, genre } = this.state;
 
     return (
       <div>
         <form data-testid="add-movie-form">
-          <label htmlFor="title" data-testid="title-input-label">
-            <p>Título</p>
-            <input id="title" data-testid="title-input" name="title" value={this.state.title} onChange={this.handleChange} />
-          </label>
-          <div>
-            <label htmlFor="subtitle" data-testid="subtitle-input-label">
-              <p>Subtítulo</p>
-              <input id="subtitle" data-testid="subtitle-input" name="subtitle" value={this.state.subtitle} onChange={this.handleChange} />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="img" data-testid="image-input-label">
-              <p>Imagem</p>
-              <input id="img" data-testid="image-input" name="imagePath" value={this.state.imagePath} onChange={this.handleChange} />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="storyline" data-testid="storyline-input-label">
-              <p>Sinopse</p>
-              <textarea id="storyline" data-testid="storyline-input" name="storyline" value={this.state.storyline} onChange={this.handleChange} />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="rating" data-testid="rating-input-label">
-              <p>Avaliação</p>
-              <input type="number" id="rating" step="0.1" data-testid="rating-input" name="rating" value={this.state.rating} onChange={this.handleChange} />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="select" data-testid="genre-input-label">
-              <p>Gênero</p>
-              <select id="rating" data-testid="genre-input" name="genre" value={this.state.genre} onChange={this.handleChange}>
-                <option data-testid="genre-option" value="action">Ação</option>
-                <option data-testid="genre-option" value="comedy">Comédia</option>
-                <option data-testid="genre-option" value="thriller">Suspense</option>
-              </select>
-            </label>
-          </div>
+          <TitleInput value={title} handleChange={this.handleChange} />
+          <SubtitleInput value={subtitle} handleChange={this.handleChange} />
+          <ImageInput value={imagePath} handleChange={this.handleChange} />
+          <Storyline value={storyline} handleChange={this.handleChange} />
+          <RatingInput value={rating} handleChange={this.handleChange} />
+          <GenreInput value={genre} handleChange={this.handleChange} />
           <button type="submit" data-testid="send-button" onClick={onClick}>Adicionar filme</button>
         </form>
       </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -15,17 +15,17 @@ class SearchBar extends React.Component {
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label id="" data-testid="text-input-label">
+          <label htmlFor="text-input" data-testid="text-input-label">
             <p>Inclui o texto</p>
-            <input data-testid="text-input" value={searchText} onChange={onSearchTextChange} />
+            <input id="text-input" data-testid="text-input" value={searchText} onChange={onSearchTextChange} />
           </label>
           <div>
-            <label data-testid="checkbox-input-label">
-              <input data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+            <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+              <input id="checkbox-input" data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
               <span>Mostrar somente favoritos</span>
             </label>
           </div>
-          <label data-testid="select-input-label">
+          <label htmlFor="select-input-label" data-testid="select-input-label">
             Filtrar por gênero
             <div>
               <select data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
