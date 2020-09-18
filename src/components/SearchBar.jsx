cf03c47f9c6715b71c0b4c026e9e6d839d2f4f46
Bruno Dantas <bruno.wesley77@gmail.com>
2020-09-18 17:55:35 -0300

    add input text tag

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,12 +2,19 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div>
         <form data-testid="search-bar-form">
           <label htmlFor="text-input" data-testid="text-input-label">Inclui o texto:</label>
-          <input type="text" id="text-input" />
+          <input type="text" id="text-input" data-testid="text-input" />
         </form>
       </div>
     );
