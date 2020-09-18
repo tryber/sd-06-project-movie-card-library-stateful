cce4b6703e2bde5d0a658fc87cd485317308eb90
Ailson Lima Jr <ailsonjr21@gmail.com>
2020-09-17 23:21:04 -0300

    Inserido as ids necessárias para os arquivos de teste

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      checkbox: false,
+      selectGenre: ''
+    }
+
+  }
+
+  onSearchTextChange() {
+    
+  }
+
   render() {
     return (
-      <div>
-        <input type="text" />
-      </div>
+      <form data-testid='search-bar-form'>
+         <div>
+          <label data-testid='text-input-label'>Inclui o texto</label><br/>
+            <input type="text" data-testid='text-input'/>
+           <br />
+          <label >
+            <input type="checkbox"data-testid='checkbox-input'/>
+            Mostrar somente os favoritos 
+          </label><br/>
+          <label data-testid='select-input-label'>Filtrar por gênero</label><br/>
+            <select data-testId='select-input'>
+              <option value="Todos">Todos</option>
+            </select>
+        </div>
+      </form>
     );
   }
 }
