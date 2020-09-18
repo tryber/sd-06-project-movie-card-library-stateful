22b9f3bbf4ad29d3c695937577beea0790f5d500
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-18 10:35:55 -0300

    cc 1.3

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <SearchBar />
-      <MovieList movies={movies}/>
+      <MovieList movies={movies} />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // import MovieList from './components/MovieList'
 
 export default class SearchBar extends React.Component {
   render() {
-    const {keyword, setKeyWord} = this.props
+			const { keyword, setKeyWord } = this.props;
     return (
       <div>
-        <input key="random1" value={keyword} placeholder={"search movie"} onChange={(e) => setKeyWord(e.target.value)} />
+        <input key='random1' value={this.props.keyword} placeholder='search movie' onChange={(e) => setKeyWord(e.target.value)} />
       </div>
     );
   }
 }
+
+SearchBar.propTypes = {
+	keyword: PropTypes.string,
+	setKeyWord:PropTypes.any
+};
 // searchText={props.searchText}
 // onSearchTextChange={props.onSearchTextChange}
 // bookmarkedOnly={props.bookmarkedOnly}
