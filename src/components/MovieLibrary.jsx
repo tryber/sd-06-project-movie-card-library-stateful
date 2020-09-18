c855b043e4ba34a8ffa92e1354f46680cdf7593e
paulo-lins <paulolins100@gmail.com>
2020-09-18 17:12:12 -0300

    AddMovie

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,20 @@
-// implement MovieLibrary component here
+import React from 'react';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+class MovieCard extends React.Component {
+    { movies } = this.props
+render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    return (
+        <div>
+            <SearchBar />
+            <AddMovie />
+        </div>
+    );
+}
+}
+
+export default MovieLibrary;
+
