037ae99161e81b494f60c1bcc67a97b2c5855f8d
ricardorosa-dev <ricardorosa@gmail.com>
2020-09-17 21:05:35 -0300

    codeclimate-1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
 
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const {
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
 
     return (
       <div>
@@ -26,5 +34,14 @@ class SearchBar extends React.Component {
     );
   };
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string,
+  onSearchTextChange: PropTypes.func,
+  bookmarkedOnly: PropTypes.bool,
+  onBookmarkedChange: PropTypes.func,
+  selectedGenre: PropTypes.string;
+  onSelectedGenreChange: PropTypes.func
+}.isRequired
 
 export default SearchBar;
