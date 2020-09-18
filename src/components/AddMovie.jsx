089390620537f99877633166fe532d281cce8cea
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-09-17 23:49:25 -0300

    Implementando AddMovie CodeClimate 04

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
   constructor() {
@@ -137,5 +138,7 @@ class AddMovie extends Component {
     );
   }
 }
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
 
 export default AddMovie;
