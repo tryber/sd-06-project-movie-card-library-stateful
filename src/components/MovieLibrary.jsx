f41600d048eebd901602e22ba7126e1b24516b63
jederson <jmacedo.ti@gmail.com>
2020-09-18 15:46:54 -0300

    SearchBar e AddMovie

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,58 @@
-// implement MovieLibrary component here
+// import React, { Component } from 'react'
+// import SeachBar from './SearchBar'
+// import AddMovie from './AddMovie'
+// // import movies from '../data'
+
+// class MovieLibrary extends Component {
+//   constructor() {
+//     super()
+
+//     this.onSearchTextChange = this.onSearchTextChange.bind(this);
+
+//     this.state = {
+//       searchText: "",
+//       bookmarkedOnly: false,
+//       selectedGenre: "",
+//       movies: ""
+//     }
+
+//   }
+
+//   onSearchTextChange({ target }) {
+//     this.setState({
+//       [target.name]: target.value
+//     });
+//   }
+
+//   onBookmarkedChange({ target }) {
+//     this.setState({
+//       [target.name]: target.value
+//     });
+//   }
+
+//   onSelectedGenreChange({ target }) {
+//     this.setState({
+//       [target.name]: target.value
+//     });
+//   }
+
+//   render() {
+//     const { movies } = this.props;
+
+//     return (
+//       <div>
+//         <SeachBar
+//           searchText={this.state.searchText}
+//           onSearchTextChange={this.onSearchTextChange}
+//           bookmarkedOnly={this.state.bookmarkedOnly}
+//           onBookmarkedChange={this.onBookmarkedChange}
+//           selectedGenre={this.state.selectedGenre}
+//           onSelectedGenreChange={this.onSelectedGenreChange}
+//         />
+//         <AddMovie />
+//       </div>
+//     )
+//   };
+// }
+
+// export default MovieLibrary;
