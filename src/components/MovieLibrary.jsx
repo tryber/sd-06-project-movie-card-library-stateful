0dbe57ccdc328d16ac9a99724d8dc97be660b563
HCaetano <hrcgeo@gmail.com>
2020-09-18 18:18:22 -0300

    Add basic code to render 'MovieLibrary' component

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,11 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+
+class MovieLibrary extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return <div></div>;
+  }
+}
+
+export default MovieLibrary;
