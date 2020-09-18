0c948a5c4228a39ed4ef372a624b6e4ee52007e5
ricardorosa-dev <ricardorosa@gmail.com>
2020-09-17 21:43:54 -0300

    codeclimate9

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,26 +5,28 @@ import SearchSelect from './SearchSelect.jsx';
 
 class SearchBar extends React.Component {
   render() {
-    const {
-      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange,
-    } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange, } = this.props;
 
     return (
       <div>
         <form data-testid="search-bar-form">
           <label htmlFor="text-input" data-testid="text-input-label">Inclui o texto</label>
           <input
-            type="text" id="text-input" data-testid="text-input" value={searchText} onChange={onSearchTextChange}
+            type="text" id="text-input" data-testid="text-input"
+            value={searchText} onChange={onSearchTextChange}
           />
 
           <input
-            type="checkbox" id="bookmark-checked" data-testid="checkbox-input" checked={bookmarkedOnly} onChange={onBookmarkedChange}
+            type="checkbox" id="bookmark-checked" data-testid="checkbox-input"
+            checked={bookmarkedOnly} onChange={onBookmarkedChange}
           />
           <label htmlFor="bookmark-checked" data-testid="checkbox-input-label" >
             Mostrar somente favoritos
           </label>
 
-          <SearchSelect selectedGenre={selectedGenre} onSelectedGenreChange={onSelectedGenreChange} />
+          <SearchSelect selectedGenre={selectedGenre}
+            onSelectedGenreChange={onSelectedGenreChange}
+          />
         </form>
       </div>
     );
