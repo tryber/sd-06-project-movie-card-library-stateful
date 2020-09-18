07c517d8621f77826fc2507fdfc94d9a5ab9edee
paulo-lins <paulolins100@gmail.com>
2020-09-18 18:57:57 -0300

    AddMovie

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -3,17 +3,17 @@ import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
 
 class MovieCard extends React.Component {
-   const { movies } = this.props
-render() {
-    const movie = this.props;
-    const { title, subtitle, storyline, rating, imagePath } = movie;
-    return (
-        <div>
-            <SearchBar />
-            <AddMovie />
-        </div>
-    );
-}
+
+    render() {
+        const movie = this.props;
+        const { title, subtitle, storyline, rating, imagePath } = movie;
+        return (
+            <div>
+                <SearchBar />
+                <AddMovie />
+            </div>
+        );
+    }
 }
 
 export default MovieLibrary;
