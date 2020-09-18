ad413d00c637e01f301e8527a57951554ac72f9b
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-09-18 19:19:57 -0300

    AddMovie mudando botao de lugar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -33,43 +33,45 @@ class AddMovie extends React.Component {
   render() {
     const { onClick } = this.props;
     return (
-      <form data-testid="add-movie-form">
-        <label htmlFor="title" data-testid="title-input-label">
-          Título
-          <input id="title" type="text" data-testid="title-input" 
-          onChange={this.handleChange} value={this.state.title} />
-        </label>
-        <label htmlFor="subtitle" data-testid="subtitle-input-label">
-          Subtítulo
-          <input id="subtitle" type="text" data-testid="subtitle-input" 
-          onChange={this.handleChange} value={this.state.subtitle} />
-        </label>
-        <label htmlFor="imagePath" data-testid="image-input-label">
-          Imagem
-          <input id="imagePath" type="text" data-testid="image-input" 
-          onChange={this.handleChange} value={this.state.imagePath} />
-        </label>
-        <label htmlFor="storyline" data-testid="storyline-input-label">
-          Sinopse
-          <textarea id="storyline" type="text" data-testid="storyline-input" 
-          onChange={this.handleChange} value={this.state.storyline}></textarea>
-        </label>
-        <label htmlFor="rating" data-testid="rating-input-label">
-          Avaliação
-          <input id="rating" type="number" data-testid="rating-input" 
-          onChange={this.handleChange} value={this.state.rating} />
-        </label>
-        <label htmlFor="genre" data-testid="genre-input-label">
-          Gênero
-          <select id="genre" data-testid="genre-input" 
-          value={this.state.genre} onChange={this.handleChange}>
-            <option value="action" data-testid="genre-option">Ação</option>
-            <option value="comedy" data-testid="genre-option">Comédia</option>
-            <option value="thriller" data-testid="genre-option">Suspense</option>
-          </select>
-        </label>
+      <div>
+        <form data-testid="add-movie-form">
+          <label htmlFor="title" data-testid="title-input-label">
+            Título
+            <input id="title" type="text" data-testid="title-input" 
+            onChange={this.handleChange} value={this.state.title} />
+          </label>
+          <label htmlFor="subtitle" data-testid="subtitle-input-label">
+            Subtítulo
+            <input id="subtitle" type="text" data-testid="subtitle-input" 
+            onChange={this.handleChange} value={this.state.subtitle} />
+          </label>
+          <label htmlFor="imagePath" data-testid="image-input-label">
+            Imagem
+            <input id="imagePath" type="text" data-testid="image-input" 
+            onChange={this.handleChange} value={this.state.imagePath} />
+          </label>
+          <label htmlFor="storyline" data-testid="storyline-input-label">
+            Sinopse
+            <textarea id="storyline" type="text" data-testid="storyline-input" 
+            onChange={this.handleChange} value={this.state.storyline}></textarea>
+          </label>
+          <label htmlFor="rating" data-testid="rating-input-label">
+            Avaliação
+            <input id="rating" type="number" data-testid="rating-input" 
+            onChange={this.handleChange} value={this.state.rating} />
+          </label>
+          <label htmlFor="genre" data-testid="genre-input-label">
+            Gênero
+            <select id="genre" data-testid="genre-input" 
+            value={this.state.genre} onChange={this.handleChange}>
+              <option value="action" data-testid="genre-option">Ação</option>
+              <option value="comedy" data-testid="genre-option">Comédia</option>
+              <option value="thriller" data-testid="genre-option">Suspense</option>
+            </select>
+          </label>
+        </form>
         <button data-testid="send-button" onClick={() => this.AddMovie(onClick)}>Adicionar filme</button>
-      </form>
+      </div>
     );
   }
 }
