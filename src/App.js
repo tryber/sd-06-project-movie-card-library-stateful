6ef47c87764d43d812f605c95054c09c352f46ee
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2020-09-18 13:37:08 -0300

    refatorando

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
-
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
+import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
+import movies from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <SearchBar />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieLibrary movies={movies} />
+        <AddMovie />
+      </div>
+    );
+  }
 }
 
 export default App;
