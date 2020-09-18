52be979358f6d3c8852fbcc89e5eb73f4a609c72
Macarena Rodriguez <macarenar697@gmail.com>
2020-09-18 17:34:05 -0300

    Falto arq BoataoAddMovie.js

---
+++ src/components/BotaoAddMovie.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class BotaoAddMovie extends React.Component {
+  render() {
+    const { handlerClick } = this.props;
+    return (
+      <button type="button" data-testid="send-button" onClick={handlerClick}>Adicionar filme</button>
+    );
+  }
+}
+
+BotaoAddMovie.propTypes = { handlerClick: PropTypes.func.isRequired };
+
+export default BotaoAddMovie;
