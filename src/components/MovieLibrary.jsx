aaa2e35bf63fc9b8aaad35159a90dea70c6a2224
Bruna Franciele <brunafranciele1902@gmail.com>
2020-09-18 17:58:21 -0300

    novo pull

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+// import movies from '../data';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
   render() {
     return (
       <div>
+        <SearchBar searchText={this.searchText} bookmarkedOnly={this.bookmarkedOnly} 
+        selectedGenre={this.selectedGenre}
+/>
         <AddMovie />
+        <MovieList movies={this.state.movies} />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf.isRequired };
 
 export default MovieLibrary;
