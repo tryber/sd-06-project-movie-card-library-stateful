1d01e5935ec073c060a86d94f08a50ff56d6e896
Alessandra <itsalematos@hotmail.com>
2020-09-18 16:48:55 -0400

    teste

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+  }
   render() {
     return (
       <div>
+        <SearchBar searchText={this.searchText}
+        bookmarkedOnly={this.bookmarkedOnly} selectedGenre={this.selectedGenre} 
+        />
         <AddMovie />
+        <MovieList movies={this.state.movies}/>
       </div>
     );
   }
 }
+
 export default MovieLibrary;
