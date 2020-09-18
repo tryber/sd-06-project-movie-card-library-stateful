7887cd614f270d7974d5ea03ab6923a69a6bc812
Julia Porto <juportob@gmail.com>
2020-09-18 12:24:10 -0300

    First commit - Project Movie Card Library Stateful

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,15 @@
-// implement MovieLibrary component here
+import React from 'react';
+import AddMovie from './AddMovie'
+
+class MovieLibrary extends React.Component {
+  render () {
+    return (
+      <div>
+        <AddMovie />
+      </div>
+
+    )
+  }
+}
+
+export default MovieLibrary;
