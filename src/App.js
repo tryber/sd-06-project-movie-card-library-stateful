3e3525284cee9ac32b281cc452cef5b6b132cdc2
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-09-18 13:03:16 -0300

    Acertando CC

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header';
-import MovieLibrary from './components/MovieLibrary';
 import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie'
+import AddMovie from './components/AddMovie';
 
 function App() {
   return (
