f76dc16c61d8c8af7c18f55b220261502ba5c6f6
Anderson <andersoni.trybe@gmail.com>
2020-09-18 18:12:48 -0300

    CodeClimate fixes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -14,7 +14,7 @@ class AddMovie extends Component {
       storyLine: '',
       rating: 0,
       genre: 'action',
-    }
+    };
   }
 
   handleChange({ target }) {
@@ -48,13 +48,19 @@ class AddMovie extends Component {
           <label data-testid="title-input-label" htmlFor="title-input">Título</label>
           <input data-testid="title-input" value={this.state.title} onChange={this.handleChange} />
           <label data-testid="subtitle-input-label" htmlFor="subtitle-input">Subtítulo</label>
-          <input data-testid="subtitle-input" value={this.state.subtitle} onChange={this.handleChange} />
+          <input 
+            data-testid="subtitle-input" value={this.state.subtitle} onChange={this.handleChange}
+          />
           <label data-testid="image-input-label" htmlFor="image-input">Imagem</label>
-          <input data-testid="image-input" value={this.state.imagePath} onChange={this.handleChange} />
+          <input
+            data-testid="image-input" value={this.state.imagePath} onChange={this.handleChange}
+          />
           <label data-testid="storyline-input-label" htmlFor="storyline-input">Sinópse</label>
           <textarea data-testid="storyline-input" value={this.state.storyline} onChange={this.handleChange} />
           <label data-testid="rating-input-label" htmlFor="rating-input">Avaliação</label>
-          <input data-testid="rating-input" value={this.state.rating} onChange={this.handleChange} />
+          <input
+            data-testid="rating-input" value={this.state.rating} onChange={this.handleChange}
+          />
           <label data-testid="genre-input-label" htmlFor="genre-option">Gênero</label>
           <select data-testid="genre-option">
             <option data-testid="genre-option" value="action">Ação</option>
