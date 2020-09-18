648eef7c15f5820dff54a8f5a7caa7dc9fb4a34a
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-18 00:09:37 -0300

    requisito MovieLibrary - incompleto (sem callbacks)

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,38 @@
 // implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import data from '../data';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: data,
+    };
+  }
+
+  render() {
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={searchText}
+          bookmarkedOnly={bookmarkedOnly}
+          selectedGenre={selectedGenre}
+        />
+        <MovieList
+          movies={movies}
+        />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
