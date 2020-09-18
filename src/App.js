9c854bd18d3f21b8273bd487a77b0efc32b520ee
Pablo Santos <pablo.trybe@gmail.com>
2020-09-17 22:07:17 -0300

    Rendering first components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-
+import movies from './data.js';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import SearchBar from './components/SearchBar.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
+      <SearchBar />
+      <MovieList movies={movies} />
     </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,42 @@
 // implement SearchBar component here
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange,
+    } = this.props;
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">Inclui o texto:</label>
+        <input
+          type="text"
+          data-testid="text-input"
+          value={searchText}
+          onChange={onSearchTextChange}
+        />
+        <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
+        <input
+          type="checkbox"
+          data-testid="checkbox-input"
+          checked="bookmarkedOnly"
+          onChange={onBookmarkedChange}
+        />
+        <label data-testid="select-input-label">Filtrar por gênero</label>
+        <select
+          data-testid="select-input"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
+        >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
