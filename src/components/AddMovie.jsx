1321af7db97c2585021753f7a1a4aba9758e29ad
Bruna Franciele <brunafranciele1902@gmail.com>
2020-09-18 14:06:14 -0300

    FFF

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,66 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+class AddMovie extends Component{
+  constructor() {
+    super()
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCLick = this.handleCLick.bind(this);
+    
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: '',
+    }
+  }
+  handleChange( {target} )  {
+    let { name, value } = target;
+    this.setState({
+      [name]: value
+    })
+  }
+  handleCLick() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    })
+  }
+  render() {
+    const { onClick } = this.props
+    return(
+      <form data-testid="add-movie-form"> 
+        <label data-testid="title-input-label">Título
+          <input name="title" data-testid="title-input" value={this.state.title} onChange={this.handleChange} />
+        </label>
+        <label data-testid="subtitle-input-label">Subtítulo
+          <input name="subtitle" data-testid="subtitle-input" value={this.state.subtitle} onChange={this.handleChange} />
+        </label>
+        <label data-testid="image-input-label">Imagem
+          <input name="imagePath" data-testid="image-input" value={this.state.imagePath} onChange={this.handleChange}></input>
+        </label>
+        <label data-testid="storyline-input-label">Sinopse
+          <textarea data-testid="storyline-input" name="storyline" value={this.state.storyline} onChange={this.handleChange}></textarea>
+        </label>
+        <label data-testid="rating-input-label">Avaliação
+          <input name="rating" data-testid="rating-input" type="number" value={this.state.rating} onChange={this.handleChange}></input>
+        </label>
+        <label data-testid="genre-input-label">Gênero
+          <select name="genre" value={this.state.genre} data-testid="genre-input" onChange={this.handleChange} >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+        <button data-testid="send-button" onClick={this.handleCLick}>Adicionar filme</button>
+      </form>
+    )
+  }
+}
+export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,37 @@
-// implement SearchBar component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class SearchBar extends React.Component {
+  render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange,
+      selectedGenre, onSelectedGenreChange } = this.props
+    return (
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label">Inclui o texto:
+        <input data-testid="text-input" onChange={onSearchTextChange}
+            value={searchText} type="text" /></label>
+        <label data-testid="checkbox-input-label">Mostrar somente favoritos<input type="checkbox" data-testid="checkbox-input"
+          checked={bookmarkedOnly} onChange={onBookmarkedChange} /></label>
+        <label data-testid="select-input-label">Filtrar por gênero<select value={selectedGenre}
+          onChange={onSelectedGenreChange} data-testid="select-input">
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+        </label>
+      </form>
+    );
+  }
+}
+
+// SearchBar.propTypes = {
+// searchText: PropTypes.string.isRequerid,
+// onSearchTextChange: PropTypes.callback.isRequerid,
+// bookmarkedOnly: PropTypes.callback.isRequerid,
+// onBookmarkedChange: PropTypes.callback.isRequerid,
+// selectedGenre: PropTypes.string.isRequerid,
+// onSelectedGenreChange: PropTypes.callback.isRequerid,
+// }
+
+export default SearchBar;
