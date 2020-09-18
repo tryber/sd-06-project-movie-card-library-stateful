f31cd7d020fb29644cb9c8f13fcd8972af9b8e0b
Anderson R <misterregisslv@gmail.com>
2020-09-18 03:11:59 -0300

    refatorando SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { searchText, onSearchTextChange, bookmarkedOnly } = this.props;
+    const { onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form action="" data-testid="search-bar-form">
        <label data-testid="text-input-label">
