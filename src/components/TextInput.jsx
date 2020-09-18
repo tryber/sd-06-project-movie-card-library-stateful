726dabf24f1d89ec8f490468c8933490c376cc98
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-09-18 16:52:57 -0300

    arquivos nao comittados anteriormente

---
+++ src/components/TextInput.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class TextInput extends Component {
+  render() {
+    const searchText = this.props.searchText;
+    const onSearchTextChange = this.props.onSearchTextChange;
+    return (
+      <label htmlFor="text" data-testid="text-input-label">Inclui o texto:
+        <input
+          id="text"
+          value={searchText}
+          onChange={onSearchTextChange}
+          data-testid="text-input"
+        />
+      </label>
+    );
+  }
+}
+
+export default TextInput;
