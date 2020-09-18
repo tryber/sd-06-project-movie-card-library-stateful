1407f780af3dd722a389c3fdd9cd4fef836400db
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-09-18 13:29:38 -0300

    Acertando CC

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-import MovieList from './MovieList';
 
 class MovieLibrary extends React.Component {
   render() {
