cee672c96c81af31d28a14677861d02bd57e9a41
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-18 16:13:32 -0300

    Renderizando SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,17 +4,22 @@ class SearchBar extends Component {
   render() {
     return (
       <div>
-        <form data-testid="search-bar-form">
-          <label htmlFor="searchText">
+        <form data-testid="search-bar-form" className="search-bar-form">
+          <label data-testid="text-input-label" htmlFor="searchText">
+            <p>Inclui o texto:</p>
             <input name="searchText" type="text" />
           </label>
-          <label htmlFor="bookmarkedOnly">
+          <label data-testid="checkbox-input-label" htmlFor="bookmarkedOnly">
             <input name="bookmarkedOnly" type="checkbox" />
+            Mostrar somente favoritos
           </label>
-          <label htmlFor="selectedGenre">
-            Escolha:
-            <select name="selectedGenre">
-              <option value="test">teste</option>
+          <label data-testid="select-input-label" htmlFor="selectedGenre">
+            <p>Filtrar por gênero</p>
+            <select data-testid="select-input" name="selectedGenre">
+              <option data-testid="select-option" value="">Todos</option>
+              {/* <option data-testid="select-option" value={action}>Ação</option>
+              <option data-testid="select-option" value={comedy}>Comédia</option>
+              <option data-testid="select-option" value={thriller}>Suspense</option> */}
             </select>
           </label>
 
