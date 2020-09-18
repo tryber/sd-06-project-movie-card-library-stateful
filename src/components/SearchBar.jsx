06f96ca0a6d43ab5139a71eb4f512db5540c891c
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-09-18 15:18:04 -0300

    fix code climate spacing SearchBar.js

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,28 +4,28 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <form 
-        data-testid="search-bar-form">
+      <form
+        data-testid="search-bar-form"
+      >
 
-        <label data-testid="text-input-label" htmlFor="">
+        <label data-testid="text-input-label" htmlFor="search-text">
           Inclui o texto:
         </label>
-        <input type="text" data-testid="text-input" />
+        <input type="text" data-testid="text-input" id="search-text" />
 
-        <label data-testid="checkbox-input-label" htmlFor="">
+        <label data-testid="checkbox-input-label" htmlFor="search-check">
           Mostrar somente favoritos
         </label>
-        <input type="checkbox" data-testid="checkbox-input" />
+        <input type="checkbox" data-testid="checkbox-input" id="search-check"/>
 
-        <label data-testid="select-input-label" htmlFor="">Filtrar por gênero</label>
-        <select>
+        <label data-testid="select-input-label" htmlFor="search-select">Filtrar por gênero</label>
+        <select id="search-select">
           <option value="">Todos</option>
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
           <option value="thriller">Suspense</option>
         </select>
-        
-      </form>    
+      </form>
     );
   }
 }
