add3a795f34fe8d52892389256fd7ef750db8a20
paulo-lins <paulolins100@gmail.com>
2020-09-18 18:11:51 -0300

    AddMovie

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -20,7 +20,8 @@ class SearchBar extends React.Component {
             <input type="text" {...inputSearch} data-testid="text-input" />
           </label>
           <label htmlFor="checkedbox" data-testid="checkbox-input-label">
-            <input id="checkedbox" type="checkbox" checked="bookmarkedOnly" onChange="onBookmarkedChange" data-testid="checkbox-input" />
+            <input id="checkedbox" type="checkbox" checked="bookmarkedOnly"
+              onChange="onBookmarkedChange" data-testid="checkbox-input" />
           </label>
           <label htmlFor="select" data-testid="select-input-label">
             <select id="select" value="selectedGenre">
