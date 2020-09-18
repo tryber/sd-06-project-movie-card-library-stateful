5191dccd58383008306d1fe945af1779653cce13
Daniel Moura <darthur.moura@gmail.com>
2020-09-18 18:26:42 -0300

    AddMovie > render imagePath input

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -6,6 +6,7 @@ class AddMovie extends Component {
 
     this.renderTitleInput = this.renderTitleInput.bind(this);
     this.renderSubtitleInput = this.renderSubtitleInput.bind(this);
+    this.renderImagePathInput = this.renderImagePathInput.bind(this);
     this.handleChange = this.handleChange.bind(this);
 
     this.state = {
@@ -56,11 +57,27 @@ class AddMovie extends Component {
     );
   }
 
+  renderImagePathInput() {
+    return (
+      <label data-testid="image-input-label" htmlFor="image">
+        Imagem
+        <input
+          data-testid="image-input"
+          name="imagePath"
+          onChange={this.handleChange}
+          type="text"
+          value={this.state.imagePath}
+        />
+      </label>
+    );
+  }
+
   render() {
     return (
       <form data-testid="add-movie-form">
         {this.renderTitleInput()}
         {this.renderSubtitleInput()}
+        {this.renderImagePathInput()}
       </form>
     );
   }
