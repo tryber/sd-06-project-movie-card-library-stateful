9c6b50a767652e2c5755bc3c7f78b2b7cc68fcd9
ilanribeiro <ilanribeiro@gmail.com>
2020-09-18 15:27:04 -0300

    validated text input label "Inclui o texto"

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,6 +6,7 @@ class SearchBar extends Component {
       <div>
         <p>Search Bar</p>
         <form action="" data-testid="search-bar-form">
+          <label htmlFor="" data-testid="text-input-label">Inclui o texto</label>
           <input type="text" name="" id="" data-testid="text-input"/>
         </form>
       </div>
