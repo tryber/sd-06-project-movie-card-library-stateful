71e24be0d5a04bbdf1a8e00eb358615e50d40abf
ilanribeiro <ilanribeiro@gmail.com>
2020-09-18 19:34:45 -0300

    label "Titulo" ok. This.state.title="" ok. value of title change, ok

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 
 class AddMovie extends Component {
   render() {
+    this.state ={
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+
     return (
       <div>
         <form action="" data-testid="add-movie-form">
+          <label htmlFor="" data-testid="title-input-label">Título</label>
+          <input type="text" name="" id="" data-testid="title-input"/>
 
         </form>
       </div>
