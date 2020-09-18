546d94c6f5fb126c3d3edd657a6b19c5befddd48
Julia Porto <juportob@gmail.com>
2020-09-18 13:19:42 -0300

    Fixing codeclimate

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,12 +12,12 @@ class AddMovie extends Component {
       storyline: '',
       rating: 0,
       genre: '',
-    }
-  };
+    };
+  }
   handleChange({ target }) {
     const { name, value } = target;
     this.setState({ [name]: value });
-  }
+  };
   handleCLick() {
     const { onClick } = this.props;
     onClick(this.state);
@@ -35,27 +35,32 @@ class AddMovie extends Component {
       <form data-testid="add-movie-form"> 
         <label data-testid="title-input-label">Título
           <input name="title" data-testid="title-input" value={this.state.title}
-          onChange={this.handleChange} />
+            onChange={this.handleChange}
+          />
         </label>
         <label data-testid="subtitle-input-label">Subtítulo
           <input name="subtitle" data-testid="subtitle-input" value={this.state.subtitle}
-          onChange={this.handleChange} />
+          onChange={this.handleChange}
+          />
         </label>
         <label data-testid="image-input-label">Imagem
           <input name="imagePath" data-testid="image-input" value={this.state.imagePath}
-          onChange={this.handleChange} />
+            onChange={this.handleChange}
+          />
         </label>
         <label data-testid="storyline-input-label">Sinopse
           <textarea data-testid="storyline-input" name="storyline" value={this.state.storyline}
-          onChange={this.handleChange} />
+            onChange={this.handleChange}
+          />
         </label>
         <label data-testid="rating-input-label">Avaliação
           <input name="rating" data-testid="rating-input" type="number" value={this.state.rating}
-          onChange={this.handleChange} />
+            onChange={this.handleChange}
+          />
         </label>
         <label data-testid="genre-input-label">Gênero
           <select name="genre" value={this.state.genre} data-testid="genre-input"
-          onChange={this.handleChange}>
+            onChange={this.handleChange}>
             <option data-testid="genre-option" value="action">Ação</option>
             <option data-testid="genre-option" value="comedy">Comédia</option>
             <option data-testid="genre-option" value="thriller">Suspense</option>
@@ -63,7 +68,7 @@ class AddMovie extends Component {
         </label>
         <button data-testid="send-button" onClick={this.handleCLick}>Adicionar filme</button>
       </form>
-    )
-  }
+    );
+  };
 }
 export default AddMovie;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,16 +3,20 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange
+    } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label">Inclui o texto:</label>
         <input data-testid="text-input" value={searchText} 
-        onChange={onSearchTextChange} type="text" />
+          onChange={onSearchTextChange} type="text"
+        />
         <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
         <input type="checkbox" data-testid="checkbox-input" checked={bookmarkedOnly}
-        onChange={onBookmarkedChange} />
+          onChange={onBookmarkedChange}
+        />
 
         <label data-testid="select-input-label">Filtrar por gênero</label>
         <select data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange}>
