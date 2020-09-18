52c58f66563a138b3cc08a7a0f9f7786ebc817b5
Anderson <andersoni.trybe@gmail.com>
2020-09-18 13:21:04 -0300

    AddMovie

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import './App.css';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
+import MovieList from './components/MovieList';
+import movies from './data';
+import AddMovie from './components/AddMovie';
 
 function App() {
   // const { search } = this.props;
@@ -10,12 +13,14 @@ function App() {
     <div className="App">
       <Header />
       <SearchBar />
+      <AddMovie />
       {/* searchText={search.searchText}
       onSearchTextChange={search.onSearchTextChange}
       bookmarkedOnly={search.bookmarkedOnly}
       onBookMarkedOnly={search.onBookMarkedOnly}
       selectedGenre={search.selectedGenre}
       onSelectedGenre={search.onSelectedGenre} */}
+      <MovieList movies={movies}/>
     </div>
   );
 }
