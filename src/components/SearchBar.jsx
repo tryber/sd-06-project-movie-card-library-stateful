521b3cdc9faea0280b15c96de55a6bf1ff5845d1
Rick Sousa <rickssti@outlook.com>
2020-09-18 02:20:43 -0300

    CodeClimate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,35 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
-  render() {
-    const {
-      searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange,
-    } = this.props;
-    const a = onSearchTextChange;
-    const b = onBookmarkedChange; const c = bookmarkedOnly; const d = onSelectedGenreChange;
 
+  selectGenerate() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label data-testid="select-input-label" htmlFor="gen-select">
+          Filtrar por gênero
+        <select
+          id="gen-select"
+          data-testid="select-input"
+          value={selectedGenre}
+          onChange={onSelectedGenreChange}
+        >
+          <option data-testid="select-option" value="">Todos</option>
+          <option data-testid="select-option" value="action">Ação</option>
+          <option data-testid="select-option" value="comedy">Comédia</option>
+          <option data-testid="select-option" value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  checkboxGenerate() {
+    const { bookmarkedOnly,  onBookmarkedChange } = this.props;
+    return (
+      <label data-testid="checkbox-input-label" htmlFor="fav-check">
+        Mostrar somente favoritos
+        <input type="checkbox" id="fav-check" data-testid="checkbox-input" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
+      </label>
+    );
+  }
+
+  render() {
+    const { searchText, onSearchTextChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="text-input">
           Inclui o texto
-          <input type="text" id="text-input" data-testid="text-input" value={searchText} onChange={a} />
+          <input
+            type="text" 
+            id="text-input"
+            data-testid="text-input"
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
         </label>
-
-        <label data-testid="checkbox-input-label" htmlFor="fav-check">
-          Mostrar somente favoritos
-          <input type="checkbox" id="fav-check" data-testid="checkbox-input" checked={c} onChange={b} />
-        </label>
-
-        <label data-testid="select-input-label" htmlFor="gen-select">
-          Filtrar por gênero
-          <select id="gen-select" data-testid="select-input" value={selectedGenre} onChange={d}>
-            <option data-testid="select-option" value="">Todos</option>
-            <option data-testid="select-option" value="action">Ação</option>
-            <option data-testid="select-option" value="comedy">Comédia</option>
-            <option data-testid="select-option" value="thriller">Suspense</option>
-          </select>
-        </label>
+        {this.checkboxGenerate()}
+        {this.selectGenerate()}
       </form>
     );
   }
