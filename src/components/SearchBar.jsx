aef94feafd6448f4f3084cbae8cf93a341f2fce4
ilanribeiro <ilanribeiro@gmail.com>
2020-09-18 17:38:44 -0300

    Searchbar.jsx completed - all tests ok - adjust codeclimate issues

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
@@ -8,7 +9,7 @@ class SearchBar extends Component {
       bookmarkedOnly,
       onBookmarkedChange,
       selectedGenre,
-      onSelectedGenreChange
+      onSelectedGenreChange,
     } = this.props;
 
     return (
@@ -16,37 +17,41 @@ class SearchBar extends Component {
         <p>Search Bar</p>
         <form action="" data-testid="search-bar-form">
 
-          <label
-            for="text-input"
-            htmlFor=""
-            data-testid="text-input-label">Inclui o texto
+          <label htmlFor="text-input" data-testid="text-input-label">
+            Inclui o texto
           </label>
 
           <input
+            id="text-input"
             type="text"
             name=""
-            id="text-input"
             data-testid="text-input"
             value={searchText}
             onChange={onSearchTextChange}
           />
 
-          <label
-            htmlFor="checkbox-input"
-            data-testid="checkbox-input-label">Mostrar somente favoritos
+          <label htmlFor="checkbox-input" data-testid="checkbox-input-label">
+            Mostrar somente favoritos
           </label>
 
           <input
+            id="checkbox-input"
             type="checkbox"
             name=""
-            id="checkbox-input"
             data-testid="checkbox-input"
             checked value={bookmarkedOnly}
             onChange={onBookmarkedChange}
           />
 
-          <label for="gender" data-testid="select-input-label">Filtrar por gênero</label>
-          <select name="gender" id="gender" form="" data-testid="select-input" value={selectedGenre} onChange={onSelectedGenreChange} >
+          <label htmlFor="gender" data-testid="select-input-label">Filtrar por gênero</label>
+          <select
+            name="gender"
+            id="gender"
+            form=""
+            data-testid="select-input"
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+          >
             <option data-testid="select-option" value="">Todos</option>
             <option data-testid="select-option" value="action">Ação</option>
             <option data-testid="select-option" value="comedy">Comédia</option>
@@ -54,8 +59,17 @@ class SearchBar extends Component {
           </select>
         </form>
       </div>
-    )
+    );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.string.isRequired,
+  bookmarkedOnly: PropTypes.string.isRequired,
+  onBookmarkedChange: PropTypes.string.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.string.isRequired,
+};
 
 export default SearchBar;
