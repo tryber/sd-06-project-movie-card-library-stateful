ac1eb33ce090760067ea6a6ff0cf6f0ff07a5252
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-18 14:40:28 -0300

    Add Movie requirements 06

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -192,15 +192,15 @@ AddMovie.defaultProps = {
 };
 
 AddMovie.propTypes = {
-  title: propTypes.string,
-  subtitle: propTypes.string,
-  imagePath: propTypes.string,
-  storyline: propTypes.string,
-  rating: propTypes.number,
-  genre: propTypes.string,
-  onChange: propTypes.func,
-  submit: propTypes.func,
-  onClick: propTypes.func,
+  // title: propTypes.string,
+  // subtitle: propTypes.string,
+  // imagePath: propTypes.string,
+  // storyline: propTypes.string,
+  // rating: propTypes.number,
+  // genre: propTypes.string,
+  // onChange: propTypes.func,
+  // submit: propTypes.func,
+  // onClick: propTypes.func,
 };
 
 export default AddMovie;
