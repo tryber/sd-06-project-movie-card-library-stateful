7d780daf347bc651c4005bd6f42de2651a02b1c7
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-18 19:01:02 -0300

    Add handleclick in button

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
   constructor() {
@@ -12,6 +13,7 @@ class AddMovie extends Component {
       genre: 'action',
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange({ target }) {
@@ -19,43 +21,88 @@ class AddMovie extends Component {
     this.setState({ [name]: value });
   }
 
+  handleClick() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
   render() {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
-    const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title-input">
           Título
-          <input data-testid="title-input" name="title" value={title} onChange={this.handleChange} />
+          <input
+            data-testid="title-input"
+            name="title" value={title}
+            onChange={this.handleChange}
+          />
         </label>
         <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
           Subtítulo
-          <input data-testid="subtitle-input" name="subtitle" value={subtitle} onChange={this.handleChange} />
+          <input
+            data-testid="subtitle-input"
+            name="subtitle" value={subtitle}
+            onChange={this.handleChange}
+          />
         </label>
         <label data-testid="image-input-label" htmlFor="image-input">
           Imagem
-          <input data-testid="image-input" name="imagePath" value={imagePath} onChange={this.handleChange} />
+          <input
+            data-testid="image-input"
+            name="imagePath" value={imagePath}
+            onChange={this.handleChange}
+          />
         </label>
         <label data-testid="storyline-input-label" htmlFor="storyline-input">
           Sinopse
-          <textarea data-testid="storyline-input" name="storyline" value={storyline} onChange={this.handleChange} />
+          <textarea
+            data-testid="storyline-input"
+            name="storyline" value={storyline}
+            onChange={this.handleChange}
+          />
         </label>
         <label data-testid="rating-input-label" htmlFor="rating-input">
           Avaliação
-          <input data-testid="rating-input" name="rating" type="number" value={rating} onChange={this.handleChange} />
+          <input
+            data-testid="rating-input"
+            name="rating" type="number"
+            value={rating}
+            onChange={this.handleChange}
+          />
         </label>
         <label data-testid="genre-input-label" htmlFor="genre-input">
           Gênero
-          <select data-testid="genre-input" name="genre" value={genre} onChange={this.handleChange}>
+          <select
+            data-testid="genre-input"
+            name="genre" value={genre}
+            onChange={this.handleChange}
+          >
             <option data-testid="genre-option" value="action">Ação</option>
             <option data-testid="genre-option" value="comedy">Comédia</option>
             <option data-testid="genre-option" value="thriller">Suspense</option>
           </select>
         </label>
-        <button data-testid="send-button" type="button" onClick={onClick}>Adicionar filme</button>
+        <button
+          data-testid="send-button"
+          type="button"
+          onClick={this.handleClick}
+        >
+          Adicionar filme
+        </button>
       </form>
     );
   }
 }
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
 
 export default AddMovie;
