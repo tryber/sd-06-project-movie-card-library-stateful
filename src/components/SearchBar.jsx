b3b0bdf231f48ff5ef0f83a6a885f8ec1ae896e7
William Gomes <williamjog@hotmail.com>
2020-09-17 21:52:17 -0300

    Fixing Code Climate Issues | Using Functions to create form

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,40 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 
 class SearchBar extends React.Component {
+
+  generateFilterByGenderSelectOptions() {
+    const { selectedGenre, onSelectedGenreChange } = this.props;
+    return (
+      <label htmlFor="genre">
+        Filtrar por gênero
+        <select value={selectedGenre} onChange={onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     const {
       searchText,
       onSearchTextChange,
       bookmarkedOnly,
       onBookmarkedChange,
-      selectedGenre,
-      onSelectedGenreChange,
     } = this.props;
 
     return (
       <form>
         <label data-testid="text-input-label" htmlFor="text">
           Inclui o texto:
-          <input data-testid="text-input" type="text" value={searchText} onChange={onSearchTextChange} />
+          <input
+            data-testid="text-input"
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+          />
         </label>
         <label data-testid="checkbox-input-label" htmlFor="favorites">
           Mostrar somente favoritos
           <input data-testid="checkbox-input" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange} />
         </label>
-        <label htmlFor="genre">
-          Filtrar por gênero
-          <select value={selectedGenre} onChange={onSelectedGenreChange}>
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </label>
+        {this.generateFilterByGenderSelectOptions()}
       </form>
     );
   }
 }
 
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
