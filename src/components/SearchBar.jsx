06001b3fedfd2f04334a896520aeb145d175bc3c
Isadora Koga <isadorakoga710@gmail.com>
2020-09-18 17:09:00 -0300

    Add searchBar pra dar pull na master

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,13 +14,14 @@ class SearchBar extends React.Component {
         <label data-testid="checkbox-input-label" htmlFor="checkbox-input">
           Mostrar somente favoritos:
           <input type="checkbox" className="text-input"
-            onChange={this.props.onBookmarkedChange} data-testid="checkbox-input" value={bookmarkedOnly} checked="bookmarkedOnly"
+          onChange={this.props.onBookmarkedChange} data-testid="checkbox-input" value={bookmarkedOnly} checked="bookmarkedOnly"
           />
         </label>
         <label data-testid="select-input-label" htmlFor="select-input">
           Filtrar por gênero:
-          <select data-testid="select-input"
-            onChange={this.props.onSelectedGenreChange} value={selectedGenre}>
+          <select
+          data-testid="select-input" onChange={this.props.onSelectedGenreChange} value={selectedGenre}
+          >
             <option data-testid="select-option" value="">Todos</option>
             <option data-testid="select-option" value="action">Ação</option>
             <option data-testid="select-option" value="comedy">Comédia</option>
@@ -39,6 +40,6 @@ SearchBar.propType = {
   onSearchTextChange: PropType.func.isRequired,
   onBookmarkedChange: PropType.func.isRequired,
   onSelectedGenreChange: PropType.func.isRequired,
-  }.isRequired;
+}.isRequired;
 
 export default SearchBar;
