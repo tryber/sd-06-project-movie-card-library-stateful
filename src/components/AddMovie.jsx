a6ca21b9ede66394a9ccf58a0d0c48526f57abd6
Pablo Santos <pablo.trybe@gmail.com>
2020-09-18 20:17:15 -0300

    Starting AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,90 @@
-// implement AddMovie component here
+import React, { Component } from 'react';
+
+class AddMovie extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      subtitle: '',
+      imagePath: '',
+      storyline: '',
+      rating: '0',
+      genre: 'action',
+    };
+    this.putTitleAndSubtitle = this.putTitleAndSubtitle.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  // handleChange ({ target }) {
+  //   const { name } = target;
+  //   const value = target.type === 'checkbox' ? target.checked : target.value;
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // }
+
+  putTitleAndSubtitle(inputLabel, label, data, type, id) {
+    const { title } = this.state;
+    return (
+      <label data-testid={inputLabel} htmlFor="movie">
+        {label}
+        <input
+          id={id}
+          data-testid={data}
+          type={type}
+          name={id}
+          value={title}
+          onChange={(event) => this.setState({ title: event.target.value })}
+        />
+      </label>
+    );
+  }
+
+  putTextArea(inputLabel, label, data, type, id) {
+    const { title } = this.state;
+    return (
+      <label data-testid={inputLabel} htmlFor="synopsis">
+        {label}
+        <input
+          id={id}
+          data-testid={data}
+          type={type}
+          name={id}
+          value={title}
+          onChange={(event) => this.setState({ title: event.target.value })}
+        />
+      </label>
+    );
+  }
+
+  putRating(inputLabel, label, data, type, id) {
+    const { rating } = this.state;
+    return (
+      <label data-testid={inputLabel} htmlFor="rating">
+        {label}
+        <input
+          id={id}
+          data-testid={data}
+          type={type}
+          name={id}
+          value={rating}
+          onChange={(event) => this.setState({ rating: event.target.value })}
+        />
+      </label>
+    );
+  }
+
+  render() {
+    return (
+      <form>
+        {this.putTitleAndSubtitle('title-input-label', 'Título', 'title-input', 'text', 'txtTitle')}
+        {this.putTitleAndSubtitle('subtitle-input-label', 'Subtítulo', 'subtitle-input', 'text', 'txtSubtitle')}
+        {this.putTitleAndSubtitle('image-input-label', 'Imagem', 'image-input', 'text', 'txtImage')}
+        {this.putTextArea('storyline-input-label', 'Sinopse', 'storyline-input', 'textArea', 'txtSynopsis')}
+        {this.putTitleAndSubtitle('rating-input-label', 'Avaliação', 'rating-input', 'number', 'txtRating')}
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,14 @@
-// implement MovieLibrary component here
+// // implement MovieLibrary component here
+// import React, { Component } from 'react';
+// import AddMovie from './AddMovie.jsx';
+// import MovieList from './MovieList.jsx';
+
+// class MovieLibrary extends Component {
+//   render() {
+//     return(
+//       <AddMovie />
+//     );
+//   }
+// }
+
+// export default MovieLibrary;
