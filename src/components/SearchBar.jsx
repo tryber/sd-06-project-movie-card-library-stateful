b407143aafe7c27ca4df863d01b151b4fc9ef972
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2020-09-18 15:10:35 -0300

    fix code climate SearchBar.js

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,29 +2,30 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  render(){
+  render() {
     return (
-      <form data-testid="search-bar-form"
-      >
-        <label data-testid="text-input-label">
+      <form 
+        data-testid="search-bar-form">
+
+        <label data-testid="text-input-label" htmlFor="">
           Inclui o texto:
         </label>
         <input type="text" data-testid="text-input" />
 
-        <label data-testid="checkbox-input-label">
+        <label data-testid="checkbox-input-label" htmlFor="">
           Mostrar somente favoritos
         </label>
         <input type="checkbox" data-testid="checkbox-input" />
 
-        <label data-testid="select-input-label">Filtrar por gênero</label>
+        <label data-testid="select-input-label" htmlFor="">Filtrar por gênero</label>
         <select>
           <option value="">Todos</option>
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
           <option value="thriller">Suspense</option>
         </select>
-      </form>
-      
+        
+      </form>    
     );
   }
 }
