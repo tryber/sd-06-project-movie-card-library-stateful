d70d6d87e002b6a8f7667f6f26ce2839bee4d4f3
DehSartoreto <fixsartoreto@gmail.com>
2020-09-18 18:40:19 -0300

    movieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
-import movies from '../data'
+import movies from '../data';
 
 class MovieLibrary extends React.Component {
   constructor() {
