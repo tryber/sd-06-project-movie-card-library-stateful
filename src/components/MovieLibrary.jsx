d51718d1bc47035a7804b16764d9c98060922a44
Alessandra <itsalematos@hotmail.com>
2020-09-18 14:21:43 -0400

    testando code climate

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,13 @@
-// codar aqui
+import React from 'react';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <AddMovie />
+      </div>
+    );
+  }
+}
+export default MovieLibrary;
