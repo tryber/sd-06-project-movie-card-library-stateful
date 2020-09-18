1e993da0f1327961c7e724f7286dce8eccb27a1b
Micaeli Teixeira <luime6@gmail.com>
2020-09-18 20:29:32 -0300

    Implementando os codigos do App e MovieLibrary

---
+++ src/components/MovieLibrary.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
+
+class MovieLibrary extends Component {
+  render() {
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+MovieLibrary.propTypes = { movies: PropTypes.object }.isRequired;
+export default MovieLibrary;
