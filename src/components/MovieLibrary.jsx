d7467d8421c4e93cbee4f83695c5977cd5c0a0ff
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-18 02:16:08 -0300

    Requisito Library parcial

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,20 @@
-// implement MovieLibrary component here
+import React, { Component } from 'react';
+import movies from '../data';
+
+class MovieLibrary extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: movies,
+    }
+  }
+  render() {
+    return (<span>Ok</span>);
+  }
+}
+
+export default MovieLibrary;
