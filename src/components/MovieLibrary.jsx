5e40daee8c8eeb3d17f0e25e8602d29146e0db68
FlavioSugano <flavio.sugano@gmail.com>
2020-09-18 16:56:14 -0300

    MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,11 +5,67 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super()
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    }
+    this.handleSearchText = this.handleSearchText.bind(this);
+    this.handleBookmarkedChange = this.handleBookmarkedChange.bind(this);
+    this.handleGenreChange = this.handleGenreChange.bind(this);
+  }
+
+  filterWithSearchText = () => {
+    const { movies, searchText } = this.state
+    const filteredMovies = movies.filter((movie) => (
+      movie.title.toUpperCase().includes(searchText.toUpperCase()) ||
+      movie.subtitle.toUpperCase().includes(searchText.toUpperCase()) ||
+      movie.storyline.toUpperCase().includes(searchText.toUpperCase())
+    ));
+    
+    if (this.state.searchText === '') {
+      this.setState({
+        movies: this.props.movies,
+      });
+    } else {
+      this.setState({
+        movies: filteredMovies,
+      });
+    }
+  }
+
+  handleSearchText = ({ target }) => {
+    const { value } = target;
+    this.setState(() => ({
+      searchText: value,
+    }), this.filterWithSearchText)
+  }
+
+  handleBookmarkedChange = ({ target }) => {
+    // filter por bookmarked e alterar o state.movies
+    const { checked } = target;
+    this.setState({
+      bookmarkedOnly: checked,
+    })
+
+  }
+
+  handleGenreChange = () => {
+    // filter por gênero e alterar o state.movies
+  }
+  
   render() {
     return (
       <div>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <SearchBar 
+          onSearchTextChange={this.handleSearchText} searchText={this.state.searchText}
+          // onBookmarkedChange={handleBookmarkedChange}
+          // onSelectedGenreChange={handleGenreChange}
+        />
+        <MovieList movies={this.state.movies} />
         <AddMovie />
       </div>
     );
