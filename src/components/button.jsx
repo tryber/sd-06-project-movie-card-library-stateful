42de83fb118a76ffbe5c0f361b6bb3d963b3f713
Luciano Scalfone <luciano.scalfone@gmail.com>
2020-09-18 02:03:07 -0300

    vai pf

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -9,7 +9,7 @@ class Button extends Component {
       <fieldset>
         <button
           data-testid="send-button" onClick={click}
-        > 
+        >
           Adicionar filme
         </button>
       </fieldset>
