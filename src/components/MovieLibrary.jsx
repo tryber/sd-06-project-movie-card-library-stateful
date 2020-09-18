4bd16e549c25e8b06017bfccef18c51b290c42ab
Ricardo Ribeiro <ralexandre11.gmail.com>
2020-09-18 19:30:50 -0300

    Implementação do componente MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -8,6 +8,10 @@ class MovieLibrary extends Component {
   constructor(props) {
     super(props);
     this.myOnClick = this.myOnClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
@@ -16,24 +20,40 @@ class MovieLibrary extends Component {
     };
   }
 
+  onSearchTextChange({ target }) {
+    this.handleChange(target);
+  }
+
+  onBookmarkedChange({ target }) {
+    this.handleChange(target);
+  }
+
+  onSelectedGenreChange({ target }) {
+    this.handleChange(target);
+  }
+
+  handleChange(target) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  }
+
   myOnClick() {
     console.log('teste');
   }
 
   render() {
-
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        {/* <SearchBar
-          searchText={'text'}
-          onSearchTextChange={this.onSearchTextChange}
-          bookmarkedOnly={'true'}
+        <SearchBar
+          searchText={this.state.searchText}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          selectedGenre={this.state.selectedGenre}
           onBookmarkedChange={this.onBookmarkedChange}
-          selectedGenre={'Genre'}
+          onSearchTextChange={this.onSearchTextChange}
           onSelectedGenreChange={this.onSelectedGenreChange}
-        />, */}
+        />,
         <MovieList movies={this.state.movies} />
         <AddMovie onClick={this.myOnClick} />
       </div>
