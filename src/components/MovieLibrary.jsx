0353cf6ac0e86f0360e4f38fa36b35d39b5712f2
Maria Alice Guimaraes Carneiro <mguimaraes.carneiro@gmail.com>
2020-09-18 16:57:29 -0300

    Starting MovieLibrary requirements

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-
-// import MovieList from './MovieList';
-// import SearchBar from './SearchBar';
-// import AddMovie from './AddMovie';
+import propTypes from 'prop-types';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: [movies],
+    };
+  }
 
   render() {
     return (
-      <div />
+      <div>
+        <SearchBar />
+        <MovieList />
+      </div>
     );
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  }).isRequired),
+};
+
+MovieLibrary.defaultProps = {
+  movies: {
+    genre: 'unknown',
+    bookmarked: false,
+  },
+};
 
 export default MovieLibrary;
