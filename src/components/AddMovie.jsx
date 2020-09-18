43e29ab9f479e34f7b56401472edf1d11e1d0b6f
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-09-18 18:07:00 -0300

    Add select option

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -45,8 +45,7 @@ class AddMovie extends Component {
         </label>
         <label data-testid="genre-input-label" htmlFor="genre-input">
           Gênero
-          <select>
-            <textarea data-testid="genre-input" name="genre" value={genre} onChange={this.handleChange} />
+          <select data-testid="genre-input" name="genre" value={genre} onChange={this.handleChange}>
             <option data-testid="genre-option" value="action">Ação</option>
             <option data-testid="genre-option" value="comedy">Comédia</option>
             <option data-testid="genre-option" value="thriller">Suspense</option>
