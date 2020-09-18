ad5d185e74f799e4363bf25c146afc1ee3e98363
Daniel Jesus <dev.danieljs@gmail.com>
2020-09-18 19:35:48 -0300

    Adding initial states, functions: onChange, handleClick and form in render

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,11 +4,42 @@ class AddMovie extends Component {
   constructor() {
     super();
 
+    this.handleClick = this.handleClick.bind(this);
+    this.onChange = this.onChange.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  onChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({ [name]: value });
+  }
+
+  handleClick() {
+    const { onClick } = this.props;
+    onClick(this.state);
+
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
   }
 
   render() {
     return (
-
+      <form action=""></form>
     );
   }
 }
