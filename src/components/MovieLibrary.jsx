80757f1ad06a7189a762fa49412947f81bd790d3
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-09-19 15:47:31 -0300

    testes passando agora só fala as protypes e o code climat

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -8,23 +8,35 @@ class MovieLibrary extends React.Component {
     super(props);
     // const movies = this.props.movies;
     this.handleChange = this.handleChange.bind(this);
+    this.onClick = this.onClick.bind(this);
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
       movies: this.props.movies,
+      listMovies: this.props.movies,
     }
   }
   handleChange({ target }) {
     const arrayKeys = ['title', 'subtitle', 'storyline'];
     const key = target.id;
     const value = target.type === 'checkbox' ? target.checked : target.value;
+    const movies = this.state.listMovies;
     this.setState({
       [key]: value,
       movies: key === 'searchText' && value !== '' ? 
-      this.props.movies.filter(movie => arrayKeys.some(key2 => value.split('').every((letter, index) => letter === movie[key2].split('')[index]))) 
-      : this.props.movies,
+      movies.filter(movie => arrayKeys.some(key2 => movie[key2].toUpperCase().includes(value.toUpperCase())))
+      : target.type === 'checkbox' && value !== false ? movies.filter(movie => movie.bookmarked) 
+      : key === 'selectedGenre' && value !== '' ? movies.filter(movie => movie.genre === value) 
+      : movies,
     });
+  }
+  onClick(stateAddMovie) {
+    const newMovie = this.state.listMovies.concat(stateAddMovie);
+    this.setState({
+      movies: newMovie,
+      listMovies: newMovie,
+    })
   }
   render() {
     return (
@@ -38,7 +50,7 @@ class MovieLibrary extends React.Component {
         onSelectedGenreChange={this.handleChange} 
         />
         <MovieList movies={this.state.movies} />
-        <AddMovie />
+        <AddMovie onClick={this.onClick} />
       </div>
     );
   }
