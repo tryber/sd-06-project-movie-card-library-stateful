a7efb45edc49748109bc2db02a5a100d02013fd6
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2020-09-19 00:09:47 -0300

    IP

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import AddMovie from './components/AddMovie';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import SearchBar from './components/SearchBar';
+import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={movies} />
       <AddMovie />
       <SearchBar />
+      <MovieList movies={movies} />
+      <MovieLibrary />
     </div>
   );
 }
