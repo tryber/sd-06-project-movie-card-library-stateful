78fafe8337fab721d5ced656e4a1cb842d8ea20e
HCaetano <hrcgeo@gmail.com>
2020-09-19 15:42:54 -0300

    Add business logic to filter by title, subtitle and storyline. Incomplete logic for bookmarked and genre

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  render() {
-    const { movies } = this.props;
+  constructor(props) {
+    super();
 
-    return <div></div>;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      originalList: props.movies,
+      movies: props.movies
+    }
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.filterMovies = this.filterMovies.bind(this);
+  }
+
+  filterMovies(value, isItCheckBox) {
+    const originalList = this.state.originalList;
+    const filteredMovies = originalList.filter(movie => {
+      const inputValue = value.toLowerCase();
+      const isInMovieTitle = movie.title.toLowerCase().includes(inputValue);
+      const isInMovieSubtitle = movie.subtitle.toLowerCase().includes(inputValue);
+      const isInMovieStoryline = movie.storyline.toLowerCase().includes(inputValue);
+      const isItBookMarked = isItCheckBox ? movie.bookmarked : false;
+      const checkGenre = movie.genre === inputValue;
+
+      return isInMovieTitle || isInMovieSubtitle || isInMovieStoryline || isItBookMarked || checkGenre;
+    })
+
+    this.setState({ movies: filteredMovies })
+  }
+
+  handleTextChange({ target }) {
+    const isItCheckBox = target.type === 'checkbox';
+    const value = isItCheckBox ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({ [name]: value }, () => {
+      this.filterMovies(value, isItCheckBox);
+    });
+  }
+
+  render() {
+    const {searchText, bookmarkedOnly, selectedGenre, movies} = this.state;
+
+    return <div>
+      <SearchBar searchText={searchText} bookmarkedOnly={bookmarkedOnly} 
+        selectedGenre={selectedGenre} onSearchTextChange={this.handleTextChange}
+        onBookmarkedChange={this.handleTextChange} onSelectedGenreChange={this.handleTextChange}/>;
+      <MovieList movies={movies}/>
+      <AddMovie />
+    </div> 
   }
 }
 
