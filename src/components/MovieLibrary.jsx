dac9278184ae45630c05c3c17c1c3feb1aecc5ba
Gustavo Oliva <gustavociviloliva@gmail.com>
2020-09-19 12:24:19 -0300

    MovieLibrary - Adequando ao CC - 01

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -12,23 +12,13 @@ class MovieLibrary extends Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: movies,
-    }
+      movies,
+    };
     this.handleSelectedGenre = this.handleSelectedGenre.bind(this);
-    // this.filterGenre = this.filterGenre.bind(this);
     this.handleSearchInput = this.handleSearchInput.bind(this);
-    this.filterText = this.filterText.bind(this);
     this.handleFavorites = this.handleFavorites.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
-
-  // filterGenre() {
-  //   const { movies } = this.props;
-  //   const { selectedGenre } = this.state;
-  //   const newMovies = (selectedGenre === '') ? movies : movies.filter(movie => movie.genre === selectedGenre);
-    
-  //   this.setState({ movies: newMovies });
-  // }
 
   handleSelectedGenre(event) {
     const genre = event.target.value;
@@ -37,33 +27,31 @@ class MovieLibrary extends Component {
       const { selectedGenre } = this.state;
       const newMovies = (selectedGenre === '') ?
         movies :
-        movies.filter(movie => movie.genre === selectedGenre);
+        movies.filter((movie) => movie.genre === selectedGenre);
       this.setState({ movies: newMovies });
     });
   }
 
-  filterText() {
-    const { movies } = this.props;
-    const { searchText } = this.state;
-    const newMovies = movies.filter((movie) => {
-      return(
+  handleSearchInput({ target }) {
+    const { value } = target;
+    this.setState({ searchText: value }, () => {
+      const { movies } = this.props;
+      const { searchText } = this.state;
+      const newMovies = movies.filter((movie) => {
+      return (
         movie.title.includes(searchText) ||
         movie.subtitle.includes(searchText) ||
         movie.storyline.includes(searchText)
-      )
+      );
     });
     this.setState({ movies: newMovies });
-  }
-
-  handleSearchInput({ target }) {
-    const { value } = target;
-    this.setState({ searchText: value}, this.filterText);
+    });
   }
 
   filterFavorites() {
     const { movies } = this.props;
     const newMovies = (this.state.bookmarkedOnly === true) ?
-      movies.filter(movie => movie.bookmarked === this.state.bookmarkedOnly) :
+      movies.filter((movie) => movie.bookmarked === this.state.bookmarkedOnly) :
       movies;
     this.setState({ movies: newMovies });
   }
@@ -71,24 +59,24 @@ class MovieLibrary extends Component {
   handleFavorites({ target }) {
     const favorito = target.checked;
     if (favorito) {
-      this.setState({ bookmarkedOnly: true }, this.filterFavorites)
+      this.setState({ bookmarkedOnly: true }, this.filterFavorites);
     } else {
-      this.setState({ bookmarkedOnly: false }, this.filterFavorites)
+      this.setState({ bookmarkedOnly: false }, this.filterFavorites);
     }
   }
 
   handleClick(obj) {
     const { subtitle, title, imagePath, storyline, rating, genre } = obj;
     const { movies } = this.state;
-    const newMovie = { 
-      subtitle: subtitle,
-      title: title,
-      imagePath: imagePath,
-      storyline: storyline,
-      rating: rating,
-      genre: genre,  
-    }    
-    this.setState({ movies: movies.concat(newMovie) })
+    const newMovie = {
+      subtitle,
+      title,
+      imagePath,
+      storyline,
+      rating,
+      genre,
+    };
+    this.setState({ movies: movies.concat(newMovie) });
   }
 
   render() {
@@ -104,14 +92,14 @@ class MovieLibrary extends Component {
           onSearchTextChange={this.handleSearchInput}
         />
         <MovieList movies={movies} />
-        <AddMovie onClick={this.handleClick}/>
+        <AddMovie onClick={this.handleClick} />
       </div>
     );
   }
 }
 
 MovieLibrary.propTypes = {
-  movie: PropTypes.shape({
+  movies: PropTypes.shape({
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
