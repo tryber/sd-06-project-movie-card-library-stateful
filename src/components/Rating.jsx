6e644f5574c2e5460d8b616ce7c5774c34c600b4
Ana Higo <anahigo.arq@gmail.com>
2020-09-19 00:17:14 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Rating extends React.Component {
+class Rating extends Component {
   render() {
     const { rating } = this.props;
     return (
