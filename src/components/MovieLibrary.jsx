4505244dac760b7d46e0c80098f4ce7140b5719e
HCaetano <hrcgeo@gmail.com>
2020-09-19 16:12:55 -0300

    Refactor 'MovieLibrary' to solve code climate issues

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
@@ -12,8 +13,8 @@ class MovieLibrary extends Component {
       bookmarkedOnly: false,
       selectedGenre: '',
       originalList: props.movies,
-      movies: props.movies
-    }
+      movies: props.movies,
+    };
 
     this.handleTextChange = this.handleTextChange.bind(this);
     this.filterMovies = this.filterMovies.bind(this);
@@ -21,7 +22,7 @@ class MovieLibrary extends Component {
 
   filterMovies(value, isItCheckBox) {
     const originalList = this.state.originalList;
-    const filteredMovies = originalList.filter(movie => {
+    const filteredMovies = originalList.filter((movie) => {
       const inputValue = value.toLowerCase();
       const isInMovieTitle = movie.title.toLowerCase().includes(inputValue);
       const isInMovieSubtitle = movie.subtitle.toLowerCase().includes(inputValue);
@@ -29,10 +30,14 @@ class MovieLibrary extends Component {
       const isItBookMarked = isItCheckBox ? movie.bookmarked : false;
       const checkGenre = movie.genre === inputValue;
 
-      return isInMovieTitle || isInMovieSubtitle || isInMovieStoryline || isItBookMarked || checkGenre;
-    })
+      return isInMovieTitle ||
+        isInMovieSubtitle ||
+        isInMovieStoryline ||
+        isItBookMarked ||
+        checkGenre;
+    });
 
-    this.setState({ movies: filteredMovies })
+    this.setState({ movies: filteredMovies });
   }
 
   handleTextChange({ target }) {
@@ -40,22 +45,43 @@ class MovieLibrary extends Component {
     const value = isItCheckBox ? target.checked : target.value;
     const name = target.name;
 
-    this.setState({ [name]: value }, () => {
-      this.filterMovies(value, isItCheckBox);
-    });
+    this.setState({ [name]: value }, () => this.filterMovies(value, isItCheckBox));
   }
 
   render() {
-    const {searchText, bookmarkedOnly, selectedGenre, movies} = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
 
-    return <div>
-      <SearchBar searchText={searchText} bookmarkedOnly={bookmarkedOnly} 
-        selectedGenre={selectedGenre} onSearchTextChange={this.handleTextChange}
-        onBookmarkedChange={this.handleTextChange} onSelectedGenreChange={this.handleTextChange}/>;
-      <MovieList movies={movies}/>
+    return (<div>
+      <SearchBar
+        searchText={searchText}
+        bookmarkedOnly={bookmarkedOnly}
+        selectedGenre={selectedGenre}
+        onSearchTextChange={this.handleTextChange}
+        onBookmarkedChange={this.handleTextChange}
+        onSelectedGenreChange={this.handleTextChange}
+      />
+      <MovieList movies={movies} />
       <AddMovie />
-    </div> 
+    </div>);
   }
 }
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
+
+  // searchText: PropTypes.string.isRequired,
+  // onSearchTextChange: PropTypes.func.isRequired,
+  // onBookmarkedChange: PropTypes.func.isRequired,
+  // selectedGenre: PropTypes.string.isRequired,
+  // onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default MovieLibrary;
