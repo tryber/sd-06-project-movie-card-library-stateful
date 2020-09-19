06b1056434fbdc8fbf56a137199c0fdb01adbe36
flaviolunaferreira <flaviolunaferreira@gmail.com>
2020-09-19 06:29:13 -0300

    corrrigindo code climate

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -4,7 +4,7 @@ import '../App.css';
 class AddMovie extends React.Component {
   constructor() {
     super();
-    this.handleChange = this.handleChange.bind(this);
+    this.entradas = this.entradas.bind(this);
     this.state = {
       subtitle: '',
       title: '',
@@ -15,16 +15,16 @@ class AddMovie extends React.Component {
     };
   }
 
-  handleChange(e) {
-    const { name, value } = e.target;
+  entradas(event) {
+    const { name, value } = event.target;
     this.setState({ [name]: value });
   }
 
-  renderSelect() {
+  ComboBox() {
     return (
       <select
         value={this.state.genre} data-testid="genre-input" name="genre"
-        onChange={this.handleChange}
+        onChange={this.entradas} className="tipoInput"
       >
         <option value="action" data-testid="genre-option">Ação</option>
         <option value="comedy" data-testid="genre-option">Comédia</option>
@@ -33,61 +33,65 @@ class AddMovie extends React.Component {
     );
   }
 
+  textoDasEntradas() {
+    return (
+      <div className="labels">
+        <label data-testid="title-input-label" htmlFor="title">
+        Título
+        </label><br />
+        <label data-testid="subtitle-input-label" htmlFor="subtitle">
+          Subtítulo
+        </label><br />
+        <label data-testid="image-input-label" htmlFor="img">
+          Imagem
+        </label><br />
+        <label data-testid="storyline-input-label" htmlFor="sinopse">
+          Sinopse
+        </label><br /><br />
+        <label data-testid="rating-input-label" htmlFor="aval">
+          Avaliação
+        </label><br />
+        <label data-testid="genre-input-label" htmlFor="select">
+          Gênero
+        </label>
+      </div>  
+    );
+  }
+
+  localDasEntradas() {
+    return (
+      <div className="inputs">
+        <input
+          value={this.state.title} data-testid="title-input" name="title"
+          onChange={this.entradas} type="text" id="title" className="tipoInput"
+        /><br />
+        <input
+          value={this.state.subtitle} data-testid="subtitle-input" name="subtitle"
+          onChange={this.entradas} type="text" id="subtitle" className="tipoInput"
+        /><br />
+        <input
+          value={this.state.imagePath} data-testid="image-input" name="imagePath"
+          onChange={this.entradas} type="text" id="img" className="tipoInput"
+        /><br />
+        <textarea
+          value={this.state.storyline} data-testid="storyline-input" name="storyline"
+          onChange={this.entradas} id="sinopse" className="tipoInput"
+        /><br />
+        <input
+          value={this.state.rating} data-testid="rating-input" name="rating"
+          onChange={this.entradas} type="number" id="aval" className="tipoInput"
+        /><br />
+        { this.ComboBox() }
+      </div>
+    );
+  }
+
   render() {
     const onClick = this.props;
     return (
       <form className="addMovie" data-testid="add-movie-form">
-        <div className="labels">
-          <label data-testid="title-input-label" htmlFor="title">
-            Título
-          </label><br />
-          <label data-testid="subtitle-input-label" htmlFor="subtitle">
-            Subtítulo
-          </label><br />
-          <label data-testid="image-input-label" htmlFor="img">
-            Imagem
-          </label><br />
-          <label data-testid="storyline-input-label" htmlFor="sinopse">
-            Sinopse
-          </label><br /><br />
-          <label data-testid="rating-input-label" htmlFor="aval">
-            Avaliação
-          </label><br />
-          <label data-testid="genre-input-label" htmlFor="select">
-            Gênero
-          </label>
-        </div>
-
-        <div className="inputs">
-          <input
-            value={this.state.title} data-testid="title-input" name="title"
-            onChange={this.handleChange} type="text" id="title" className="tipoInput"
-          /><br />
-          <input
-            value={this.state.subtitle} data-testid="subtitle-input" name="subtitle"
-            onChange={this.handleChange} type="text" id="subtitle" className="tipoInput"
-          /><br />
-          <input
-            value={this.state.imagePath} data-testid="image-input" name="imagePath"
-            onChange={this.handleChange} type="text" id="img" className="tipoInput"
-          /><br />
-          <textarea
-            value={this.state.storyline} data-testid="storyline-input" name="storyline"
-            onChange={this.handleChange} id="sinopse" className="tipoInput"
-          /><br />
-          <input
-            value={this.state.rating} data-testid="rating-input" name="rating"
-            onChange={this.handleChange} type="number" id="aval" className="tipoInput"
-          />
-          <select
-            value={this.state.genre} data-testid="genre-input" name="genre"
-            onChange={this.handleChange} className="tipoInput"
-          >
-            <option value="action" data-testid="genre-option">Ação</option>
-            <option value="comedy" data-testid="genre-option">Comédia</option>
-            <option value="thriller" data-testid="genre-option">Suspense</option>
-          </select>
-        </div>
+        { this.textoDasEntradas() }
+        { this.localDasEntradas() } 
         <button className="btnAdicionar" data-testid="send-button" onClick={onClick}>
           Adicionar filme
         </button>
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-// import MovieList from './MovieList';
+// import React from 'react';
+// import SearchBar from './SearchBar';
+// // import MovieList from './MovieList';
 
 
-class MovieLibrary extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: this.props.movies,
-    }
-  };
-  render() {
-    const movies = this.props;
-    return (
-      <SearchBar
-      searchText={this.state.searchText}
-      bookmarkedOnly={this.state.bookmarkedOnly}
-      />
-    ) 
-  }
-} 
+// class MovieLibrary extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       searchText: '',
+//       bookmarkedOnly: false,
+//       selectedGenre: '',
+//       movies: this.props.movies,
+//     }
+//   };
+//   render() {
+//     const movies = this.props;
+//     return (
+//       <SearchBar
+//       searchText={this.state.searchText}
+//       bookmarkedOnly={this.state.bookmarkedOnly}
+//       />
+//     ) 
+//   }
+// } 
