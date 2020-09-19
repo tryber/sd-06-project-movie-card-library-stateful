f92a9f96dd95b071f06b70132779f5f8e7ef3765
Bruno Dantas <bruno.wesley77@gmail.com>
2020-09-19 10:25:03 -0300

    fix codeclimate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,14 +6,16 @@ class SearchBar extends React.Component {
     return (
       <div>
         <form data-testid="search-bar-form">
-          <label htmlFor="text-input" data-testid="text-input-label">Inclui o texto:</label>
-          <input 
-            type="text" 
-            id="text-input" 
-            data-testid="text-input" 
-            value={this.props.searchText} 
-            onChange={this.props.onSearchTextChange} 
-          />
+          <label htmlFor="text-input" data-testid="text-input-label">
+            Inclui o texto:
+            <input
+              type="text"
+              id="text-input"
+              data-testid="text-input"
+              value={this.props.searchText}
+              onChange={this.props.onSearchTextChange}
+            />
+          </label>
         </form>
       </div>
     );
