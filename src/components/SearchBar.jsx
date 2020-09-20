61326574fa93d06c1de1cfd8752386000dc65c06
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-20 18:43:50 -0300

    Testa com sucesso se ocorre a renderização do componente SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,11 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <h1>Olá! Eu estou renderizando A-Ok!</h1>
+    );
+  }
+}
+
+export default SearchBar;
