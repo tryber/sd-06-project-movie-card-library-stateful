0e24d84d37401efd5c59b4f7d8106c12e6e38068
William Gomes <williamjog@hotmail.com>
2020-09-20 18:52:01 -0300

    CodeClimate

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -7,7 +7,13 @@ class Genre extends React.Component {
     return (
       <label data-testid="genre-input-label" htmlFor="genre">
         Gênero
-        <select name="genre" id="genre" data-testid="rating-input" onChange={onChange} value={value}>
+        <select
+          name="genre"
+          id="genre"
+          data-testid="rating-input"
+          onChange={onChange}
+          value={value}
+        >
           <option data-testid="genre-option" value="action">Ação</option>
           <option data-testid="genre-option" value="comedy">Comédia</option>
           <option data-testid="genre-option" value="thriller">Suspense</option>
