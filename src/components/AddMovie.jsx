119c10b173fc2f593f99c5978737ca16c9b83452
ilanribeiro <ilanribeiro@gmail.com>
2020-09-20 14:28:57 -0300

    built input imageInput in AddMovie.jsx

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 class AddMovie extends Component {
-  render() {
+  constructor() {
+    super();
     this.state = {
       subtitle: '',
       title: '',
@@ -10,14 +11,21 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+  }
+
+  render() {
 
     return (
       <div>
         <form action="" data-testid="add-movie-form">
           <label htmlFor="title-input" data-testid="title-input-label">Título</label>
           <input type="text" name="" id="title-input" data-testid="title-input" />
+
           <label htmlFor="subtitle-input" data-testid="subtitle-input-label">Subtítulo</label>
           <input type="text" name="" id="subtitle-input" data-testid="subtitle-input" />
+
+          <label htmlFor="image-input" data-testid="image-input-label">Imagem</label>
+          <input type="text" name="" id="" data-testid="image-input" />
         </form>
       </div>
     );
