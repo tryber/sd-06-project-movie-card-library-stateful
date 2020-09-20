18d32a24df8770d2373c52d8088b64c51ce01bd7
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-20 18:58:40 -0300

    Define estados iniciais para as props: searchText, bookmarkedOnly, selectedGenre

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
   render() {
     return (
       <h1>Olá! Eu estou renderizando A-Ok!</h1>
