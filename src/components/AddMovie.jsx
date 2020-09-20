562f119ced01850e6094b936af79c8ff16451f32
Bruno Lemos <brunolemos.ssa@gmail.com>
2020-09-20 14:51:08 -0300

    Configurado estado inicial do componente AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,21 @@
-// implement AddMovie component here
+/*
+  props recebidas:
+    - onClick, uma callback
+  
+  O form deve apresentar o att data-testid="add-movie-form"
+*/
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      subtitle: '',
+      storyline: '',
+      rating: 0,
+      imagePath: '',
+      genre: '',
+    };
+  }
+}
