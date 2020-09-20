a50622a606c0343402528b502c405a265b0a77bd
ilanribeiro <ilanribeiro@gmail.com>
2020-09-20 14:57:03 -0300

    built input Avaliaçao in AddMovie.jsx

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -17,6 +17,7 @@ class AddMovie extends Component {
     return (
       <div>
         <form action="" data-testid="add-movie-form">
+
           <label htmlFor="title-input" data-testid="title-input-label">Título</label>
           <input type="text" name="" id="title-input" data-testid="title-input" />
 
@@ -24,10 +25,13 @@ class AddMovie extends Component {
           <input type="text" name="" id="subtitle-input" data-testid="subtitle-input" />
 
           <label htmlFor="image-input" data-testid="image-input-label">Imagem</label>
-          <input type="text" name="" id="" data-testid="image-input" />
+          <input type="text" name="" id="image-input" data-testid="image-input" />
 
           <label htmlFor="storyline-input" data-testid="storyline-input-label">Sinopse</label>
-          <input type="text" name="" id="" data-testid="storyline-input" />
+          <input type="text" name="" id="storyline-input" data-testid="storyline-input" />
+
+          <label htmlFor="rating-input" data-testid="rating-input-label">Avaliação</label>
+          <input type="text" name="" id="rating-input" data-testid="rating-input" />
         </form>
       </div>
     );
