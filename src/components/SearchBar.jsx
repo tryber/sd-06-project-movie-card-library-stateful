879a9c070e797e9475a31585b3e0f0c98324e2ec
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-09-20 13:17:24 -0300

    cc diminuindo erros

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,17 +7,20 @@ class SearchBar extends React.Component {
     const { selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <div>
-        <label data-testid="select-input-label">Filtrar por gênero</label>
-        <select
-          data-testid="select-input"
-          value={selectedGenre}
-          onChange={onSelectedGenreChange}
-        >
-          <option data-testid="select-option" value="">Todos</option>
-          <option data-testid="select-option" value="action">Ação</option>
-          <option data-testid="select-option" value="comedy">Comédia</option>
-          <option data-testid="select-option" value="thriller">Suspense</option>
-        </select>
+        <label data-testid="select-input-label" htmlFor="select">
+Filtrar por gênero
+          <select
+            name="select"
+            data-testid="select-input"
+            value={selectedGenre}
+            onChange={onSelectedGenreChange}
+          >
+            <option data-testid="select-option" value="">Todos</option>
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </div>
 
 
@@ -33,23 +36,28 @@ class SearchBar extends React.Component {
       <div>
         <form id='data-testid="search-bar-form'>
           <div>
-            <label data-testid="text-input-label">Inclui o Texto</label>
-            <input
-              data-testid="text-input"
-              value={searchText}
-              onChange={onSearchTextChange}
-              type="text"
-            />
+            <label data-testid="text-input-label" htmlFor="x">
+Inclui o Texto
+              <input
+                name="x"
+                data-testid="text-input"
+                value={searchText}
+                onChange={onSearchTextChange}
+                type="text"
+              />
+            </label>
           </div>
           <div>
-            <label data-testid="checkbox-input-label">Mostrar somente favoritos</label>
-            <input
-              type="checkbox"
-              data-testid="checkbox-input"
-              onChange={onBookmarkedChange}
-              checked={bookmarkedOnly}
-            />
-
+            <label data-testid="checkbox-input-label" htmlFor="checkbox">
+Mostrar somente favoritos
+              <input
+                name="checkbox"
+                type="checkbox"
+                data-testid="checkbox-input"
+                onChange={onBookmarkedChange}
+                checked={bookmarkedOnly}
+              />
+            </label>
           </div>
           <div />
 
