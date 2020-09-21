df556f4dc99d9ac773c26ba785958f8039905e23
DekoPossas <andrepossaster@gmail.com>
2020-09-20 23:36:19 -0300

    criei uma checkbox!

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,15 +10,36 @@ class SearchBar extends React.Component {
       onSearchTextChange: '',
       bookmarkedOnly: false,
       onBookmarkedChange: '',
-      selectedGenre: '',
-      onSelectedGenreChange: '',
+      // selectedGenre: '',
+      // onSelectedGenreChange: '',
     };
   }
+
   render() {
     return (
-      <form className="form" data-testid="search-bar-form">
-        <label data-testid="text-input-label">Inclui o texto:
-          <input type="text" />
+      <form
+        data-testid="search-bar-form"
+      >
+        <label 
+          data-testid="text-input-label"
+        >
+          Inclui o texto:
+          <input
+            data-testid="text-input"
+            type="text"
+            value={this.state.searchText}
+            onChange={this.state.onSearchTextChange}
+          />
+        </label>
+        <br />
+        <label data-testid="checkbox-input-label">
+          Mostrar somente favoritos
+          <input
+            data-testid="checkbox-input"
+            type="checkbox"
+            checked={this.state.bookmarkedOnly}
+            onChange={this.state.onBookmarkedChange}
+          />
         </label>
       </form>
     );
