feaeda9b20deb904ed832b5d4719a0658b9620ea
vbergadev <vbergadev@gmail.com>
2020-09-21 15:33:12 -0300

    addMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends Component {
-  constructor() {
-	  super();
-	  this.state = {
-		  subtitle: '',
-		  title: '',
-		  imagePath: '',
-		  storyline: '',
-		  rating: 0,
-		  genre: 'action',
-	};
+constructor() {
+	super();
+	this.state = {
+    subtitle: '',
+		title: '',
+		imagePath: '',
+		storyline: '',
+		rating: 0,
+		genre: 'action',
+};
 }
 
   render() {
