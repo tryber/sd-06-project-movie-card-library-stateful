eff611e0a9b37ea814e37e92074135be8021c544
Paulo <paulocheloni@gmail.com>
2020-09-21 14:37:10 -0300

    fixing lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -11,26 +11,26 @@ class MovieList extends React.Component {
 
   filterBookMarked(UnfilteredMovies, bookmarkedOnly) {
     if (bookmarkedOnly) {
-      return UnfilteredMovies.filter(movie => movie.bookmarked === true);
+      return UnfilteredMovies.filter((movie) => movie.bookmarked === true);
     }
     return UnfilteredMovies;
   }
 
   filterGender(UnfilteredMovies, gender) {
     if (gender !== '') {
-      return UnfilteredMovies.filter(movie => movie.genre === gender);
+      return UnfilteredMovies.filter((movie) => movie.genre === gender);
     }
     return UnfilteredMovies;
   }
 
   render() {
     let { movies } = this.props;
-    const { searchText, bookmarkedOnly, selectedGenre} = this.props;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.props;
     movies = this.filterBookMarked(movies, bookmarkedOnly);
     movies = this.filterGender(movies, selectedGenre);
     return (
       <div data-testid="movie-list" className="movie-list">
-        {movies.filter(movie => Object.values(movie).join('\n')
+        {movies.filter((movie) => Object.values(movie).join('\n')
           .toLowerCase().includes(searchText.toLowerCase()))
           .map((movie) => <MovieCard key={movie.title} movie={movie} />)}
       </div>
