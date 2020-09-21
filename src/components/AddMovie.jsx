d848f1f64194769171b1ac7b62db26c13ca90064
DekoPossas <andrepossaster@gmail.com>
2020-09-21 11:58:39 -0300

    criei uma requisicao proptypes para on click, criei uma label e os estados referente as labels, e a function on change

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 // implement AddMovie component here
 import React from 'react';
 
@@ -5,16 +6,40 @@ class AddMovie extends React.Component {
   constructor() {
     super();
 
-    this.state = { onClick: '' };
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: action,
+    };
+  }
+
+  onClick({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
   }
 
   render() {
+    const { onClick } = this.props;
     return (
       <form data-testid="add-movie-form">
-        ok
+        <label data-testid="title-input-label">
+          Título
+          <input
+            data-testid="title-input"
+            name="title"
+            type="text"
+            value={this.state.title}
+            onChange={this.onChange}
+          />
+        </label>
       </form>
     );
   }
 }
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
 
 export default AddMovie;
