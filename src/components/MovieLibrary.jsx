5f94821a7b1589e085339ed78e9c7aded8453f2f
danieleperse <danieleperse3@gmail.com>
2020-09-21 11:14:47 -0300

    filtro aplicado para genero e favoritos

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -16,16 +16,47 @@ class MovieLibrary extends React.Component {
     };
 
     this.buttonFunction = this.buttonFunction.bind(this);
+    this.upDateBookmarked = this.upDateBookmarked.bind(this);
+    this.upDateSelectedGenre = this.upDateSelectedGenre.bind(this);
   }
 
   buttonFunction(value) {
     this.setState({ movies: this.state.movies.concat(value) });
   }
 
+  upDateBookmarked(event) {
+    this.setState({ bookmarkedOnly: event.target.checked }, () => {
+      if (this.state.bookmarkedOnly === true) {
+        const favorites = this.props.movies.filter((movie) => movie.bookmarked === true);
+        this.setState({ movies: favorites });
+      } else {
+        this.setState({ movies: this.props.movies });
+      }
+    });
+  }
+
+  upDateSelectedGenre(event) {
+    this.setState({ selectedGenre: event.target.value }, () => {
+      if (this.state.selectedGenre === '') {
+        this.setState({ movies: this.props.movies });
+      } else {
+        const genre = this.props.movies.filter((movie) => (
+          movie.genre === this.state.selectedGenre
+        ));
+        this.setState({ movies: genre });
+      }
+    });
+  }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          onBookmarkedChange={this.upDateBookmarked}
+          selectedGenre={this.state.selectedGenre}
+          onSelectedGenreChange={this.upDateSelectedGenre}
+        />
         <MovieList movies={this.state.movies} />
         <AddMovie onClick={this.buttonFunction} />
       </div>
