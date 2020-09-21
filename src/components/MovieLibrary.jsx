0288097b47f09b838975b9590d72b2a5fc474586
Tiago Berwanger <berwangertiago@gmail.com>
2020-09-21 00:31:22 -0300

    filtros das funções Search

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -13,6 +13,9 @@ class MovieLibrary extends React.Component {
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
     this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
     this.filteringSearchChange = this.filteringSearchChange.bind(this);
+    this.filteredByBookmarked = this.filteredByBookmarked.bind(this);
+    this.filteredByGenre = this.filteredByGenre.bind(this);
+    this.refresh = this.refresh.bind(this);
 
     this.state = {
       searchText: '',
@@ -24,19 +27,38 @@ class MovieLibrary extends React.Component {
 
   onSearchTextChange({ target }) {
     const { name, value } = target;
+    const { searchText } = this.state;
+    const { movies } = this.props;
+    console.log('teste');
     this.setState({ [name]: value }, () => {
-      this.filteringSearchChange();
+      if (searchText === '') {
+        console.log('teste1');
+        // porque não captura backspace dentro do 'if'?
+        this.setState({ movies });
+      } else {
+        return this.filteringSearchChange();
+      }
     });
   }
 
   onBookmarkedChange({ target }) {
     const { name, checked } = target;
-    this.setState({ [name]: checked });
+    const { movies } = this.props;
+    this.setState({ [name]: checked }, () => {
+      if (checked === true) {
+        this.filteredByBookmarked();
+      } else {
+        return this.setState({ movies });
+      }
+    });
   }
 
   onSelectedGenreChange({ target }) {
     const { name, value } = target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => {
+      this.refresh();
+      this.filteredByGenre(value);
+    });
   }
 
   filteringSearchChange() {
@@ -44,7 +66,48 @@ class MovieLibrary extends React.Component {
     const filteredByTitle = movies
       .filter((movie) => movie.title.toUpperCase()
         .includes(searchText.toUpperCase()));
-    this.setState({ movies: filteredByTitle });
+
+    const filteredBySubtitle = movies
+      .filter((movie) => movie.subtitle.toUpperCase()
+        .includes(searchText.toUpperCase()));
+
+    const filteredByStoryline = movies
+      .filter((movie) => movie.storyline.toUpperCase()
+        .includes(searchText.toUpperCase()));
+    let text = '';
+    if (filteredByTitle.length > 0) {
+      text = filteredByTitle;
+    } else if (filteredBySubtitle.length > 0) {
+      text = filteredBySubtitle;
+    } else if (filteredByStoryline.length > 0) {
+      text = filteredByStoryline;
+    }
+    this.setState({ movies: text });
+  }
+
+  filteredByBookmarked() {
+    const { movies } = this.state;
+    const filteredByBookmarked = movies
+      .filter((movie) => movie.bookmarked === true);
+    this.setState({ movies: filteredByBookmarked });
+  }
+
+  filteredByGenre(value) {
+    const { movies } = this.state;
+    const filteredByGenre = movies
+      .filter((movie) => movie.genre === value);
+    this.setState({ movies: filteredByGenre });
+  }
+
+
+  refresh() {
+    const { movies } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies,
+    };
   }
 
   render() {
@@ -59,10 +122,11 @@ class MovieLibrary extends React.Component {
           onBookmarkedChange={this.onBookmarkedChange}
           onSelectedGenreChange={this.onSelectedGenreChange}
         />
+        <AddMovie />
         <MovieList
           movies={movies}
         />
-        <AddMovie />
+
       </div>
     );
   }
