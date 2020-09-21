2da4f7e78ad9eb63d8903318a980241f6fb8558d
DekoPossas <andrepossaster@gmail.com>
2020-09-20 22:36:29 -0300

    deletei o arquivo forms que acabei criando sem querer, e tirei seu render e seu import de SearchBar, estou renderizando normal

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,12 @@
 // implement SearchBar component here
 import React from 'react';
-import Form from './Form';
 
 class SearchBar extends React.Component {
   render() {
     return (
-      <Form data-testid="search-bar-form" />
+      <form className="form" data-testid="search-bar-form">
+        <input type="text" />
+      </form>
     );
   }
 }
