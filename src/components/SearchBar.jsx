4e5002c3145222548dfc99b7ebaf8155574f23e6
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2020-09-21 20:41:48 -0300

    code climat serchBar propTypes isRequired

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -53,12 +53,12 @@ class SearchBar extends React.Component {
 }
 
 const prop = {
-  searchText: PropTypes.string,
-  onSearchTextChange: PropTypes.func,
-  bookmarkedOnly: PropTypes.bool,
-  onBookmarkedChange: PropTypes.func,
-  selectedGenre: PropTypes.string,
-  onSelectedGenreChange: PropTypes.func,
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 SearchBar.propTypes = prop;
 
