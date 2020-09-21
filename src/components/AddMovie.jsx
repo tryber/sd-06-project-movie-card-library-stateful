17c8e62cfd8e3ceacbc7a025186365d10ddfaece
DekoPossas <andrepossaster@gmail.com>
2020-09-21 15:07:01 -0300

    terminei o forms de add movie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -78,6 +78,20 @@ class AddMovie extends React.Component {
             onChange={this.onChange}
           />
         </label>
+        <br />
+        <label data-testid="genre-input-label">
+          Gênero
+          <select
+            data-testid="genre-input"
+            name="genre"
+            value={this.state.genre}
+            onChange={this.onChange}
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
       </form>
     );
   }
