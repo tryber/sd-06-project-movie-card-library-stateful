5c4dcaff681be790210ee74783b0f65a241f8653
Paulo <paulocheloni@gmail.com>
2020-09-21 14:37:45 -0300

    fixing lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import './components/MovieLibrary';
-import MovieLibrary from './components/MovieLibrary'
+import MovieLibrary from './components/MovieLibrary';
 import Header from './components/Header';
 import data from './data';
 
