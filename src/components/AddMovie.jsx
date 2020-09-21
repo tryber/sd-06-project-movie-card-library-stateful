550dd8bc44a4f62cae48d4c5740d2f7e9b389ae5
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-20 21:06:23 -0300

    Seguindo orientações dos plantões Testa com sucesso se ocorre a renderização dos componentes AddMovie, SearchBar e MovieList dentro do componente MovieLibrary Faz-se necessário comentar algumas linhas em MovieList para não quebrar a aplicação.

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Testa se renderiza</p>
+      </div>
+    )
+  }
+}
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,19 @@
-// implement MovieLibrary component here
+import React from 'react';
+import AddMovie from './AddMovie' 
+import MovieList from './MovieList'
+import SearchBar from './SearchBar';
+
+
+class MovieLibrary extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <MovieList />
+        <AddMovie />
+      </div>
+    )
+  }
+}
+
+export default MovieLibrary;
