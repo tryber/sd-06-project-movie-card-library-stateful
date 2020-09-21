fd47640100029233bb171729dd883500eedaf303
LeonardoCavachini <cavachini240@gmail.com>
2020-09-21 08:46:29 -0300

    SearchBar && AddMovie CC update

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,35 +1,33 @@
 // implement MovieLibrary component here
-import React, {Component} from 'react'
-import SearchBar from './SearchBar'
-import AddMovie from './AddMovie'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-    constructor(props) {
-        super(props)
-        const {movies} = this.props
-        this.state = {
-            searchText: '',
-            bookmarkedOnly: false,
-            selectedGenre: '',
-            movies: movies
-        }
-    }
-    nome() {
-        
-    }
-    render() {
-        
-        return (
-            <div>
-                <SearchBar 
-                searchText={this.state.searchText}
-                bookmarkedOnly={this.bookmarkedOnly}
-                selectedGenre={this.selectedGenre}
-                />
-                <AddMovie onClick={this.nome}/>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    const { moviesArray } = this.props;
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: moviesArray,
+    };
+  }
+  nome() {}
+  render() {
+    return (
+      <div>
+        <SearchBar
+          searchText={this.state.searchText}
+          bookmarkedOnly={this.bookmarkedOnly}
+          selectedGenre={this.selectedGenre}
+        />
+        <AddMovie onClick={this.nome} />
+      </div>
+    );
+  }
 }
-
-export default MovieLibrary
+MovieLibrary.propTypes = { moviesArray: PropTypes.arrayOf.isRequired };
+export default MovieLibrary;
