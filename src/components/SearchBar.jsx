1e2ce0810eef937a5cb83d6da9d3fbbadf383646
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-21 06:42:16 -0300

    Cria as labels e inputs correspondentes para gerar o formulário Embora implemente funcionalidades para passar nos testes, falha nos testes referentes à passagem de props. Carece de aprimoramento quanto ao recebimento de props do componente pai e correção dos erros apontados pelo codeclimate referentes a exceder o tamanho máximo de linhas.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    };
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.cheked : target.value;
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
-      <form data-testid="text-input-label">
-        <label htmlFor="testing-form"> Digite algo se você me vê:
-          <input type="text" id="fname" name="testing-form" />
+      <form data-testid="search-bar-form">
+        <label htmlFor="searchText" data-testid="text-input-label"> Inclui o texto
+          <input type="text" name="searchText" value={this.state.searchText} onChange={this.handleChange} data-testid="text-input" />
+        </label>
+        <label htmlFor="bookmarkedOnly" data-testid="checkbox-input-label"> Mostrar somente favoritos
+          <input type="checkbox" name="bookmarkedOnly" value={this.state.bookmarkedOnly} onChange={this.handleChange} data-testid="checkbox-input" />
+        </label>
+        <label htmlFor="selectedGenre" data-testid="select-input-label"> Filtrar por gênero
+          <select name="selectedGenre" value={this.state.selectedGenre} onChange={this.handleChange} data-testid="select-input" >
+            <option data-testid="select-option" value="action">Ação</option>
+            <option data-testid="select-option" value="comedy">Comédia</option>
+            <option data-testid="select-option" value="thriller">Suspense</option>
+          </select>
         </label>
       </form>
     );
