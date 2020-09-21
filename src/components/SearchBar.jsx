c0a46076522ed8f410a08e59a6894594124c76c4
vbergadev <vbergadev@gmail.com>
2020-09-21 14:56:03 -0300

    addMovie

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,10 +11,10 @@ class SearchBar extends React.Component {
         value={selectedGenre}
         onChange={onSelectedGenreChange}
       >
+        <option data-testid="select-option" value="">Todos</option>
         <option data-testid="select-option" value="action">Ação</option>
         <option data-testid="select-option" value="comedy">Comédia</option>
         <option data-testid="select-option" value="thriller">Suspense</option>
-        <option data-testid="select-option" value="">Todos</option>
       </select>
     );
   }
