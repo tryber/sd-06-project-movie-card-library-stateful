facae8b4d3c2439a51384fa0801596fde73646b8
kissyla <kissylamottapereira@gmail.com>
2020-09-22 14:58:53 -0300

    corrigindo codeclimate 1

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -67,25 +67,50 @@ this.handleChange = this.handleChange.bind(this);
       </label>
       <label data-testid="subtitle-input-label">
       Subtítulo
-     <input type="text" data-testid="subtitle-input" name="subtitle" onChange={this.handleChange} >{subtitle}</input>
+     <input 
+        type="text" 
+        data-testid="subtitle-input" 
+        name="subtitle" 
+        onChange={this.handleChange}
+     >{subtitle}</input>
       </label>
 
       <label data-testid="image-input-label">
       Imagem
-     <input type="text" data-testid="image-input" name="image" onChange={this.handleChange} >{imagePath}</input>
+     <input 
+        type="text" 
+        data-testid="image-input" 
+        name="image" 
+        onChange={this.handleChange} 
+     >{imagePath}</input>
       </label>
 
       <label data-testid="storyline-input-label">
       Sinopse
-     <textarea data-testid="storyline-input" name="storyline" onChange={this.handleChange} >{storyline}</textarea>
+     <textarea 
+        data-testid="storyline-input" 
+        name="storyline" 
+        onChange={this.handleChange} 
+     >{storyline}</textarea>
       </label>
       <label data-testid="rating-input-label">
       Avaliação
-     <input type="number" data-testid="rating-input" name="rating" onChange={this.handleChange} defaultValue="0">{rating}</input>
+     <input 
+        type="number" 
+        data-testid="rating-input" 
+        name="rating" 
+        onChange={this.handleChange} 
+        defaultValue="0"
+     >{rating}</input>
       </label>
       <label data-testid="genre-input-label">
           Gênero
-        <select value={this.props.genre}  data-testid="genre-input" name="genre" onChange={this.handleChange} >
+        <select 
+          value={this.props.genre}  
+          data-testid="genre-input" 
+          name="genre" 
+          onChange={this.handleChange}
+        >
           <option value="action" data-testid="genre-option">Ação</option>
           <option value="comedy" data-testid="genre-option">Comédia</option>
           <option value="thriller" data-testid="genre-option">Suspense</option>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,21 +4,33 @@ import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
- 
-    return(
+    return (
       <div>
         <form data-testid="search-bar-form">
           <label data-testid="text-input-label">
           Inclui o texto:
-            <input type="text" value={this.props.searchText} data-testid="text-input" onChange={this.props.onSearchTextChange} />
+            <input 
+            type="text" 
+            value={this.props.searchText} 
+            data-testid="text-input" 
+            onChange={this.props.onSearchTextChange} />
             </label>
             <label data-testid="checkbox-input-label">
             Mostrar somente favoritos
-            <input type="checkbox" checked={this.props.bookmarkedOnly} data-testid="checkbox-input" onChange={this.props.onBookmarkedChange} />
+            <input 
+              type="checkbox" 
+              checked={this.props.bookmarkedOnly} 
+              data-testid="checkbox-input" 
+              onChange={this.props.onBookmarkedChange} 
+            />
             </label>
             <label data-testid="select-input-label">
             Filtrar por gênero
-              <select value={this.props.selectedGenre} data-testid="select-input" onChange={this.props.onSelectedGenreChange} >
+              <select 
+                value={this.props.selectedGenre} 
+                data-testid="select-input" 
+                onChange={this.props.onSelectedGenreChange}
+              >
               <option value="" data-testid="select-option">Todos</option>
               <option value="action" data-testid="select-option">Ação</option>
               <option value="comedy" data-testid="select-option">Comédia</option>
