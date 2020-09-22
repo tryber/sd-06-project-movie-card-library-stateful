18415f4c80ac1b0f0b05e0549f94c5f18620aae2
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-22 02:28:48 -0300

    Created filter functions

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,30 +1,68 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 import MovieList from './MovieList';
-import data from '../data';
 
 class MovieLibrary extends React.Component {
-  constructor() {
-    super();
-    //this.filteredMovies = this.filteredMovies.bind(this);
+  constructor(props) {
+    super(props);
+
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
 
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: data,
+      movies: props.movies,
     };
   }
 
-  // filteredMovies(movies) {
-  //   const { searchText } = this.state;
-  //   const searchTextRegEx = new RegExp(searchText);
-  //   if (searchText.length !== 0) {
-  //     return movies.filter((movie) => movie.title.match(searchTextRegEx));
-  //   }
-  //   return movies;
-  // }
+  onClick() {
+    return ('');
+  }
+
+  onSearchTextChange({ target }) {
+    const { value } = target;
+    const { movies } = this.state;
+    const filteredMovies = (value.length !== 0)
+      ? movies.filter((movie) => {
+        const bool = movie.title.includes(value)
+          || movie.subtitle.includes(value)
+          || movie.storyline.includes(value);
+        return bool;
+      })
+      : movies;
+
+    this.setState({
+      searchText: value,
+      movies: filteredMovies,
+    });
+  }
+
+  onBookmarkedChange({ target }) {
+    const { checked } = target;
+    const { movies } = this.state;
+    const filteredMovies = (checked) ? movies.filter((movie) => movie.bookmarked) : movies;
+
+    this.setState({
+      bookmarkedOnly: checked,
+      movies: filteredMovies,
+    });
+  }
+
+  onSelectedGenreChange({ target }) {
+    const { value } = target;
+    const { movies } = this.state;
+    const filteredMovies = (value !== '') ? movies.filter((movie) => movie.genre === value) : movies;
+
+    this.setState({
+      selectedGenre: value,
+      movies: filteredMovies,
+    });
+  }
 
   render() {
     const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
@@ -34,12 +72,17 @@ class MovieLibrary extends React.Component {
           searchText={searchText}
           bookmarkedOnly={bookmarkedOnly}
           selectedGenre={selectedGenre}
+          onSearchTextChange={this.onSearchTextChange}
+          onBookmarkedChange={this.onBookmarkedChange}
+          onSelectedGenreChange={this.onSelectedGenreChange}
         />
         <MovieList movies={movies} />
-        <AddMovie />
+        <AddMovie onClick={this.onClick} />
       </div>
     );
   }
 }
+
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default MovieLibrary;
