fa1bbcdeca99719d0a32004e950d21c0fa711058
Isadora Koga <isadorakoga710@gmail.com>
2020-09-22 13:37:12 -0300

    agr vai

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -12,7 +12,7 @@ class MovieLibrary extends Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: props.movies,
+      movies: this.props.movies,
     };
   }
 
