e240070d36212bf5178b721b995f2e4aa18ad705
Pedro <pedroedu77@gmail.com>
2020-09-22 03:23:24 -0300

    Iniciando Projeto Movie Card State full VQV Requisitos Iniciais

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,18 @@
-// implement SearchBar component here
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render () {
+    return (
+      <div>
+        <form data-testid="search-bar-form">
+          <div>
+            <label htmlFor="search-text" data-testid="text-input-label">Inclui o texto:</label>
+            <input type="text" id="search-text" data-testid="text-input" onChange={this.props.onSearchTextChange} valeu={this.props.searchText}/>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
