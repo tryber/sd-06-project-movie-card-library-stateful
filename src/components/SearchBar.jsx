796202b6341f7f024f74c05c6e77ebb9cdfa1db5
paulo-lins <paulolins100@gmail.com>
2020-09-21 22:53:28 -0300

    SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,8 +5,14 @@ import PropTypes from 'prop-types';
 class SearchBar extends React.Component {
 
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly,
-     onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const { 
+      searchText,
+      onSearchTextChange,
+      bookmarkedOnly,
+      onBookmarkedChange,
+      selectedGenre,
+      onSelectedGenreChange
+    } = this.props;
     return (
      <div className="movie-card-header">
       <form id="search-bar-form" data-testid="search-bar-form">
