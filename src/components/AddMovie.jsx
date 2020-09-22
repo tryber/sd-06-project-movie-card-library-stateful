7490240153bb1932e684e2c152387995eeba7625
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-09-22 00:26:33 -0300

    Implemented functions: AddMovie, MovieCard, MovieLibrary, MovieList and SearchBar

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,120 @@
-// implement AddMovie component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+    this.sendMovie = this.sendMovie.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  sendMovie(currentState, onClick) {
+    onClick(currentState);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
+    const { onClick } = this.props;
+    const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
+    return (
+      <form data-testid="add-movie-form" >
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            name="title"
+            id="title-input"
+            data-testid="title-input"
+            value={title}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label htmlFor="subtitle-input-label" data-testid="subtitle-input-label">
+          Subtítulo
+          <input
+            type="text"
+            name="subtitle"
+            id="subtitle-input-label"
+            data-testid="subtitle-input"
+            value={subtitle}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label htmlFor="image-input-label" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            name="imagePath"
+            id="image-input-label"
+            data-testid="image-input"
+            value={imagePath}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label htmlFor="storyline-input-label" data-testid="storyline-input-label">
+          Sinopse
+          <textarea
+            type="textarea"
+            name="storyline"
+            id="storyline-input-label"
+            data-testid="storyline-input"
+            value={storyline}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label htmlFor="rating-input-label" data-testid="rating-input-label">
+          Avaliação
+          <input
+            type="number"
+            name="rating"
+            id="rating-input-label"
+            data-testid="rating-input"
+            value={rating}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label htmlFor="genre-input-label" data-testid="genre-input-label">
+          Gênero
+          <select
+            id="genre-input-label"
+            name="genre"
+            onChange={this.handleChange}
+            data-testid="genre-input"
+            value={genre}
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
+        </label>
+        <button type="button" onClick={() => this.sendMovie(this.state, onClick)} data-testid="send-button">Adicionar filme</button>
+      </form>
+    );
+  }
+}
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
+
+export default AddMovie;
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,45 @@
-// implement MovieLibrary component here
+import React from 'react';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+import MovieList from './MovieList';
+import data from '../data';
+
+class MovieLibrary extends React.Component {
+  constructor() {
+    super();
+    //this.filteredMovies = this.filteredMovies.bind(this);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: data,
+    };
+  }
+
+  // filteredMovies(movies) {
+  //   const { searchText } = this.state;
+  //   const searchTextRegEx = new RegExp(searchText);
+  //   if (searchText.length !== 0) {
+  //     return movies.filter((movie) => movie.title.match(searchTextRegEx));
+  //   }
+  //   return movies;
+  // }
+
+  render() {
+    const { movies, searchText, bookmarkedOnly, selectedGenre } = this.state;
+    return (
+      <div>
+        <SearchBar
+          searchText={searchText}
+          bookmarkedOnly={bookmarkedOnly}
+          selectedGenre={selectedGenre}
+        />
+        <MovieList movies={movies} />
+        <AddMovie />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
