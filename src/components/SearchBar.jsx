a9a93e9446158e1f5f7b8d65e3c250b7b0bcd86e
paulo-lins <paulolins100@gmail.com>
2020-09-21 22:50:17 -0300

    SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -53,7 +53,7 @@ class SearchBar extends React.Component {
 }
 }
 
-SearchBar.PropTypes = {
+SearchBar.propTypes = {
   searchText: PropTypes.string,
   onSearchTextChange: PropTypes.func,
   bookmarkedOnly: PropTypes.bool,
