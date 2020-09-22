de2d6235a3762a4273140e713bfee8c4eb05d4eb
Matheuscara <matheuscara2000@gmail.com>
2020-09-22 17:09:49 -0300

    Code-Climate

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -32,5 +32,7 @@ class MovieLibrary extends React.Component {
     );
   }
 }
+
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default MovieLibrary;
