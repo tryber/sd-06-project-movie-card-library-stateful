fae9461c429301ace3bac9161fefd2029b3abecc
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-09-22 10:32:09 -0300

    Corrige o componente SearchBar de modo a contemplar os requisitos exigidos no teste.

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,11 +7,61 @@ import SearchBar from './SearchBar';
 
 
 class MovieLibrary extends React.Component {
+  constructor(props) {
+    super();
+    // estado inicial do pai (MovieLibrary) para o filho (SearchBar) receber via props
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // essa função vai do pai (MovieLibrary) para o filho (SearchBar), via props, para pegar o valor
+  // dos inputs de texto e o check box
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value }, () => { this.choosingMyMovies() });
+  }
+
+  // essa função vai filtrar os filmes escolhidos, de acordo com as opções de
+  // favoitos(bookmarkedOnly) palavra-chave(searchText) ou gênero(selectedGenre)
+  choosingMyMovies() {
+    let myMovies = this.props.movies;
+
+    if (this.state.bookmarkedOnly) {
+      myMovies = myMovies.filter((movie) => movie.bookmarked === true);
+    }
+
+    if (this.state.selectedGenre !== '') {
+      myMovies = myMovies.filter((movie) => movie.genre === this.state.selectedGenre);
+    }
+
+    if (this.state.searchText !== '') {
+      myMovies = myMovies.filter((movie) => movie.title.includes(this.state.searchText) ||
+                                movie.subtitle.includes(this.state.searchText) ||
+                                movie.storyline.includes(this.state.searchText),
+      );
+    }
+
+    // atribui ao estado de movies a definição dos filmes filtrados (ou não) em
+    // choosingMyMovie();
+    this.setState({ movies: myMovies });
+  }
+
   render() {
     const { movies } = this.props;
     return (
       <div>
-        <SearchBar />
+        <SearchBar
+          searchText={this.state.searchText} onSearchTextChange={this.handleChange}
+          bookmarkedOnly={this.state.bookmarkedOnly} onBookmarkedChange={this.handleChange}
+          selectedGenre={this.state.selectedGenre} onSelectedGenreChange={this.handleChange}
+        />
         <MovieList movies={movies} />
         <AddMovie />
       </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,35 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  constructor() {
-    super();
-
-    this.handleChange = this.handleChange.bind(this);
-
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-    };
-  }
-
-  handleChange({ target }) {
-    const { name } = target;
-    const value = target.type === 'checkbox' ? target.cheked : target.value;
-    this.setState({ [name]: value });
-  }
 
   render() {
+    const { searchText, onSearchTextChange, bookmarkedOnly, onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
     return (
       <form data-testid="search-bar-form">
         <label htmlFor="searchText" data-testid="text-input-label"> Inclui o texto
-          <input type="text" name="searchText" value={this.state.searchText} onChange={this.handleChange} data-testid="text-input" />
+          <input type="text" name="searchText" value={searchText} onChange={onSearchTextChange} data-testid="text-input" />
         </label>
         <label htmlFor="bookmarkedOnly" data-testid="checkbox-input-label"> Mostrar somente favoritos
-          <input type="checkbox" name="bookmarkedOnly" value={this.state.bookmarkedOnly} onChange={this.handleChange} data-testid="checkbox-input" />
+          <input type="checkbox" name="bookmarkedOnly" value={bookmarkedOnly} onChange={onBookmarkedChange} data-testid="checkbox-input" />
         </label>
         <label htmlFor="selectedGenre" data-testid="select-input-label"> Filtrar por gênero
-          <select name="selectedGenre" value={this.state.selectedGenre} onChange={this.handleChange} data-testid="select-input" >
+          <select name="selectedGenre" value={selectedGenre} onChange={onSelectedGenreChange} data-testid="select-input" >
+            <option data-testid="select-option" value="">Todos</option>
             <option data-testid="select-option" value="action">Ação</option>
             <option data-testid="select-option" value="comedy">Comédia</option>
             <option data-testid="select-option" value="thriller">Suspense</option>
