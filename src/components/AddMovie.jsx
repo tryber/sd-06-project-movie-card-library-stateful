8789eafbe73b2eda66b88c923e07911fedcc45b9
Kesley Lahoz <namielahoz@gmail.com>
2020-09-23 11:18:51 -0300

    fixing CC 5

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -40,38 +40,39 @@ class AddMovie extends React.Component {
     const { title, subtitle, imagePath, storyline, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label data-testid="title-input-label" htmlFor="title-input">
-          Título
-          <input data-testid="title-input" name="title"
-            onChange={this.handleChange} value={title}
+        <label data-testid="title-input-label" htmlFor="title-input">Título
+          <input
+            data-testid="title-input" name="title" value={title}
+            onChange={this.handleChange}
           />
         </label>
-        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">
-          Subtítulo
-          <input data-testid="subtitle-input" name="subtitle"
-            onChange={this.handleChange} value={subtitle} />
-        </label>
-        <label data-testid="image-input-label" htmlFor="image-input">
-          Imagem
-          <input data-testid="image-input" name="imagePath" 
-            onChange={this.handleChange} value={imagePath} 
+        <label data-testid="subtitle-input-label" htmlFor="subtitle-input">Subtítulo
+          <input
+            data-testid="subtitle-input" name="subtitle" value={subtitle}
+            onChange={this.handleChange}
           />
         </label>
-        <label data-testid="storyline-input-label" htmlFor="storyline-input">
-          Sinopse
-          <textarea data-testid="storyline-input" name="storyline"
-            onChange={this.handleChange} value={storyline}
+        <label data-testid="image-input-label" htmlFor="image-input">Imagem
+          <input
+            data-testid="image-input" name="imagePath" value={imagePath}
+            onChange={this.handleChange}
           />
         </label>
-        <label data-testid="rating-input-label" htmlFor="rating-input">
-          Avaliação
-          <input data-testid="rating-input" name="rating" type="number"
-            onChange={this.handleChange} value={rating}
+        <label data-testid="storyline-input-label" htmlFor="storyline-input">Sinopse
+          <textarea data-testid="storyline-input" name="storyline" value={storyline}
+            onChange={this.handleChange}
           />
         </label>
-        <label data-testid="genre-input-label" htmlFor="genre-input">
-          Gênero
-          <select data-testid="genre-input" name="genre" value={genre} onChange={this.handleChange}>
+        <label data-testid="rating-input-label" htmlFor="rating-input">Avaliação
+          <input
+            data-testid="rating-input" name="rating" type="number" value={rating}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label data-testid="genre-input-label" htmlFor="genre-input">Gênero
+          <select data-testid="genre-input" name="genre" value={genre}
+            onChange={this.handleChange}
+          >
             <option data-testid="genre-option" value="action">Ação</option>
             <option data-testid="genre-option" value="comedy">Comédia</option>
             <option data-testid="genre-option" value="thriller">Suspense</option>
