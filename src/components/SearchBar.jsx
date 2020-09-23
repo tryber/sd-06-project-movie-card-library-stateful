2aefc4d777fa54c0879272de5c07641de6cd8a19
rodrigoderkian <derkianpromo@gmail.com>
2020-09-23 17:45:00 -0300

    Criando o SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
     const {
       searchText,
       onSearchTextChange,
-      bookmarkedOnly,
-      onBookmarkedChange,
-      selectedGenre,
+    //   bookmarkedOnly,
+    //   onBookmarkedChange,
+    //   selectedGenre,
     } = this.props;
     return (
-      <form data-testid="search-bar-form">
+      <form data-testid="search-bar-form" htmlFor="text-input">
         <label data-testid="text-input-label">
           Inclui o texto:
           <input
@@ -31,7 +31,6 @@ SearchBar.propTypes = {
   bookmarkedOnly: PropTypes.bool.isRequired,
   onBookmarkedChange: PropTypes.func.isRequired,
   selectedGenre: PropTypes.string.isRequired,
-  onSelectedGenreChange: PropTypes.func.isRequired,
 };
 
 export default SearchBar;
