b7034aa71abeb870f8f8340f11bd22d067840dd8
Pedro Duarte <ikbenpedro@gmail.com>
2020-09-23 16:47:43 -0300

    cc 2.0

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -36,57 +36,22 @@ export default class AddMovie extends React.Component {
       <div>
         <form onSubmit={this.addMovie}>
           <label data-testid="title-input-label">Título</label>
-          <input
-            value={this.props.title}
-            type="text"
-            name="title"
-            onChange={this.handleChange}
-            data-testid="title-input"
-          />
-
+          <input value={this.props.title} type="text" name="title"
+            data-testid="title-input" onChange={this.handleChange}/>
           <label data-testid="subtitle-input-label">Subtítulo</label>
-          <input
-            name="subtitle"
-            value={this.state.subtitle}
-            type="text"
-            onChange={this.handleChange}
-            data-testid="subtitle-input"
-          />
-
+          <input name="subtitle" value={this.state.subtitle} type="text"
+            onChange={this.handleChange} data-testid="subtitle-input" />
           <label data-testid="image-input-label">Imagem</label>
-          <input
-            name="imagePath"
-            value={this.state.imagePath}
-            type="text"
-            onChange={this.handleChange}
-            data-testid="image-input"
-          />
-
+          <input name="imagePath" value={this.state.imagePath} type="text"
+            onChange={this.handleChange} data-testid="image-input" />
           <label data-testid="storyline-input-label">Sinopse</label>
-          <textarea
-            data-testid="storyline-input"
-            name="storyline"
-            value={this.state.storyline}
-            type="text"
-            onChange={this.handleChange}
-          />
-
+          <textarea data-testid="storyline-input" name="storyline" value={this.state.storyline} type="text" onChange={this.handleChange} />
           <label data-testid="rating-input-label">Avaliação</label>
-          <input
-            name="rating"
-            data-testid="rating-input"
-            value={this.state.rating}
-            type="number"
-            onChange={this.handleChange}
-          />
-
+          <input name="rating" data-testid="rating-input" value={this.state.rating}
+            type="number" onChange={this.handleChange} />
           <label data-testid="genre-input-label">Gênero</label>
-          <select
-            data-testid="select-input"
-            value={this.state.genre}
-            onChange={this.genreChanger}
-          >
-
+          <select data-testid="select-input" value={this.state.genre}
+            onChange={this.genreChanger} >
             <option data-testid="genre-option" value="action">Ação</option>
             <option data-testid="genre-option" value="comedy">Comédia</option>
             <option data-testid="genre-option" value="thriller">Suspense</option>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,24 +6,18 @@ class SearchBar extends React.Component {
     return (
       <div>
         <label data-testid="text-input-label">Inclui o texto:</label>
-        <input
-          data-testid="text-input"
-          value={this.props.searchText}
+        <input data-testid="text-input" value={this.props.searchText}
           onChange={this.props.onSearchTextChange}
           type="text"/>
         <form data-testid="search-bar-form">
           <label data-testid="checkbox-input-label">
             Mostrar somente favoritos
           </label>
-          <input
-            data-testid="checkbox-input"
-            type="checkbox"
+          <input data-testid="checkbox-input" type="checkbox"
             checked={this.props.bookmarkedOnly}
             onChange={this.props.onBookmarkedChange}/>
           <label data-testid="select-input-label"> Filtrar por gênero</label>
-          <select
-            data-testid="select-input"
-            value={this.props.selectedGenre}
+          <select data-testid="select-input" value={this.props.selectedGenre}
             onChange={this.props.onSelectedGenreChange}>
             <option data-testid="select-option" value="">Todos</option>
             <option data-testid="select-option" value="action">Ação</option>
