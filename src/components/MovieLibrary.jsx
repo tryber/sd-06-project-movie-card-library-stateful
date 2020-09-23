6858d8062595202d079a0024722ccacfadb0fc6b
vapatty6848 <vapatty@gmail.com>
2020-09-23 17:37:11 -0300

    primeiro push apos update

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,20 +5,17 @@ import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
-  constructor(props) {
+ constructor(props) {
     super(props);
-    const { movies } = this.props;
-
-    this.state = {
+    const { movies } = this.props; 
+ 
+     this.state = {
       searchtext: '',
       bookmarkedOnly: false,
       selectedGenre: '',
       movies:
-    };//erro ??
-
-    this.searchtext = this.searchtext.bind(this);
-    this.bookmarkedOnly = this.bookmarkedOnly.bind(this);
-    this.selectedGenre = this.selectedGenre.bind(this);
+    }
+    
   };
 
   render() {
