61152415e64e3cde8dddbcd2779cc4649669026a
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2020-09-22 22:43:03 -0300

    cc3

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AddMovie from './AddMovie';
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
+import PropTypes from 'prop-types';
 
 class MovieLibrary extends React.Component {
 
@@ -13,8 +14,20 @@ class MovieLibrary extends React.Component {
         <MovieList movies={movies} />
         <AddMovie />
       </div>
-    )
+    );
   }
+}
+
+MovieLibrary.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
 };
 
 export default MovieLibrary;
