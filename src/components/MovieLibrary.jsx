cd383f109a5b9cca14fcc41396087682e58fb384
ilanribeiro <ilanribeiro@gmail.com>
2020-09-23 07:39:54 -0300

    MovieLibrary render success SearchBar component

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -6,6 +6,16 @@ import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: props.movies,
+    }
+  }
   render() {
     const { movies } = this.props;
 
