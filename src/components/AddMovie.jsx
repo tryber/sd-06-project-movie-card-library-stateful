05f7542b679f073542ad96133ac132f4caf7e4ed
Kesley Lahoz <namielahoz@gmail.com>
2020-09-23 12:28:02 -0300

    Fixing CC 11

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -71,7 +71,14 @@ class AddMovie extends React.Component {
           />
         </label>
         <label data-testid="genre-input-label" htmlFor="genre-input">Gênero
-          {this.state.inputSelect}
+          <select
+            data-testid="genre-input" name="genre" value={genre}
+            onChange={this.handleChange}
+          >
+            <option data-testid="genre-option" value="action">Ação</option>
+            <option data-testid="genre-option" value="comedy">Comédia</option>
+            <option data-testid="genre-option" value="thriller">Suspense</option>
+          </select>
         </label>
         <button data-testid="send-button" type="submit" onClick={this.handleClick}>Adicionar filme
         </button>
