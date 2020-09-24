ebb14a5fa4f77d8016b65bae26006378180efa1f
rodrigoderkian <derkianpromo@gmail.com>
2020-09-24 17:06:29 -0300

    Criando MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends React.Component {
-    render() {
-        return (
-            <div>
-                <SearchBar />
-                <AddMovie />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <AddMovie />
+      </div>
+    );
+  }
 }
 
 export default MovieLibrary;
