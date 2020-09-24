9cb6810f4de35329215de0ed97721d59282f9b19
rodrigoderkian <derkianpromo@gmail.com>
2020-09-24 15:02:02 -0300

    Criando AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   render() {
-    const { onClick } = this.props;
+    // const { onClick } = this.props;
     return <form data-testid="add-movie-form">Teste</form>;
   }
 }
+
+AddMovie.propTypes = { onClick: PropTypes.func.isRequired };
 
 export default AddMovie;
