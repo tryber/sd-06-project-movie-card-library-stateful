6ad4e90aff4616f5d9c5eea716b8fa296e7dd0e8
rodrigoderkian <derkianpromo@gmail.com>
2020-09-24 15:43:07 -0300

    Implementando AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -3,8 +3,45 @@ import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
   render() {
-    // const { onClick } = this.props;
-    return <form data-testid="add-movie-form">Teste</form>;
+    constructor() {
+        super()
+        this.state = {
+            subtitle: '',
+            title: '',
+            imagePath: '',
+            storyline: '',
+            rating: 0,
+            genre: 'action',
+        }
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+    handleChange({ target }) {
+        const { name, value } = target;
+        this.setState({ [name]: value });
+      }
+    
+    handleClick() {
+        const { onClick } = this.props;
+        onClick(this.state);
+        this.setState({
+          subtitle: '',
+          title: '',
+          imagePath: '',
+          storyline: '',
+          rating: 0,
+          genre: 'action',
+        });
+      }
+    return (
+    <form data-testid="add-movie-form">
+        <label data-testid="title-input-label" htmlFor="title-input">
+          Título
+          <input data-testid="title-input" name="title" value={title} onChange={this.handleChange}/>
+        </label>
+    </form>
+    )
   }
 }
 
