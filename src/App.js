fb2aad9f2ce8a48cdeeeba5982555bc3c860b691
Cássio Rubens <cassiorubensadv@gmail.com>
2020-09-23 23:41:53 -0300

    Renders a  component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import MovieList from './components/MovieList';
 import movies from './data';
-import SearchBar from './components/SearchBar';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <MovieList movies={movies} />
-      <AddMovie />
+      <MovieLibrary movies={movies} />
     </div>
   );
 }
