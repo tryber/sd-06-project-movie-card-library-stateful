bd82b00c282da78631b57c6035f20822a1813e4e
rodrigoderkian <derkianpromo@gmail.com>
2020-09-24 15:49:05 -0300

    Implementando AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -2,46 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddMovie extends React.Component {
-  render() {
-    constructor() {
-        super()
-        this.state = {
-            subtitle: '',
-            title: '',
-            imagePath: '',
-            storyline: '',
-            rating: 0,
-            genre: 'action',
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleChange({ target }) {
-        const { name, value } = target;
-        this.setState({ [name]: value });
-      }
-    
-    handleClick() {
-        const { onClick } = this.props;
-        onClick(this.state);
-        this.setState({
-          subtitle: '',
-          title: '',
-          imagePath: '',
-          storyline: '',
-          rating: 0,
-          genre: 'action',
-        });
-      }
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  handleClick() {
+    const { onClick } = this.props;
+    onClick(this.state);
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    });
+  }
+
+  render() {
     return (
-    <form data-testid="add-movie-form">
+      <form data-testid="add-movie-form">
         <label data-testid="title-input-label" htmlFor="title-input">
           Título
-          <input data-testid="title-input" name="title" value={title} onChange={this.handleChange}/>
+          <input
+            data-testid="title-input"
+            name="title"
+            value={title}
+            onChange={this.handleChange}
+          />
         </label>
-    </form>
-    )
+      </form>
+    );
   }
 }
 
