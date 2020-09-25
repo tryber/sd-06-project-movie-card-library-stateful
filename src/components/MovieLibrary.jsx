5cc4fb51ded0b92d1bcdeea24698a1f39f3e43b9
LughWalle <civalfran1@gmail.com>
2020-09-25 17:53:56 -0300

    bug fix

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,62 +5,21 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-    this.onClick = this.onClick.bind(this);
-    this.onSearchTextChange = this.onSearchTextChange.bind(this);
-    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
-    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
-
-    this.state = {
-      searchText: '',
-      bookmarkedOnly: false,
-      selectedGenre: '',
-      movies: [],
-    };
-  }
-
-  onSelectedGenreChange() {
-  }
-
-  onBookmarkedChange() {
-  }
-
-  onSearchTextChange() {
-  }
-
-  onClick() {
-  }
-
   render() {
-    const {
-      onClick,
-      movies,
-      onSearchTextChange,
-      onBookmarkedChange,
-      onSelectedGenreChange,
-    } = this.props;
+    const { movies } = this.props;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar
-          onSearchTextChange={onSearchTextChange}
-          onBookmarkedChange={onBookmarkedChange}
-          onSelectedGenreChange={onSelectedGenreChange}
-        />
+        <SearchBar />
         <MovieList movies={movies} />
-        <AddMovie onClick={onClick} />
+        <AddMovie />
       </div>
     );
   }
 }
 
 MovieLibrary.propTypes = {
-  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-  onClick: PropTypes.func.isRequired,
-  onSearchTextChange: PropTypes.func.isRequired,
-  onBookmarkedChange: PropTypes.func.isRequired,
-  onSelectedGenreChange: PropTypes.func.isRequired,
+  movies: propTypes.objectOf(propTypes.any).isRequired,
 };
 
 export default MovieLibrary;
