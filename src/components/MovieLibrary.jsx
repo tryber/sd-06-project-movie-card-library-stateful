8bf8de834d1332abb05b487592ec8ae1f5ef6607
vapatty6848 <vapatty@gmail.com>
2020-09-25 12:38:10 -0300

    atualizaçao dos MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,8 +5,20 @@ import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 
 class MovieLibrary extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies:
+    }
+    onSearchTextChange = onSearchTextChange.bind(this);
+    onBookmarkedChange = onBookmarkedChange.bind(this);
+  }
   render() {
     const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { onSearchTextChange, onBookmarkedChange } = this.state;
     return (
       <div>
         <h2>My awesome movie library</h2>
@@ -14,6 +26,8 @@ class MovieLibrary extends React.Component {
           searchText={searchText}
           bookmarkedOnly={bookmarkedOnly}
           selectedGenre={selectedGenre}
+          onSearchTextChange={onSearchTextChange}
+          onBookmarkedChange={onBookmarkedChange}
         />
         <MovieList movies={movies} />
         <AddMovie />
