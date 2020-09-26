040f7ecb9cb9ec2c079e2149158c728e7919040e
LughWalle <civalfran1@gmail.com>
2020-09-26 09:22:47 -0300

    fixed 5

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -33,6 +33,6 @@ class MovieLibrary extends Component {
   }
 }
 
-MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired, };
+MovieLibrary.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 
 export default MovieLibrary;
