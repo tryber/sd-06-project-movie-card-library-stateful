4fa35bff4466b847cae5dbd3478c417f54d4527b
Marthus Sera <marthus2001@gmail.com>
2020-10-23 15:02:27 -0300

    Code Climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends React.Component {
   render() {
@@ -6,18 +7,27 @@ class SearchBar extends React.Component {
 
     return (
       <form data-testid="search-bar-form">
-        <label data-testid="text-input-label">
+        <label data-testid="text-input-label" htmlFor="text-input">
           Inclui o texto:
           <input
-          data-testid="text-input"
-          onChange={onSearchTextChange}
-          value={searchText}
-          type="text"
+            data-testid="text-input"
+            onChange={onSearchTextChange}
+            value={searchText}
+            type="text"
           />
         </label>
       </form>
     );
   }
 }
+
+SearchBar.propTypes = {
+  searchText: PropTypes.string.isRequired,
+  onSearchTextChange: PropTypes.func.isRequired,
+  bookmarkedOnly: PropTypes.bool.isRequired,
+  onBookmarkedChange: PropTypes.func.isRequired,
+  selectedGenre: PropTypes.string.isRequired,
+  onSelectedGenreChange: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
