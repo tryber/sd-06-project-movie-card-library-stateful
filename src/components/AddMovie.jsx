f6675f82b47b33bd9fcaf64096b8b9f26141e220
Marthus Sera <marthus2001@gmail.com>
2020-10-23 17:17:21 -0300

    Requirement 7

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // genre: 'action',
+      // imagePath: '',
+      // rating: 0,
+      // storyline: '',
+      // subtitle: '',
+      title: '',
+    };
+  }
+
   render() {
+    const { title } = this.state;
+
     return (
       <form data-testid="add-movie-form">
-        <h2>form</h2>
+        <label data-testid="title-input-label" htmlFor="text-input">
+          <input
+            data-testid="title-input"
+            onChange=""
+            type="text"
+            value={title}
+          />
+        </label>
       </form>
     );
   }
