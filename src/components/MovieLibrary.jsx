561cf634d01ad4d2c86db67958357ae385af0e19
Marthus Sera <marthus2001@gmail.com>
2020-10-23 19:31:36 -0300

    Requirement 15

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,34 @@
-// implement MovieLibrary component here
+/* eslint-disable react/prop-types */
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // bookmarkedOnly: false,
+      movies: props.movies,
+      searchText: '',
+      // selectedGenre: '',
+    };
+  }
+
+  render() {
+    const { movies } = this.state;
+    const { searchText } = this.state;
+
+    return (
+      <div>
+        <h2>Movie Library</h2>
+        <SearchBar
+          searchText={searchText}
+        />
+        <MovieList movies={movies} />
+      </div>
+    );
+  }
+}
+
+export default MovieLibrary;
