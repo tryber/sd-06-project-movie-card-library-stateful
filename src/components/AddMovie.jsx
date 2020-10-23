18423c9efc548daebe7ee9916b25c4674d9cbad1
Marthus Sera <marthus2001@gmail.com>
2020-10-23 16:22:46 -0300

    Requirement 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,13 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  render() {
+    return (
+      <form data-testid="add-movie-form">
+        <h2>form</h2>
+      </form>
+    );
+  }
+}
+
+export default AddMovie;
